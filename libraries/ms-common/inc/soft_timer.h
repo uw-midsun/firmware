af80465b52c0167d6eb3a58270231f9567663c21
ckitagawa <calderk2010@gmail.com>
2017-01-08 16:17:20 -0500

    ELEC-37 renaming, general cleanup, timer speed is automatic.

--- libraries/ms-common/inc/soft_timer.h
+++ libraries/ms-common/inc/soft_timer.h
@@ -1,23 +1,29 @@
 #pragma once
 
+#include <stdbool.h>
 #include <stdint.h>
 
 #include "status.h"
 
-#define SOFT_TIMER_NUM 10
+#define SOFT_TIMER_MAX_TIMERS 10
 
-typedef void (*soft_timer_callback)(uint8_t timer_id, void* context);
+typedef uint8_t SoftTimerID;
+
+typedef void (*SoftTimerCallback)(SoftTimerID timer_id, void* context);
 
 // Initializes a set of software timers. Clock speed should be in MHz. Subsequent calls will do
 // nothing. The clock speed is that of the external PLL crystal.
-void soft_timer_init(uint8_t clock_speed);
+void soft_timer_init(void);
 
 // Adds a software timer. The provided duration is the number of microseconds before running and the
 // callback is the process to run once the time has expired. The timer_id is set to the id of the
 // timer that will run the callback.
-StatusCode soft_timer_start(uint32_t duration_micros, soft_timer_callback callback,
-                            uint8_t* timer_id, void* context);
+StatusCode soft_timer_start(uint32_t duration_us, SoftTimerCallback callback, void* context,
+                            SoftTimerID* timer_id);
 
 // Updates the software timer running any callbacks that the time had expired for. Call this in your
 // main loop if using software timers.
 void soft_timer_update(void);
+
+// Checks if software timers are running, returns true if any soft timers are in use.
+bool soft_timer_inuse(void);
--- libraries/ms-common/test/test_soft_timer.c
+++ libraries/ms-common/test/test_soft_timer.c
@@ -7,30 +7,30 @@
 #include "unity.h"
 
 void setup_test(void) {
-  // This will be 4x slow on the stm32f0 discovery as it only has an 8 MHz ext clock. The controller
-  // board is 32 MHz.
-  soft_timer_init(32);
+  soft_timer_init();
 }
 
-void teardown_test(void) { }
+void teardown_test(void) {}
 
 static bool s_callback_ran = false;
 static uint8_t s_interrupt_id = 255;
 
-void prv_soft_timer_test_callback(uint8_t id, void* context) {
+void prv_soft_timer_test_callback(SoftTimerID id, void* context) {
   s_interrupt_id = id;
   s_callback_ran = true;
 }
 
 // Software timer end to end test.
 void test_soft_timer_end2end(void) {
-  uint8_t timer_id = 255;
+  SoftTimerID timer_id = 255;
   // 1 second timer.
-  TEST_ASSERT_OK(soft_timer_start(1000000, prv_soft_timer_test_callback, &timer_id, NULL));
+  TEST_ASSERT_FALSE(soft_timer_inuse());
+  TEST_ASSERT_OK(soft_timer_start(2000000, prv_soft_timer_test_callback, NULL, &timer_id));
   // Assume the timer id changed to something else. (Not a specific number as this would prevent the
   // test from passing if we ever modified the internals).
   TEST_ASSERT_NOT_EQUAL(255, timer_id);
   TEST_ASSERT_FALSE(s_callback_ran);
+  TEST_ASSERT_TRUE(soft_timer_inuse());
 
   // Update assuming the above operation is faster than the timer.
   soft_timer_update();
@@ -46,12 +46,12 @@ void test_soft_timer_end2end(void) {
 
 // Software timer create too many
 void test_soft_timer_resource_exhausted(void) {
-  uint8_t timer_id = 255;
+  SoftTimerID timer_id = 255;
   // Empty timer.
-  for (int i = 0; i < SOFT_TIMER_NUM; i++) {
-    TEST_ASSERT_OK(soft_timer_start(0, prv_soft_timer_test_callback, &timer_id, NULL));
+  for (int i = 0; i < SOFT_TIMER_MAX_TIMERS; i++) {
+    TEST_ASSERT_OK(soft_timer_start(0, prv_soft_timer_test_callback, NULL, &timer_id));
   }
 
   TEST_ASSERT_EQUAL(STATUS_CODE_RESOURCE_EXHAUSTED,
-    soft_timer_start(0, prv_soft_timer_test_callback, &timer_id, NULL));
+                    soft_timer_start(0, prv_soft_timer_test_callback, &timer_id, NULL));
 }
