d93eb98eafffe783fe8b5ea746735dad82088313
ckitagawa <calderk2010@gmail.com>
2017-02-12 15:12:19 -0500

    ELEC-37 Event driven timers

--- libraries/ms-common/src/stm32f0xx/soft_timer.c
+++ libraries/ms-common/src/stm32f0xx/soft_timer.c
@@ -2,104 +2,172 @@
 
 #include <stdbool.h>
 #include <stdint.h>
-#include <stdio.h>
 
+#include "critical_section.h"
+#include "interrupt.h"
 #include "status.h"
+#include "stm32f0xx_interrupt.h"
 #include "stm32f0xx_rcc.h"
 #include "stm32f0xx_tim.h"
 
-typedef struct timer {
+typedef struct Timer {
   uint32_t duration;
-  uint32_t time_started;
-  uint32_t last_update_value;
   void *context;
   bool inuse;
-  bool rolled_over;
   SoftTimerCallback callback;
-} timer;
+} Timer;
 
-static timer s_soft_timer_array[SOFT_TIMER_MAX_TIMERS];
+static Timer s_soft_timer_elems[SOFT_TIMER_MAX_TIMERS];
+static SoftTimerID s_active_timer_id;
+static uint32_t s_active_timer_duration;
 
-// Used to track rollovers this will happen every ~1.19 hrs as we are using microseconds.
+void prv_start_timer(uint32_t duration_us) {
+  // Stop the timer.
+  TIM_Cmd(TIM2, DISABLE);
 
-void soft_timer_init(void) {
-  for (uint32_t i = 0; i < SOFT_TIMER_MAX_TIMERS; i++) {
-    s_soft_timer_array[i].inuse = false;
-    s_soft_timer_array[i].last_update_value = 0;
-  }
+  // Clear any pending interrupts, this will always occur in an interrupt or new timer getting
+  // started in a critical section so we need to prevent a accidental trigger if the time expires in
+  // the middle of the critical section and we started a new timer which falls before it.
+  TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
 
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
-
+  // Get the clock speed of the clocks to determine the SYSCLK speed which is what TIM2 uses.
   RCC_ClocksTypeDef clock_speeds;
   RCC_GetClocksFreq(&clock_speeds);
 
-  // Configure each clock tick to be 1 microsecond from 0 to UINT32_MAX. Uses the external 8 MHz
-  // clock for tick source. This will need to adjusted or made into a variable if we choose a
-  // different external clock from the discovery board.
-  TIM_TimeBaseInitTypeDef init_struct = {.TIM_Prescaler = clock_speeds.SYSCLK_Frequency / 1000000,
-                                         .TIM_CounterMode = TIM_CounterMode_Up,
-                                         .TIM_Period = UINT32_MAX,
-                                         .TIM_ClockDivision = TIM_CKD_DIV1 };
+  // Configure each clock tick to be 1 microsecond from 0 to duration.
+  TIM_TimeBaseInitTypeDef init_struct = { .TIM_Prescaler = clock_speeds.SYSCLK_Frequency / 1000000,
+                                          .TIM_CounterMode = TIM_CounterMode_Up,
+                                          .TIM_Period = duration_us,
+                                          .TIM_ClockDivision = TIM_CKD_DIV1 };
+  s_active_timer_duration = duration_us;
 
+  // Reset the counter to 0.
+  TIM_SetCounter(TIM2, 0);
+
+  // Start the timer.
   TIM_TimeBaseInit(TIM2, &init_struct);
   TIM_Cmd(TIM2, ENABLE);
 }
 
+void prv_soft_timer_update(uint32_t curr_time) {
+  // Default these values to max. If there is ever a newer timer it will replace them. In the event
+  // there are no timers left the defaults will result in the module being reset to a state where no
+  // timers exist.
+  SoftTimerID min_time_id = SOFT_TIMER_MAX_TIMERS;
+  uint32_t min_duration = UINT32_MAX;
+  for (uint32_t i = 0; i < SOFT_TIMER_MAX_TIMERS; i++) {
+    // If the timer is in use and wasn't just added then update its duration.
+    if (s_soft_timer_elems[i].inuse) {
+      if (curr_time >= s_soft_timer_elems[i].duration) {
+        // For each inuse timer see if the timer expired. If it has run its callback.
+        s_soft_timer_elems[i].callback(i, s_soft_timer_elems[i].context);
+        s_soft_timer_elems[i].inuse = false;
+      } else {
+        // Otherwise, update the duration left on the timer.
+        s_soft_timer_elems[i].duration -= curr_time;
+        // Figure out the next timer.
+        if (min_duration >= s_soft_timer_elems[i].duration) {
+          min_time_id = i;
+          min_duration = s_soft_timer_elems[i].duration;
+        }
+      }
+    }
+  }
+  // Set the active timer id.
+  s_active_timer_id = min_time_id;
+}
+
+void soft_timer_init(void) {
+  // Start the PeiphClock
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+  // Stop the timer if it was running and ensure only overflows trigger updates.
+  // Also remove the ARR preload.
+  TIM_Cmd(TIM2, DISABLE);
+  TIM_UpdateRequestConfig(TIM2, TIM_UpdateSource_Regular);
+  TIM_ARRPreloadConfig(TIM2, DISABLE);
+
+  // Disable all the timers and forget the last running timer.
+  s_active_timer_id = SOFT_TIMER_MAX_TIMERS;
+  for (uint32_t i = 0; i < SOFT_TIMER_MAX_TIMERS; i++) {
+    s_soft_timer_elems[i].inuse = false;
+  }
+
+  // Enable the interrupts. TIM2 uses IRQ channel 15.
+  stm32f0xx_interrupt_nvic_enable(15, INTERRUPT_PRIORITY_NORMAL);
+  TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+}
+
 StatusCode soft_timer_start(uint32_t duration_us, SoftTimerCallback callback, void *context,
                             SoftTimerID *timer_id) {
+  // Get the time this was called.
   uint32_t curr_time = TIM_GetCounter(TIM2);
+
+  // Enable a critical section.
+  bool critical = critical_section_start();
+
   for (uint32_t i = 0; i < SOFT_TIMER_MAX_TIMERS; i++) {
-    if (!s_soft_timer_array[i].inuse) {
-      // Populate next available timer if possible.
-      s_soft_timer_array[i].duration = duration_us;
-      s_soft_timer_array[i].time_started = curr_time;
-      s_soft_timer_array[i].last_update_value = curr_time;
-      s_soft_timer_array[i].callback = callback;
-      s_soft_timer_array[i].context = context;
-      s_soft_timer_array[i].inuse = true;
-      s_soft_timer_array[i].rolled_over = false;
+    if (!s_soft_timer_elems[i].inuse) {
+      // Look for an empty timer.
+
+      if (s_active_timer_id >= SOFT_TIMER_MAX_TIMERS) {
+        // New timer will be the only timer. Start it.
+        s_active_timer_id = i;
+        prv_start_timer(duration_us);
+      } else if (duration_us < s_soft_timer_elems[s_active_timer_id].duration - curr_time) {
+        // New timer will run be before the active timer. Update and start it. Use as close to now
+        // as possible for the update.
+        prv_soft_timer_update(TIM_GetCounter(TIM2));
+        s_active_timer_id = i;
+        prv_start_timer(duration_us);
+      } else {
+        // Add the current time to cancel what was on the clock when called.
+        duration_us += curr_time;
+      }
+      // Otherwise, the new timer is longest ignore it until later.
+
+      // Actually populate the timer. Do this after the previous steps as this is a critical section
+      // and this timer shouldn't be updated in prv_soft_timer_update.
+      s_soft_timer_elems[i].duration = duration_us;
+      s_soft_timer_elems[i].callback = callback;
+      s_soft_timer_elems[i].context = context;
+      s_soft_timer_elems[i].inuse = true;
       *timer_id = i;
+      critical_section_end(critical);
       return STATUS_CODE_OK;
     }
   }
 
+  // Out of timers.
+  critical_section_end(critical);
   return status_msg(STATUS_CODE_RESOURCE_EXHAUSTED, "Out of software timers.");
-}
-
-void soft_timer_update(void) {
-  // Get the time to use to update.
-  uint32_t curr_time = TIM_GetCounter(TIM2);
-
-  for (uint32_t i = 0; i < SOFT_TIMER_MAX_TIMERS; i++) {
-    if (s_soft_timer_array[i].inuse) {
-      // For each inuse timer see if the timer expired. If it has run its callback. Otherwise update
-      // if it has rolled over.
-
-      // If the timer has rolled over relative to the last time it was updated take note of this.
-      bool rolled_over = false;
-      if (s_soft_timer_array[i].last_update_value > curr_time) {
-        rolled_over = true;
-      }
-
-      if ((uint64_t)curr_time + UINT32_MAX * rolled_over - s_soft_timer_array[i].time_started >
-          s_soft_timer_array[i].duration) {
-        s_soft_timer_array[i].callback(i, s_soft_timer_array[i].context);
-        s_soft_timer_array[i].inuse = false;
-        s_soft_timer_array[i].last_update_value = 0;
-      } else {
-        s_soft_timer_array[i].last_update_value = curr_time;
-        s_soft_timer_array[i].rolled_over = rolled_over;
-      }
-    }
-  }
 }
 
 bool soft_timer_inuse(void) {
   for (uint32_t i = 0; i < SOFT_TIMER_MAX_TIMERS; i++) {
-    if (s_soft_timer_array[i].inuse) {
+    if (s_soft_timer_elems[i].inuse) {
       return true;
     }
   }
 
   return false;
+}
+
+// TIM2 Interrupt handler as defined in stm32f0xx.h
+void TIM2_IRQHandler(void) {
+  if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET) {
+    // Stop the timer to prevent repeat triggers if it is a really short period.
+    TIM_Cmd(TIM2, DISABLE);
+
+    // Update the timer assuming the active timer expired.
+    prv_soft_timer_update(s_active_timer_duration);
+
+    // Start the next active timer if it exists as determined by the update.
+    if (s_active_timer_id < SOFT_TIMER_MAX_TIMERS) {
+      prv_start_timer(s_soft_timer_elems[s_active_timer_id].duration);
+      return;
+    }
+
+    // Clear the pending bit to stop a repeat trigger if there are no new timers.
+    TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+  }
 }
--- libraries/ms-common/src/x86/soft_timer.c
+++ libraries/ms-common/src/x86/soft_timer.c
@@ -1,64 +1,175 @@
 #include "soft_timer.h"
 
+#include <signal.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <time.h>
 
+#include "critical_section.h"
+#include "interrupt.h"
 #include "status.h"
+#include "x86_interrupt.h"
 
-typedef struct timer {
-  uint64_t time_started_usecs;
+typedef struct Timer {
   uint32_t duration;
   void* context;
   bool inuse;
   SoftTimerCallback callback;
-} timer;
+} Timer;
 
-static timer s_soft_timer_array[SOFT_TIMER_MAX_TIMERS];
+typedef struct POSIXTimer {
+  timer_t timer_id;
+  uint32_t duration_us;
+  struct sigevent event;
+  bool created;
+  bool active;
+} POSIXTimer;
+
+static Timer s_soft_timer_array[SOFT_TIMER_MAX_TIMERS];
+static SoftTimerID s_active_timer_id;
+static POSIXTimer s_posix_timer;
+
+void prv_soft_timer_update(uint32_t curr_time) {
+  // Default these values to max. If there is ever a newer timer it will replace them. In the event
+  // there are no timers left the defaults will result in the module being reset to a state where no
+  // timers exist.
+  SoftTimerID min_time_id = SOFT_TIMER_MAX_TIMERS;
+  uint32_t min_duration = UINT32_MAX;
+  for (uint32_t i = 0; i < SOFT_TIMER_MAX_TIMERS; i++) {
+    // If the timer is in use and wasn't just added then update its duration.
+    if (s_soft_timer_array[i].inuse) {
+      if (curr_time >= s_soft_timer_array[i].duration) {
+        // For each inuse timer see if the timer expired. If it has run its callback.
+        s_soft_timer_array[i].callback(i, s_soft_timer_array[i].context);
+        s_soft_timer_array[i].inuse = false;
+      } else {
+        // Otherwise, update the duration left on the timer.
+        s_soft_timer_array[i].duration -= curr_time;
+
+        // Figure out the next timer.
+        if (min_duration >= s_soft_timer_array[i].duration) {
+          min_time_id = i;
+          min_duration = s_soft_timer_array[i].duration;
+        }
+      }
+    }
+  }
+  // Set the active timer id.
+  s_active_timer_id = min_time_id;
+}
+
+void prv_start_timer(uint32_t duration_us) {
+  // Create an empty interrupt timerspec.
+  struct itimerspec timerspec = { { 0, 0 }, { 0, 0 } };
+
+  // Set the it_val to the value we want the interrupt to trigger on.
+  timerspec.it_value.tv_sec = duration_us / 1000000;
+  timerspec.it_value.tv_nsec = duration_us % 1000000 * 1000;
+
+  // Set the time of the interrupt on the timer. 0 is the flag for using time relative to the
+  // current clock time. NULL is since we don't care what the previous settings were.
+  timer_settime(s_posix_timer.timer_id, 0, &timerspec, NULL);
+
+  // Update the state of the timer.
+  s_posix_timer.duration_us = duration_us;
+  s_posix_timer.active = true;
+}
+
+void prv_soft_timer_interrupt(void) {
+  // Assume the next interrupt is the active one.
+  bool critical = critical_section_start();
+  prv_soft_timer_update(s_soft_timer_array[s_active_timer_id].duration);
+  if (s_active_timer_id < SOFT_TIMER_MAX_TIMERS) {
+    prv_start_timer(s_soft_timer_array[s_active_timer_id].duration);
+  }
+  critical_section_end(critical);
+}
+
+void prv_soft_timer_handler(uint8_t interrupt_id) {
+  // Run the interrupt since there is only one for this handler.
+  prv_soft_timer_interrupt();
+}
 
 void soft_timer_init(void) {
+  // Delete the timer if it exists.
+  if (s_posix_timer.created) {
+    timer_delete(s_posix_timer.timer_id);
+    s_posix_timer.created = false;
+    s_posix_timer.active = false;
+  }
+
+  // Set all timers to make none appear active.
+  s_active_timer_id = SOFT_TIMER_MAX_TIMERS;
   for (uint32_t i = 0; i < SOFT_TIMER_MAX_TIMERS; i++) {
     s_soft_timer_array[i].inuse = false;
   }
+
+  // Register a handler and interrupt.
+  uint8_t handler_id;
+  x86_interrupt_register_handler(prv_soft_timer_handler, &handler_id);
+  InterruptSettings it_settings = { .type = INTERRUPT_TYPE_INTERRUPT,
+                                    .priority = INTERRUPT_PRIORITY_NORMAL };
+  uint8_t interrupt_id;
+  x86_interrupt_register_interrupt(handler_id, &it_settings, &interrupt_id);
+
+  // Create the timer. It triggers an interrupt on the interrupt registered here.
+  s_posix_timer.event.sigev_value.sival_int = interrupt_id;
+  s_posix_timer.event.sigev_notify = SIGEV_SIGNAL;
+  s_posix_timer.event.sigev_signo = SIGRTMIN + INTERRUPT_PRIORITY_NORMAL;
+  timer_create(CLOCK_MONOTONIC, &s_posix_timer.event, &s_posix_timer.timer_id);
+  s_posix_timer.created = true;
 }
 
 StatusCode soft_timer_start(uint32_t duration_us, SoftTimerCallback callback, void* context,
                             SoftTimerID* timer_id) {
-  struct timespec curr_time;
-  clock_gettime(CLOCK_MONOTONIC, &curr_time);
+  // Start a critical section to prevent this section from being broken.
+  bool critical = critical_section_start();
+
+  // Get the current time.
+  uint32_t curr_time = 0;
+  if (s_posix_timer.active) {
+    struct itimerspec curr_timespec;
+    timer_gettime(s_posix_timer.timer_id, &curr_timespec);
+    curr_time = s_posix_timer.duration_us -
+                (curr_timespec.it_value.tv_sec * 1000000 + curr_timespec.it_value.tv_nsec / 1000);
+  }
+
   for (uint32_t i = 0; i < SOFT_TIMER_MAX_TIMERS; i++) {
     if (!s_soft_timer_array[i].inuse) {
-      // Populate next available timer if possible.
+      // Look for an empty timer.
+      if (s_active_timer_id >= SOFT_TIMER_MAX_TIMERS) {
+        // New timer will be the only timer. Start it.
+        s_active_timer_id = i;
+        prv_start_timer(duration_us);
+      } else if (duration_us < s_soft_timer_array[s_active_timer_id].duration - curr_time) {
+        // New timer will run be before the active timer. Update and start it. Use as close to now
+        // as possible for the update.
+        prv_soft_timer_update(curr_time);
+        s_active_timer_id = i;
+        prv_start_timer(duration_us);
+      } else {
+        // Add the curr time to the newest timer so it isn't affected by the timer currently on the
+        // clock.
+        duration_us += curr_time;
+      }
+      // Otherwise, the new timer is longest ignore it until later.
+
+      // Actually populate the timer. Do this after the previous steps as this is a critical section
+      // and this timer shouldn't be updated.
       s_soft_timer_array[i].duration = duration_us;
-      s_soft_timer_array[i].time_started_usecs =
-          curr_time.tv_sec * 1000000 + curr_time.tv_nsec / 1000;
       s_soft_timer_array[i].callback = callback;
       s_soft_timer_array[i].context = context;
       s_soft_timer_array[i].inuse = true;
       *timer_id = i;
+      critical_section_end(critical);
       return STATUS_CODE_OK;
     }
   }
 
+  // Out of timers.
+  critical_section_end(critical);
   return status_msg(STATUS_CODE_RESOURCE_EXHAUSTED, "Out of software timers.");
-}
-
-void soft_timer_update(void) {
-  struct timespec update_time;
-  clock_gettime(CLOCK_MONOTONIC, &update_time);
-  uint64_t update_time_usecs = update_time.tv_sec * 1000000 + update_time.tv_nsec / 1000;
-
-  for (uint32_t i = 0; i < SOFT_TIMER_MAX_TIMERS; i++) {
-    if (s_soft_timer_array[i].inuse) {
-      // For each inuse timer see if the timer expired. If it has run its callback. Otherwise update
-      // if it has rolled over.
-      if ((uint64_t)update_time_usecs - s_soft_timer_array[i].time_started_usecs >
-          s_soft_timer_array[i].duration) {
-        s_soft_timer_array[i].callback(i, s_soft_timer_array[i].context);
-        s_soft_timer_array[i].inuse = false;
-      }
-    }
-  }
 }
 
 bool soft_timer_inuse(void) {
