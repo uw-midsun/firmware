14811070288ed0d71de88d5387461d46c355e190
Michelle Dang <michelle.dang@hotmail.com>
2017-03-04 12:32:08 -0500

    Removed unnecessary settings and usart support
    removed: word length, 1.5 stop bits, hardware flow control and clock settings

--- libraries/ms-common/inc/usart.h
+++ libraries/ms-common/inc/usart.h
@@ -6,19 +6,10 @@
 #include "interrupt.h"
 #include "status.h"
 
-
-// For setting word length
-typedef enum {
-  USART_WORD_LENGTH_8b = 0,
-  USART_WORD_LENGTH_9b,
-  USART_WORD_LENGTH_7b,
-} USARTWordLength;
-
 // For setting number of stop bits
 typedef enum {
   USART_STOP_BITS_1 = 0,
   USART_STOP_BITS_2,
-  USART_STOP_BITS_1_5,
 } USARTStopBits;
 
 // For setting parity mode
@@ -34,69 +25,28 @@ typedef enum {
   USART_MODE_TX,
 } USARTMode;
 
-// For setting hardware flow control mode
-typedef enum {
-  USART_HARDWARE_FLOW_CONTROL_NONE = 0,
-  USART_HARDWARE_FLOW_CONTROL_RTS,
-  USART_HARDWARE_FLOW_CONTROL_CTS,
-  USART_HARDWARE_FLOW_CONTROL_RTS_CTS,
-} USARTHardwareFlowControl;
-
-// For enabling/disabling USART clock
-typedef enum {
-  USART_CLOCK_DISABLE = 0,
-  USART_CLOCK_ENABLE,
-} USARTClock;
-
-// For setting steady state of clock
-typedef enum {
-  USART_CPOLARITY_LOW = 0,
-  USART_CPOLARITY_HIGH,
-} USARTCpol;
-
-// For setting when bit capture is made
-typedef enum {
-  USART_CPHASE_1EDGE = 0,
-  USART_CPHASE_2EDGE,
-} USARTCpha;
-
-// For setting if clock pulse corresponds to last transmitted data bit
-typedef enum {
-  USART_LASTBIT_DISABLE = 0,
-  USART_LASTBIT_ENABLE,
-} USARTLastBit;
-
 // For setting interrupt source
 typedef enum {
-  USART_IT_SOURCE_TXE = 0;
-  USART_IT_SOURCE_RXNE;
-  USART_IT_SOURCE_TC;
-  USART_IT_SOURCE_IDLE;
-  USART_IT_SOURCE_CTS;
-  USART_IT_SOURCE_LBD;
-  USART_IT_SOURCE_PE;
-  USART_IT_SOURCE_ERR;
+  USART_IT_SOURCE_TXE = 0,
+  USART_IT_SOURCE_RXNE,
+  USART_IT_SOURCE_TC,
+  USART_IT_SOURCE_IDLE,
+  USART_IT_SOURCE_CTS,
+  USART_IT_SOURCE_LBD,
+  USART_IT_SOURCE_PE,
+  USART_IT_SOURCE_ERR,
 } USARTITSource;
 
 // USART settings
 typedef struct {
   uint32_t baud_rate;
-  USARTWordLength word_length;
   USARTStopBits stop_bits;
   USARTParity parity;
   USARTMode mode;
-  USARTHardwareFlowControl hardware_flow_control;
   GPIOAddress tx_address;
   GPIOAddress rx_address;
 } USARTSettings;
 
-// USART clock settings (for synchronous mode)
-typedef struct {
-  USARTClock clock;
-  USARTCpol polarity;
-  USARTCpha phase;
-  USARTLastBit last_bit;
-} USARTClockSettings;
 
 typedef void (*usart_it_callback)(uint8_t periph, void *context);
 
@@ -111,11 +61,8 @@ StatusCode usart_it_trigger_interrupt(uint8_t address);
 // Initializes USART with settings
 StatusCode usart_init(uint8_t periph, USARTSettings *settings);
 
-// Initializes USART peripheral clock (for synchronous mode)
-StatusCode usart_clock_init(uint8_t periph, USARTClockSettings *clock_settings);
-
 // Configures interrupt source
-StatusCode usart_it_config(uint8_t periph, USARTITSource source, FunctionalState state);
+StatusCode usart_it_config(uint8_t periph, USARTITSource source);
 
 // Transmits data
 StatusCode usart_send_data(USARTSettings *settings, uint16_t data);
