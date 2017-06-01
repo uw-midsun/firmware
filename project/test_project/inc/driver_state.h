affa3057b91ed834c37f5f7832373e89e175f0be
Fidel Musa <fxmusa@gmail.com>
2017-06-01 15:35:33 -0400

    ELEC-174: Modify State Machine

--- project/test_project/inc/driver_state.h
+++ project/test_project/inc/driver_state.h
@@ -6,19 +6,16 @@
 
 void driver_state_off(FSM* fsm, const Event* e, void *context);
 
-void driver_state_idle_neutral(FSM* fsm, const Event* e, void *context);
-void driver_state_idle_forward(FSM* fsm, const Event* e, void *context);
-void driver_state_idle_backward(FSM* fsm, const Event* e, void *context);
+void driver_state_idle(FSM* fsm, const Event* e, void *context);
+void driver_state_brake(FSM* fsm, const Event* e, void *context);
+void driver_state_driving(FSM* fsm, const Event* e, void *context);
 
-void driver_state_driving_forward(FSM* fsm, const Event* e, void *context);
-void driver_state_driving_backward(FSM* fsm, const Event* e, void *context);
-
-void driver_state_brake_neutral(FSM* fsm, const Event* e, void *context);
-void driver_state_brake_forward(FSM* fsm, const Event* e, void *context);
-void driver_state_brake_backward(FSM* fsm, const Event* e, void *context);
+void driver_state_neutral(FSM* fsm, const Event* e, void *context);
+void driver_state_forward(FSM* fsm, const Event* e, void *context);
+void driver_state_reverse(FSM* fsm, const Event* e, void *context);
 
 typedef enum {
-  INPUT_EVENT_POWER_ON,
+  INPUT_EVENT_POWER_ON = 0,
   INPUT_EVENT_POWER_OFF,
   INPUT_EVENT_GAS_PRESSED,
   INPUT_EVENT_GAS_RELEASED,
@@ -32,6 +29,7 @@ typedef enum {
   INPUT_EVENT_DIRECTION_SELECTOR_REVERSE,
   INPUT_EVENT_HEADLIGHTS_ON,
   INPUT_EVENT_HEADLIGHTS_OFF,
+  INPUT_EVENT_TURN_SIGNAL_NONE,
   INPUT_EVENT_TURN_SIGNAL_LEFT,
   INPUT_EVENT_TURN_SIGNAL_RIGHT,
   INPUT_EVENT_HAZARD_LIGHT_ON,
@@ -43,78 +41,76 @@ typedef enum {
   INPUT_EVENT_REGEN_STRENGTH_ON
 } InputEvent;
 
-// Off State: when the car is not receiving power
-FSM_DECLARE_STATE(state_off);
+typedef struct FSMGroup {
+    FSM pedal_fsm;
+    FSM direction_fsm;
+    FSM turn_signal_fsm;
+} FSMGroup;
 
-// Idle State: when neither of the gas pedals are pressed
-FSM_DECLARE_STATE(state_idle_neutral);
-FSM_DECLARE_STATE(state_idle_forward);
-FSM_DECLARE_STATE(state_idle_reverse);
+FSM_DECLARE_STATE(state_off);     // Off State: Car is not receiving power
+FSM_DECLARE_STATE(state_idle);    // Idle State: Neither of the gas pedals are pressed
+FSM_DECLARE_STATE(state_brake);   // Brake State: Driver is holding down the brake pedal
+FSM_DECLARE_STATE(state_driving); // Driving State: Car is in motion due to the gas pedal
 
-// Brake State: when the driver is holding down the brake pedal
-FSM_DECLARE_STATE(state_brake_neutral);
-FSM_DECLARE_STATE(state_brake_forward);
-FSM_DECLARE_STATE(state_brake_reverse);
+FSM_DECLARE_STATE(state_neutral); 
+FSM_DECLARE_STATE(state_forward);
+FSM_DECLARE_STATE(state_reverse);
 
-// Driving State: when the car is in motion due to the gas pedal
-FSM_DECLARE_STATE(state_driving_forward);
-FSM_DECLARE_STATE(state_driving_reverse);
+FSM_DECLARE_STATE(state_no_signal);
+FSM_DECLARE_STATE(state_left_signal);
+FSM_DECLARE_STATE(state_right_signal);
 
-// State table for off state
+/********************  Transition table for pedal state machine  ******************/
 FSM_STATE_TRANSITION(state_off) {
-  FSM_ADD_TRANSITION(INPUT_EVENT_POWER_ON, state_idle_neutral);
+  FSM_ADD_TRANSITION(INPUT_EVENT_POWER_ON, state_idle);
 }
 
-// State table for idle superstate
-FSM_STATE_TRANSITION(state_idle_neutral) {
+FSM_STATE_TRANSITION(state_idle) {
   FSM_ADD_TRANSITION(INPUT_EVENT_POWER_OFF, state_off);
-  FSM_ADD_TRANSITION(INPUT_EVENT_BRAKE_PRESSED, state_brake_neutral);
+  FSM_ADD_TRANSITION(INPUT_EVENT_GAS_PRESSED, state_driving);
+  FSM_ADD_TRANSITION(INPUT_EVENT_BRAKE_PRESSED, state_brake);
   FSM_ADD_TRANSITION(INPUT_EVENT_EMERGENCY_STOP, state_off);
 }
 
-FSM_STATE_TRANSITION(state_idle_forward) {
-  FSM_ADD_TRANSITION(INPUT_EVENT_GAS_PRESSED, state_driving_forward);
-  FSM_ADD_TRANSITION(INPUT_EVENT_BRAKE_PRESSED, state_brake_forward);
+FSM_STATE_TRANSITION(state_brake) {
+  FSM_ADD_TRANSITION(INPUT_EVENT_BRAKE_RELEASED, state_idle);
   FSM_ADD_TRANSITION(INPUT_EVENT_EMERGENCY_STOP, state_off);
 }
 
-FSM_STATE_TRANSITION(state_idle_reverse) {
-  FSM_ADD_TRANSITION(INPUT_EVENT_GAS_PRESSED, state_driving_reverse);
-  FSM_ADD_TRANSITION(INPUT_EVENT_BRAKE_PRESSED, state_brake_reverse);
+FSM_STATE_TRANSITION(state_driving) {
+  FSM_ADD_TRANSITION(INPUT_EVENT_GAS_RELEASED, state_idle);
+  FSM_ADD_TRANSITION(INPUT_EVENT_BRAKE_PRESSED, state_brake);
   FSM_ADD_TRANSITION(INPUT_EVENT_EMERGENCY_STOP, state_off);
 }
 
-// State table for brake superstate
-FSM_STATE_TRANSITION(state_brake_neutral) {
-  FSM_ADD_TRANSITION(INPUT_EVENT_DIRECTION_SELECTOR_DRIVE, state_brake_forward);
-  FSM_ADD_TRANSITION(INPUT_EVENT_DIRECTION_SELECTOR_REVERSE, state_brake_reverse);
-  FSM_ADD_TRANSITION(INPUT_EVENT_BRAKE_RELEASED, state_idle_neutral);
-  FSM_ADD_TRANSITION(INPUT_EVENT_EMERGENCY_STOP, state_off);
+/********************  Transition table for direction state machine  ******************/
+FSM_STATE_TRANSITION(state_neutral) {
+  FSM_ADD_TRANSITION(INPUT_EVENT_DIRECTION_SELECTOR_DRIVE, state_forward);
+  FSM_ADD_TRANSITION(INPUT_EVENT_DIRECTION_SELECTOR_REVERSE, state_reverse);
 }
 
-FSM_STATE_TRANSITION(state_brake_forward) {
-  FSM_ADD_TRANSITION(INPUT_EVENT_DIRECTION_SELECTOR_NEUTRAL, state_brake_neutral);
-  FSM_ADD_TRANSITION(INPUT_EVENT_DIRECTION_SELECTOR_REVERSE, state_brake_reverse);
-  FSM_ADD_TRANSITION(INPUT_EVENT_BRAKE_RELEASED, state_idle_forward);
-  FSM_ADD_TRANSITION(INPUT_EVENT_EMERGENCY_STOP, state_off);
+FSM_STATE_TRANSITION(state_forward) {
+  FSM_ADD_TRANSITION(INPUT_EVENT_DIRECTION_SELECTOR_NEUTRAL, state_neutral);
+  FSM_ADD_TRANSITION(INPUT_EVENT_DIRECTION_SELECTOR_REVERSE, state_reverse);
 }
 
-FSM_STATE_TRANSITION(state_brake_reverse) {
-  FSM_ADD_TRANSITION(INPUT_EVENT_DIRECTION_SELECTOR_NEUTRAL, state_brake_neutral);
-  FSM_ADD_TRANSITION(INPUT_EVENT_DIRECTION_SELECTOR_DRIVE, state_brake_forward);
-  FSM_ADD_TRANSITION(INPUT_EVENT_BRAKE_RELEASED, state_idle_reverse);
-  FSM_ADD_TRANSITION(INPUT_EVENT_EMERGENCY_STOP, state_off);
+FSM_STATE_TRANSITION(state_reverse) {
+  FSM_ADD_TRANSITION(INPUT_EVENT_DIRECTION_SELECTOR_DRIVE, state_forward);
+  FSM_ADD_TRANSITION(INPUT_EVENT_DIRECTION_SELECTOR_NEUTRAL, state_neutral);
 }
 
-// State table for driving superstate
-FSM_STATE_TRANSITION(state_driving_forward) {
-  FSM_ADD_TRANSITION(INPUT_EVENT_GAS_RELEASED, state_idle_forward);
-  FSM_ADD_TRANSITION(INPUT_EVENT_BRAKE_PRESSED, state_brake_forward);
-  FSM_ADD_TRANSITION(INPUT_EVENT_EMERGENCY_STOP, state_off);
+/********************  Transition table for the turn signal state machine  ******************/
+FSM_STATE_TRANSITION(state_no_signal) {
+  FSM_ADD_TRANSITION(INPUT_EVENT_TURN_SIGNAL_LEFT, state_left_signal);
+  FSM_ADD_TRANSITION(INPUT_EVENT_TURN_SIGNAL_RIGHT, state_right_signal);
 }
 
-FSM_STATE_TRANSITION(state_driving_reverse) {
-  FSM_ADD_TRANSITION(INPUT_EVENT_GAS_RELEASED, state_idle_reverse);
-  FSM_ADD_TRANSITION(INPUT_EVENT_BRAKE_PRESSED, state_brake_reverse);
-  FSM_ADD_TRANSITION(INPUT_EVENT_EMERGENCY_STOP, state_off);
+FSM_STATE_TRANSITION(state_left_signal) {
+  FSM_ADD_TRANSITION(INPUT_EVENT_TURN_SIGNAL_NONE, state_no_signal);
+  FSM_ADD_TRANSITION(INPUT_EVENT_TURN_SIGNAL_RIGHT, state_right_signal);
+}
+
+FSM_STATE_TRANSITION(state_right_signal) {
+  FSM_ADD_TRANSITION(INPUT_EVENT_TURN_SIGNAL_LEFT, state_left_signal);
+  FSM_ADD_TRANSITION(INPUT_EVENT_TURN_SIGNAL_NONE, state_no_signal);
 }
