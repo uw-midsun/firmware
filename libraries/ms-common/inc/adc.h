d1ec0ac411093649b34d07b982aa9a70921f0e07
Fidel Musa <fxmusa@gmail.com>
2017-06-07 12:44:29 -0400

    ELEC-174: Update ADC Driver

--- libraries/ms-common/inc/adc.h
+++ libraries/ms-common/inc/adc.h
@@ -9,22 +9,22 @@
 
 	External Channel mapping
 	Pin		ADC Channel
-    PA0     ADC_IN0
-    PA1     ADC_IN1
-    PA2     ADC_IN2
-    PA3     ADC_IN3
-    PA4     ADC_IN4
-    PA5     ADC_IN5
-    PA6     ADC_IN6
-    PA7     ADC_IN7
-    PB0     ADC_IN8
-    PB1     ADC_IN9
-    PC0     ADC_IN10
-    PC1     ADC_IN11
-    PC2     ADC_IN12
-    PC3     ADC_IN13
-    PC4     ADC_IN14
-    PC5     ADC_IN15
+  PA0   ADC_IN0
+  PA1   ADC_IN1
+  PA2   ADC_IN2
+  PA3   ADC_IN3
+  PA4   ADC_IN4
+  PA5   ADC_IN5
+  PA6   ADC_IN6
+  PA7   ADC_IN7
+  PB0   ADC_IN8
+  PB1   ADC_IN9
+  PC0   ADC_IN10
+  PC1   ADC_IN11
+  PC2   ADC_IN12
+  PC3   ADC_IN13
+  PC4   ADC_IN14
+  PC5   ADC_IN15
 	
 */
 
@@ -34,18 +34,30 @@ typedef enum {
 } ADCMode;
 
 typedef enum {
+  ADC_RESOLUTION_12,
+  ADC_RESOLUTION_10,
+  ADC_RESOLUTION_8,
+  ADC_RESOLUTION_6
+} ADCResolution;
+
+typedef enum {
   ADC_SAMPLE_RATE_1 = 0,	// 1.5 cycles
-  ADC_SAMPLE_RATE_2, 		// 7.5 cycles
-  ADC_SAMPLE_RATE_3,		// 13.5 cycles
-  ADC_SAMPLE_RATE_4,		// 28.5 cycles
-  ADC_SAMPLE_RATE_5,		// 41.5 cycles
-  ADC_SAMPLE_RATE_6,		// 55.5 cycles
-  ADC_SAMPLE_RATE_7,		// 71.5 cycles
-  ADC_SAMPLE_RATE_8			// 239.5 cycles
+  ADC_SAMPLE_RATE_2, 		  // 7.5 cycles
+  ADC_SAMPLE_RATE_3,		  // 13.5 cycles
+  ADC_SAMPLE_RATE_4,		  // 28.5 cycles
+  ADC_SAMPLE_RATE_5,	    // 41.5 cycles
+  ADC_SAMPLE_RATE_6,		  // 55.5 cycles
+  ADC_SAMPLE_RATE_7,		  // 71.5 cycles
+  ADC_SAMPLE_RATE_8			  // 239.5 cycles
 } ADCSampleRate;
 
+typedef struct ADCSettings {
+  ADCMode mode;
+  ADCResolution resolution;
+} ADCSettings;
+
 // Initialize the onboard ADC in the specified conversion mode
-void adc_init(ADCMode adc_mode);
+void adc_init(ADCSettings* settings);
 
 // Sets the continuous mode sample rate for the given pin.
 bool adc_init_pin(GPIOAddress* address, ADCSampleRate adc_sample_rate);
--- libraries/ms-common/src/adc.c
+++ libraries/ms-common/src/adc.c
@@ -1,7 +1,7 @@
 #include "adc.h"
 
 static uint8_t prv_get_channel(GPIOAddress* address) {
-  volatile uint8_t adc_channel = address->pin;
+  uint8_t adc_channel = address->pin;
 
   switch (address->port) {
      case GPIO_PORT_A:
@@ -25,12 +25,12 @@ static uint8_t prv_get_channel(GPIOAddress* address) {
   return adc_channel;
 }
 
-void adc_init(ADCMode adc_mode) {
+void adc_init(ADCSettings* settings) {
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
 
   ADC_InitTypeDef adc_settings = {
-    ADC_Resolution_10b,
-    adc_mode,
+    settings->resolution,
+    settings->mode,
     ADC_ExternalTrigConvEdge_None,
     ADC_ExternalTrigConv_T1_TRGO,
     ADC_DataAlign_Right,
@@ -42,7 +42,7 @@ void adc_init(ADCMode adc_mode) {
   ADC_GetCalibrationFactor(ADC1);
   ADC_Cmd(ADC1, ENABLE);
 
-  ADC_ContinuousModeCmd(ADC1, adc_mode);
+  ADC_ContinuousModeCmd(ADC1, settings->mode);
   while (ADC_GetFlagStatus(ADC1, ADC_FLAG_ADCAL)) {}
 
   ADC_StartOfConversion(ADC1);
@@ -57,17 +57,19 @@ bool adc_init_pin(GPIOAddress* address, ADCSampleRate adc_sample_rate) {
 
 uint16_t adc_read(GPIOAddress* address, uint16_t max) {
 	bool continuous_mode = ADC1->CFGR1 >> 13;
-	
+	uint8_t adc_channel = prv_get_channel(address);
+  uint16_t resolution = 1 << (12 - 2*adc_channel);
+
 	if (!continuous_mode) {
 		ADC_StartOfConversion(ADC1);
 	}
 
-	ADC1->CFGR1 ^= (ADC1->CFGR1 >> 13) ? ADC_CR_ADSTART : 0;
-  ADC1->CHSELR = 1 << prv_get_channel(address);
-	ADC1->CFGR1 ^= (ADC1->CFGR1 >> 13) ? ADC_CR_ADSTART : 0;
+	ADC1->CFGR1 ^= (continuous_mode) ? ADC_CR_ADSTART : 0;
+  ADC1->CHSELR = 1 << adc_channel;
+	ADC1->CFGR1 ^= (continuous_mode) ? ADC_CR_ADSTART : 0;
   
   uint16_t adc_reading = ADC_GetConversionValue(ADC1);
-  uint16_t reading = (max * adc_reading)/1024;
+  uint16_t reading = (max * adc_reading)/resolution;
 
   return reading;
 }
