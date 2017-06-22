953d2ea5dca20baf004307178e2074c6af0fd695
Fidel Musa <fxmusa@gmail.com>
2017-06-22 19:23:45 -0400

    ELEC-164: Added x86 placeholder implementation

---
+++ libraries/ms-common/src/x86/adc.c
@@ -0,0 +1,13 @@
+#include "adc.h"
+
+void adc_init(ADCMode adc_mode) { }
+
+StatusCode adc_set_channel(ADCChannel adc_channel, bool new_state) { }
+
+StatusCode adc_register_callback(ADCChannel adc_channel, adc_callback callback, void *context) { }
+
+void adc_start_continuous() { }
+
+uint16_t adc_read_value(ADCChannel adc_channel) { }
+
+void adc_disable() { }
