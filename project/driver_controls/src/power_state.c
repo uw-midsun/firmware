85495f841192d010f9b281c040e6317c673caa61
Fidel Musa <fxmusa@gmail.com>
2017-06-30 12:56:35 -0400

    ELEC-198: Added a separate FSM for power

---
+++ project/driver_controls/src/power_state.c
@@ -0,0 +1,42 @@
+#include "power_state.h"
+
+FSM_DECLARE_STATE(state_off);
+FSM_DECLARE_STATE(state_on);
+
+// State machine transition tables
+FSM_STATE_TRANSITION(state_off) {
+  FSM_ADD_TRANSITION(INPUT_EVENT_POWER, state_on);
+}
+
+FSM_STATE_TRANSITION(state_on) {
+  FSM_ADD_TRANSITION(INPUT_EVENT_POWER, state_off);
+}
+
+// Transition check functions
+static bool prv_check_off(const Event *e) {
+  if (e->id == INPUT_EVENT_POWER) {
+    return true;
+  }
+
+  return false;
+}
+
+static bool prv_check_on(const Event *e) {
+  return true;
+}
+
+// State output functions
+static void prv_state_off(FSM *fsm, const Event *e, void *context) {
+  fsm->context = prv_check_off;
+}
+
+static void prv_state_on(FSM *fsm, const Event *e, void *context) {
+  fsm->context = prv_check_on;
+}
+
+void power_state_init(FSM *power_fsm, void *context) {
+  fsm_state_init(state_off, prv_state_off);
+  fsm_state_init(state_on, prv_state_on);
+
+  fsm_init(power_fsm, "power_fsm", &state_off, prv_check_off);
+}
