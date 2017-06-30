e2b3c5e717ffbba9e830791c8741b10cae57563f
Fidel Musa <fxmusa@gmail.com>
2017-06-30 12:54:39 -0400

    ELEC-198: Added FSM for mechanical brake

---
+++ project/driver_controls/inc/mechanical_brake.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "fsm_state.h"
+
+void mechanical_brake_state_init(FSM *mechanical_brake_fsm, void *context);
---
+++ project/driver_controls/src/mechanical_brake.c
@@ -0,0 +1,47 @@
+#include "power_state.h"
+
+FSM_DECLARE_STATE(state_engaged);
+FSM_DECLARE_STATE(state_disengaged);
+
+// State machine transition tables
+FSM_STATE_TRANSITION(state_engaged) {
+  FSM_ADD_TRANSITION(INPUT_EVENT_MECHANICAL_BRAKE, state_disengaged);
+}
+
+FSM_STATE_TRANSITION(state_disengaged) {
+  FSM_ADD_TRANSITION(INPUT_EVENT_MECHANICAL_BRAKE, state_engaged);
+}
+
+// Transition check functions
+static bool prv_check_mechanical_brake_engaged(const Event *e) {
+  switch (e->id) {
+    case INPUT_EVENT_GAS_COAST:
+    case INPUT_EVENT_GAS_PRESSED:
+    case INPUT_EVENT_CRUISE_CONTROL:
+    case INPUT_EVENT_CRUISE_CONTROL_INC:
+    case INPUT_EVENT_CRUISE_CONTROL_DEC:
+      return false;
+    default:
+      return true;
+  }
+}
+
+static bool prv_check_mechanical_brake_disengaged(const Event *e) {
+  return true;
+}
+
+// State output functions
+static void prv_state_mechanical_brake_engaged(FSM *fsm, const Event *e, void *context) {
+  fsm->context = prv_check_mechanical_brake_engaged;
+}
+
+static void prv_state_mechanical_brake_disengaged(FSM *fsm, const Event *e, void *context) {
+  fsm->context = prv_check_mechanical_brake_disengaged;
+}
+
+void power_state_init(FSM *power_fsm, void *context) {
+  fsm_state_init(state_engaged, prv_state_mechanical_brake_engaged);
+  fsm_state_init(state_disengaged, prv_state_mechanical_brake_disengaged);
+
+  fsm_init(power_fsm, "power_fsm", &state_disengaged, prv_check_mechanical_brake_disengaged);
+}
