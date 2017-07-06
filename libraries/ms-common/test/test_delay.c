4a6567b4ab223fa352d1ffb37404f1148e2d2a54
ckitagawa <calderk2010@gmail.com>
2017-07-06 19:26:06 -0400

    ELEC-210: Delay and wait module

---
+++ libraries/ms-common/test/test_delay.c
@@ -0,0 +1,26 @@
+#include "delay.h"
+
+#include "interrupt.h"
+#include "soft_timer.h"
+#include "unity.h"
+
+// These tests serve a dual purpose as they also implicitly test the wait module.
+
+void setup_test(void) {
+  interrupt_init();
+  soft_timer_init();
+}
+
+void teardown_test(void) {}
+
+void test_delay_us(void) {
+  delay_us(10000);
+}
+
+void test_delay_ms(void) {
+  delay_ms(10);
+}
+
+void test_delay_s(void) {
+  delay_s(1);
+}
