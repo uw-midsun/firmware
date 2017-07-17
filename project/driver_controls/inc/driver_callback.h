62486c9e83503350b63ce8da6fdb6464b786386a
Fidel Musa <fxmusa@gmail.com>
2017-07-17 17:51:33 -0400

    ELEC-198: Restored driver_callback module

---
+++ project/driver_controls/inc/driver_callback.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// Callback functions for GPIO device interrupts
+
+// Once an event has been detected, the proper ISR will be called to determine the Event ID based
+// on the triggering address so that the correct event can be raised in the priority queue.
+
+#include "adc.h"
+#include "gpio.h"
+
+// Since the gas pedal is monitored through ADC interrupts, a separate callback is needed
+void driver_callback_pedal(ADCChannel adc_channel, void *context);
+
+// Main ISR for GPIO device interrupts
+void driver_callback_input(GPIOAddress *address, void *context);
