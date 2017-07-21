ebb291840b53a06a4ff79a36dc6548c895c8ca63
Fidel Musa <fxmusa@gmail.com>
2017-07-21 11:33:42 -0400

    ELEC-229: Added TEST_ASSERT_NOT_OK()

--- libraries/ms-common/inc/test_helpers.h
+++ libraries/ms-common/inc/test_helpers.h
@@ -6,6 +6,7 @@
 #include "unity.h"
 
 #define TEST_ASSERT_OK(code) TEST_ASSERT_EQUAL(STATUS_CODE_OK, (code))
+#define TEST_ASSERT_NOT_OK(code) TEST_ASSERT_NOT_EQUAL(STATUS_CODE_OK, (code))
 
 // TEST ONLY FUNCTION TO SET TIMER COUNTER FOR SOFT TIMERS UNSAFE TO CALL OUTSIDE A TEST.
 void _test_soft_timer_set_counter(uint32_t counter_value);
