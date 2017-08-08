fdf7d7cab61fccb4d70589beff6723fd32202ef4
Fidel Musa <fxmusa@gmail.com>
2017-08-08 13:25:02 -0400

    ELEC-216: Fixed i2c timeout issue

--- projects/driver_controls/src/magnetic_sensor.c
+++ projects/driver_controls/src/magnetic_sensor.c
@@ -10,29 +10,31 @@ static uint8_t s_write_reg[NUM_TLV493D_WRITE_REGISTERS];
 
 StatusCode magnetic_sensor_init(I2CPort i2c_port) {
   // Reset device by calling address 0x0
-  i2c_read(i2c_port, 0x0, &data, 0);
+  i2c_read(i2c_port, 0x0, s_read_reg, 0);
 
   // Obtain the values of the read registers
-  i2c_read(i2c_port, 0x4E, &s_read_reg, NUM_TLV493D_READ_REGISTERS);
+  i2c_read(i2c_port, TLV493D_ADDRESS, s_read_reg, NUM_TLV493D_READ_REGISTERS);
 
   // Correctly configure the write bits
-  s_write_reg[TLV493D_RES1] = 0x00,
+  s_write_reg[TLV493D_RES1] = 0x00;
 
   // FACTSET1 [4:3] -> MOD1 [4:3]
   s_write_reg[TLV493D_MOD1] = s_read_reg[TLV493D_FACTSET1] & 0x18;
 
   // FACTSET2 [7:0] -> RES2 [7:0]
-  s_write_reg[TLV493D_RES2] = s_read_reg[TLV493D_FACTSET2]
+  s_write_reg[TLV493D_RES2] = s_read_reg[TLV493D_FACTSET2];
 
   // FACTSET3 [4:0] -> MOD2 [4:0]
   s_write_reg[TLV493D_MOD2] = s_read_reg[TLV493D_FACTSET3] & 0x1f;
 
 
-  i2c_write(i2c_port, )
+  i2c_write(i2c_port, TLV493D_ADDRESS, s_write_reg, NUM_TLV493D_WRITE_REGISTERS);
 }
 
-StatusCode magnetic_sensor_read_data(I2CPort i2c_port, TLV493DHallProbe probe) {
-  uint8_t data[3];
+StatusCode magnetic_sensor_read_data(I2CPort i2c_port, MagneticSensorProbe probe) {
+  uint8_t data[NUM_MAGNETIC_SENSOR_PROBES] = { 0 };
 
-  i2c_read(I2CPort i2c, 0x4E, data, 3)
+  i2c_read(i2c_port, TLV493D_ADDRESS, data, NUM_MAGNETIC_SENSOR_PROBES);
+
+  return data[probe];
 }
