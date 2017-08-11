9787a980c4aa11310d9ace8b977200778ca66858
Fidel Musa <fxmusa@gmail.com>
2017-08-11 14:00:22 -0400

    ELEC-216: Gathered register macros into enums

--- projects/driver_controls/inc/tlv493d.h
+++ projects/driver_controls/inc/tlv493d.h
@@ -3,27 +3,32 @@
 // TLV493D default I2C address definition (Datasheet Chapter 4.2)
 #define TLV493D_ADDRESS   0x5E
 
-// TLV493D read registers (Datasheet Chapter 7.1)
-#define TLV493D_BX        0x0
-#define TLV493D_BY        0x1
-#define TLV493D_BZ        0x2
-#define TLV493D_TEMP      0x3
-#define TLV493D_BX2       0x4
-#define TLV493D_BZ2       0x5
-#define TLV493D_TEMP2     0x6
-#define TLV493D_FACTSET1  0x7
-#define TLV493D_FACTSET2  0x8
-#define TLV493D_FACTSET3  0x9
+// TLV493D register definitions (Datasheet Chapter 7.1)
+typedef enum {
+  TLV493D_BX = 0,
+  TLV493D_BY,
+  TLV493D_BZ,
+  TLV493D_TEMP,
+  TLV493D_BX2,
+  TLV493D_BZ2,
+  TLV493D_TEMP2,
+  TLV493D_FACTSET1,
+  TLV493D_FACTSET2,
+  TLV493D_FACTSET3,
+  NUM_TLV493D_READ_REGISTERS,
+  TLV493D_RES1 = 0,
+  TLV493D_MOD1,
+  TLV493D_RES2,
+  TLV493D_MOD2, 
+  NUM_TLV493D_WRITE_REGISTERS
+} TLV493DRegister;
 
-#define NUM_TLV493D_READ_REGISTERS 10
+// TLV493D register bitmasks
+#define TLV493D_MASTER_CONTROLLED_MODE  0x3
 
-// TLV493D read registers (Datasheet Chapter 7.1)
-#define TLV493D_RES1      0x0
-#define TLV493D_MOD1      0x1
-#define TLV493D_RES2      0x2
-#define TLV493D_MOD2      0x3
+#define TLV493D_FACTSET1_MASK           0x18
+#define TLV493D_FACTSET3_MASK           0x1F
 
-#define NUM_TLV493D_WRITE_REGISTERS 4
 
 // Constant to convert from LSB to microteslas (Datasheet chapter 3.1)
 #define TLV493D_LSB_TO_TESLA 98
