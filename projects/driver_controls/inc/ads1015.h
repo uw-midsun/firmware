7af6b92bef59e08aeba3b597d9e2af1a92bf95e7
Fidel Musa <fxmusa@gmail.com>
2017-08-15 17:24:00 -0400

    ELEC-217: Added support for callbacks

--- projects/driver_controls/inc/ads1015.h
+++ projects/driver_controls/inc/ads1015.h
@@ -7,11 +7,6 @@
 // Driver for the ADS1015 ADC
 
 typedef enum {
-  ADS1015_MODE_SINGLE_SHOT,
-  ADS1015_MODE_CONTINUOUS
-} ADS1015Mode;
-
-typedef enum {
   ADS1015_CHANNEL_0,
   ADS1015_CHANNEL_1,
   ADS1015_CHANNEL_2,
@@ -21,10 +16,11 @@ typedef enum {
 
 typedef void (*ADS1015Callback)(ADS1015Channel channel, void *context);
 
-StatusCode ads1015_init(I2CPort i2c_port, GPIOAddress address, ADS1015Mode op_mode);
+StatusCode ads1015_init(I2CPort i2c_port, GPIOAddress address);
 
 // Register a callback function to be called when the specified channel completes a conversion
-StatusCode ads1015_register_callback(ADS1015Channel channel, ADS1015Callback callback, void *context);
+StatusCode ads1015_register_callback(ADS1015Channel channel,
+                                      ADS1015Callback callback, void *context);
 
 // Obtain the raw 12-bit value read by the specified channel
 StatusCode ads1015_read_raw(ADS1015Channel channel, uint16_t *reading);
--- projects/driver_controls/src/ads1015.c
+++ projects/driver_controls/src/ads1015.c
@@ -1,6 +1,9 @@
 #include "ads1015.h"
+
 #include <stdbool.h>
 #include <stdio.h>
+
+#include "gpio_it.h"
 
 // ADS1015 I2C address definition (Datasheet Section 8.5.1.1)
 #define ADS1015_I2C_ADDRESS             0x48
@@ -14,13 +17,18 @@
 #define ADS1015_LO_THRESH_REGISTER      0x02
 #define ADS1015_HI_THRESH_REGISTER      0x03
 
-// ADS1015 Bitmasks 
+// ADS1015 Bitmasks
 #define ADS1015_CONFIG_MODE             0x100
 
 #define ADS1015_CONFIG_MUX_AIN0         0x4000
 #define ADS1015_CONFIG_MUX_AIN1         0x5000
 #define ADS1015_CONFIG_MUX_AIN2         0x6000
 #define ADS1015_CONFIG_MUX_AIN3         0x7000
+
+// Number of successive conversions required before asserting ALERT/RDY
+#define ADS1015_CONFIG_COMP_QUE_ONE     0x1
+#define ADS1015_CONFIG_COMP_QUE_TWO     0x2
+#define ADS1015_CONFIG_COMP_QUE_FOUR    0x4
 
 // Union definition to help process 16-bit registers
 // Due to endian-ness, index 0 refers to the most significant byte
@@ -29,8 +37,24 @@ typedef union ADS1015Register {
   uint8_t data[2];
 } ADS1015Register;
 
-// The address to be used as the ISR pin
+typedef struct ADS1015Interrupt {
+  ADS1015Callback callback;
+  void *context;
+  int16_t reading;
+} ADS1015Interrupt;
+
 static GPIOAddress s_address;
+static ADS1015Channel s_current_channel;
+
+static ADS1015Interrupt s_interrupts[NUM_ADS1015_CHANNELS];
+
+static StatusCode prv_channel_valid(ADS1015Channel channel) {
+  if (channel >= NUM_ADS1015_CHANNELS) {
+    return STATUS_CODE_INVALID_ARGS;
+  }
+
+  return STATUS_CODE_OK;
+}
 
 // Initiates a read as outlined in section 8.5.3 of the datasheet
 static StatusCode prv_read(I2CPort i2c_port, uint8_t reg, uint8_t *data) {
@@ -49,45 +73,28 @@ static StatusCode prv_write(I2CPort i2c_port, uint8_t reg, uint8_t *data) {
   return STATUS_CODE_OK;
 }
 
-StatusCode ads1015_init(I2CPort i2c_port, GPIOAddress address, ADS1015Mode op_mode) {
-  s_address = address;
+static void prv_interrupt_handler(GPIOAddress *address, void *context) {
+  ADS1015Register reg;
 
-  // Reset the internal registers to their default values 
-  uint8_t reset = ADS1015_RESET_BYTE;
-  status_ok_or_return(i2c_write(i2c_port, 0, &reset, 1));
+  // Obtain ADC readings
+  status_ok_or_return(prv_read(I2C_PORT_1, ADS1015_CONVERSION_REGISTER, reg.data));
+  s_interrupts[s_current_channel].reading = (reg.data[0] << 4) | (reg.data[1] >> 4);
 
-  // Configure the device operating mode 
-
-  ADS1015Register config;
-  status_ok_or_return(prv_read(I2C_PORT_1, ADS1015_CONFIG_REGISTER, config.data));
-
-  switch (op_mode) {
-    case ADS1015_MODE_SINGLE_SHOT:
-      config.data[0] |= (ADS1015_CONFIG_MODE >> 8);
-    case ADS1015_MODE_CONTINUOUS:
-      config.data[0] &= ~(ADS1015_CONFIG_MODE >> 8);
+  if (s_interrupts[s_current_channel].callback != NULL) {
+    s_interrupts[s_current_channel].callback(s_current_channel,
+                                              s_interrupts[s_current_channel].context);
   }
 
-  status_ok_or_return(prv_write(I2C_PORT_1, ADS1015_CONFIG_REGISTER, config.data));
+  // Update so that the ADC reads from the next channel
+  s_current_channel = (s_current_channel + 1) % NUM_ADS1015_CHANNELS;
 
-  return STATUS_CODE_OK;
-}
-
-StatusCode ads1015_register_callback(ADS1015Channel channel, ADS1015Callback callback, void *context) {
-
-}
-
-StatusCode ads1015_read_raw(ADS1015Channel channel, uint16_t *reading) {
-  // Select the channel to convert
-  ADS1015Register reg;
+  // Obtain the current value for the config register
   status_ok_or_return(prv_read(I2C_PORT_1, ADS1015_CONFIG_REGISTER, reg.data));
 
-  //printf("data = %#x%x -> ", reg.data[0], reg.data[1]);
-  // Clear bits 12-14
-  reg.raw &= 0x8FFF;
+  // Set bits 12-14 based on the current channel
+  reg.data[0] &= 0x81;
 
-  // Set mux bits to the correct setting based on the selected channel
-  switch (channel) {
+  switch (s_current_channel) {
     case ADS1015_CHANNEL_0:
       reg.raw |= (ADS1015_CONFIG_MUX_AIN0 >> 8);
       break;
@@ -102,20 +109,68 @@ StatusCode ads1015_read_raw(ADS1015Channel channel, uint16_t *reading) {
       break;
   }
 
-  //printf("%#x%x\n", reg.data[0], reg.data[1]);
-
   status_ok_or_return(prv_write(I2C_PORT_1, ADS1015_CONFIG_REGISTER, reg.data));
 
-  // Obtain ADC readings
-  status_ok_or_return(prv_read(I2C_PORT_1, ADS1015_CONVERSION_REGISTER, reg.data));
-
-  // Data is stored in the 12 most significant bits
-  //printf("reg.raw = %#x\n", reg.raw);
-  *reading = (reg.data[0] << 4) | (reg.data[1] >> 4);
-  printf("reading = 0x%03x | data = 0x%02x%x\n", *reading, reg.data[0], reg.data[1]);
   return STATUS_CODE_OK;
 }
 
-StatusCode ads1015_read_converted(ADS1015Channel channel, uint16_t *reading) {
+StatusCode ads1015_init(I2CPort i2c_port, GPIOAddress address) {
+  // Reset the internal registers to their default values
+  uint8_t reset = ADS1015_RESET_BYTE;
+  status_ok_or_return(i2c_write(i2c_port, 0, &reset, 1));
 
+  // Initialize the ADC in continuous mode
+  ADS1015Register config;
+  status_ok_or_return(prv_read(I2C_PORT_1, ADS1015_CONFIG_REGISTER, config.data));
+  printf("data = %#x%x\n", config.data[0], config.data[1]);
+  config.data[0] &= ~(ADS1015_CONFIG_MODE >> 8);
+  printf("data = %#x%x\n", config.data[0], config.data[1]);
+  status_ok_or_return(prv_write(I2C_PORT_1, ADS1015_CONFIG_REGISTER, config.data));
+
+  // Configure the given GPIO address as a conversion ready pin
+  GPIOSettings gpio_settings = { .direction = GPIO_DIR_IN, .alt_function = GPIO_ALTFN_NONE };
+  InterruptSettings it_settings = { INTERRUPT_TYPE_INTERRUPT, INTERRUPT_PRIORITY_NORMAL };
+
+  gpio_init_pin(&address, &gpio_settings);
+  gpio_it_register_interrupt(&address, &it_settings, INTERRUPT_EDGE_FALLING,
+                              prv_interrupt_handler, NULL);
+
+  // Conversion ready must be initialized by setting the MSB of the HI_THRESH and LO_THRESH to 1
+  // and 0 respectively
+  uint8_t data[] = { 0x80, 0x00 };
+  prv_write(I2C_PORT_1, ADS1015_HI_THRESH_REGISTER, data);
+
+  data[0] = 0x00;
+  prv_write(I2C_PORT_1, ADS1015_LO_THRESH_REGISTER, data);
+
+  // Enable ALERT/RDY pin
+  data[1] = ADS1015_CONFIG_COMP_QUE_FOUR;
+  prv_write(I2C_PORT_1, ADS1015_CONFIG_REGISTER, data);
+
+  // Initialize interrupt callbacks
+  for (uint8_t i = 0; i < NUM_ADS1015_CHANNELS; i++) {
+    s_interrupts[i].callback = NULL;
+    s_interrupts[i].context = NULL;
+    s_interrupts[i].reading = 0;
+  }
+
+  return STATUS_CODE_OK;
 }
+
+StatusCode ads1015_register_callback(ADS1015Channel channel,
+                                      ADS1015Callback callback, void *context) {
+  status_ok_or_return(prv_channel_valid(channel));
+
+  s_interrupts[channel].callback = callback;
+  s_interrupts[channel].context = context;
+
+  return STATUS_CODE_OK;
+}
+
+StatusCode ads1015_read_raw(ADS1015Channel channel, uint16_t *reading) {
+  status_ok_or_return(prv_channel_valid(channel));
+
+  *reading = s_interrupts[channel].reading;
+}
+
+StatusCode ads1015_read_converted(ADS1015Channel channel, uint16_t *reading) { }
