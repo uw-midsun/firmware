b44d5f6528477e30c03098be80c69a5ca73b938e
ckitagawa <calderk2010@gmail.com>
2017-08-27 12:22:36 -0400

    ELEC-263: improve test by adding delay timeout

--- libraries/ms-common/test/test_pwm.c
+++ libraries/ms-common/test/test_pwm.c
@@ -2,16 +2,22 @@
 
 #include <stdint.h>
 
+#include "delay.h"
 #include "gpio.h"
 #include "gpio_it.h"
 #include "interrupt.h"
 #include "interrupt_def.h"
 #include "log.h"
+#include "soft_timer.h"
 #include "test_helpers.h"
 #include "unity.h"
 
+#define PWM_TIMER PWM_TIMER_14
 #define PERIOD_MS 100
-#define WAIT_CYCLES 5
+#define DUTY_CYCLE 50
+#define EXPECTED_EDGES 3
+#define PWM_TEST_ADDR \
+  { GPIO_PORT_A, 7 }
 
 static volatile uint16_t s_counter = 0;
 
@@ -21,6 +27,7 @@ static void prv_pwm_test(const GPIOAddress *address, void *context) {
 
 void setup_test(void) {
   interrupt_init();
+  soft_timer_init();
   gpio_init();
   gpio_it_init();
   s_counter = 0;
@@ -29,39 +36,34 @@ void setup_test(void) {
 void teardown_test(void) {}
 
 void test_pwm_guards(void) {
-  TEST_ASSERT_EQUAL(STATUS_CODE_UNINITIALIZED, pwm_set_pulse(PWM_TIMER_3, 50));
-  TEST_ASSERT_EQUAL(STATUS_CODE_INVALID_ARGS, pwm_init(PWM_TIMER_3, 0));
-  TEST_ASSERT_OK(pwm_init(PWM_TIMER_3, PERIOD_MS));
-  TEST_ASSERT_EQUAL(STATUS_CODE_INVALID_ARGS, pwm_set_dc(PWM_TIMER_3, 101));
-  TEST_ASSERT_EQUAL(STATUS_CODE_INVALID_ARGS, pwm_set_pulse(PWM_TIMER_3, PERIOD_MS + 1));
+  TEST_ASSERT_EQUAL(STATUS_CODE_UNINITIALIZED, pwm_set_pulse(PWM_TIMER, DUTY_CYCLE));
+  TEST_ASSERT_EQUAL(STATUS_CODE_INVALID_ARGS, pwm_init(PWM_TIMER, 0));
+  TEST_ASSERT_OK(pwm_init(PWM_TIMER, PERIOD_MS));
+  TEST_ASSERT_EQUAL(STATUS_CODE_INVALID_ARGS, pwm_set_dc(PWM_TIMER, 101));
+  TEST_ASSERT_EQUAL(STATUS_CODE_INVALID_ARGS, pwm_set_pulse(PWM_TIMER, PERIOD_MS + 1));
   TEST_ASSERT_EQUAL(STATUS_CODE_INVALID_ARGS, pwm_init(NUM_PWM_TIMERS, PERIOD_MS));
 }
 
 void test_pwm_output(void) {
-  TEST_ASSERT_OK(pwm_init(PWM_TIMER_3, PERIOD_MS));
-  TEST_ASSERT_EQUAL(PERIOD_MS, pwm_get_period(PWM_TIMER_3));
-  TEST_ASSERT_OK(pwm_set_pulse(PWM_TIMER_3, PERIOD_MS - 1));
-  TEST_ASSERT_OK(pwm_set_dc(PWM_TIMER_3, 50));
+  TEST_ASSERT_OK(pwm_init(PWM_TIMER, PERIOD_MS));
+  TEST_ASSERT_EQUAL(PERIOD_MS, pwm_get_period(PWM_TIMER));
+  TEST_ASSERT_OK(pwm_set_pulse(PWM_TIMER, PERIOD_MS - 1));
+  TEST_ASSERT_OK(pwm_set_dc(PWM_TIMER, DUTY_CYCLE));
 
-  const GPIOAddress addr = {
-    .port = GPIO_PORT_A,  //
-    .pin = 6,             //
-  };
+  const GPIOAddress addr = PWM_TEST_ADDR;
   const GPIOSettings settings = {
     .direction = GPIO_DIR_OUT,
     .state = GPIO_STATE_HIGH,
     .resistor = GPIO_RES_PULLUP,
-    .alt_function = GPIO_ALTFN_1,
+    .alt_function = GPIO_ALTFN_4,
   };
   const InterruptSettings it_settings = {
     .type = INTERRUPT_TYPE_INTERRUPT,       //
     .priority = INTERRUPT_PRIORITY_NORMAL,  //
   };
   TEST_ASSERT_OK(gpio_init_pin(&addr, &settings));
-  TEST_ASSERT_OK(gpio_it_register_interrupt(&addr, &it_settings, INTERRUPT_EDGE_RISING_FALLING,
-                                            prv_pwm_test, NULL));
-  while (s_counter < WAIT_CYCLES) {
-  }
-
-  TEST_ASSERT_TRUE(s_counter >= WAIT_CYCLES);
+  TEST_ASSERT_OK(
+      gpio_it_register_interrupt(&addr, &it_settings, INTERRUPT_EDGE_RISING, prv_pwm_test, NULL));
+  delay_ms((EXPECTED_EDGES + 1) * PERIOD_MS);
+  TEST_ASSERT_TRUE(s_counter >= EXPECTED_EDGES);
 }
