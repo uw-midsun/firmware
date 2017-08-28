284b5a9fb0dc47a91a561896fb241a175148b0ff
ckitagawa <calderk2010@gmail.com>
2017-08-28 16:07:47 -0400

    ELEC-268: Move generic code to libcore

--- libraries/ms-common/inc/hal_test_helpers.h
+++ libraries/ms-common/inc/hal_test_helpers.h
@@ -3,13 +3,6 @@
 
 #include <stdint.h>
 
-#include "status.h"
-#include "unity.h"
-
-// General use:
-#define TEST_ASSERT_OK(code) TEST_ASSERT_EQUAL(STATUS_CODE_OK, (code))
-#define TEST_ASSERT_NOT_OK(code) TEST_ASSERT_NOT_EQUAL(STATUS_CODE_OK, (code))
-
 // For testing soft_timer.h:
 // TEST ONLY FUNCTION TO SET TIMER COUNTER FOR SOFT TIMERS UNSAFE TO CALL OUTSIDE A TEST.
 void _test_soft_timer_set_counter(uint32_t counter_value);
--- libraries/ms-common/src/x86/hal_test_helpers.c
+++ libraries/ms-common/src/x86/hal_test_helpers.c
@@ -1,3 +1,5 @@
+#include "hal_test_helpers.h"
+
 #include <stdint.h>
 
 void _test_soft_timer_set_counter(uint32_t counter_value) {
