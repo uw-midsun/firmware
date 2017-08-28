5bc0181e145af3e4ea6b8e6550f0972566f63a0b
Titus Chow <tituschow96@gmail.com>
2017-08-28 13:35:46 -0700

    ELEC-203: Missed merge change

--- libraries/ms-common/test/test_gpio.c
+++ libraries/ms-common/test/test_gpio.c
@@ -173,7 +173,7 @@ void test_gpio_set_state_invalid_state(void) {
   TEST_ASSERT_OK(gpio_init());
   // A port that should be valid on all configurations.
   GPIOAddress address = {.port = VALID_PORT, .pin = VALID_PIN };
-  TEST_ASSERT_EQUAL(STATUS_CODE_INVALID_ARGS, gpio_set_state(&address, NUM_GPIO_STATE));
+  TEST_ASSERT_EQUAL(STATUS_CODE_INVALID_ARGS, gpio_set_state(&address, NUM_GPIO_STATES));
   TEST_ASSERT_EQUAL(STATUS_CODE_INVALID_ARGS, gpio_set_state(&address, -1));
 }
 
