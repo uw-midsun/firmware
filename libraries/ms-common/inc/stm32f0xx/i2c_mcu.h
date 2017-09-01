e7ea6bcb46dcbfe2afa36b2144515464c5eceb00
ckitagawa <calderk2010@gmail.com>
2017-09-01 15:57:44 -0400

    ELEC-275: Add altfn definitions

--- libraries/ms-common/inc/stm32f0xx/i2c_mcu.h
+++ libraries/ms-common/inc/stm32f0xx/i2c_mcu.h
@@ -1,7 +1,13 @@
 #pragma once
 
+#include "gpio.h"
+
 typedef enum {
   I2C_PORT_1 = 0,
   I2C_PORT_2,
   NUM_I2C_PORTS,
 } I2CPort;
+
+#define I2C_PORT_1_GPIO_ALTFN GPIO_ALTFN_1
+#define I2C_PORT_2_PB_10_11_GPIO_ALTFN GPIO_ALTFN_1
+#define I2C_PORT_2_PB_13_14_GPIO_ALTFN GPIO_ALTFN_5
