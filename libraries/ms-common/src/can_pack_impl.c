25b85d3bdd1ac5f1f6c4e7d5f72724e5abe339e8
ckitagawa <calderk2010@gmail.com>
2017-09-02 12:45:44 -0400

    ELEC-276: Add size field to can_pack_impl

--- libraries/ms-common/src/can_pack_impl.c
+++ libraries/ms-common/src/can_pack_impl.c
@@ -1,53 +1,66 @@
 #include "can_pack_impl.h"
 
-#include <limits.h>
 #include <stdint.h>
 
 #include "can_msg.h"
 
-// Calculate the number of used bytes if the trailing bytes are 0 or unused then there is no point
-// wasting bus time transmitting them.
-#define CAN_PACK_CALC_DLC(data) CHAR_BIT - (size_t)__builtin_ffsll((int64_t)(data)) / CHAR_BIT
-
-void can_pack_impl_u8(CANMessage *msg, uint16_t source_id, CANMessageID id, uint8_t f1, uint8_t f2,
-                      uint8_t f3, uint8_t f4, uint8_t f5, uint8_t f6, uint8_t f7, uint8_t f8) {
+StatusCode can_pack_impl_u8(CANMessage *msg, uint16_t source_id, CANMessageID id, size_t num_bytes,
+                            uint8_t f1, uint8_t f2, uint8_t f3, uint8_t f4, uint8_t f5, uint8_t f6,
+                            uint8_t f7, uint8_t f8) {
+  if (num_bytes > CAN_PACK_IMPL_MAX_DLC) {
+    return status_msg(STATUS_CODE_INVALID_ARGS, "DLC too large");
+  }
   *msg = (CANMessage){
     .type = CAN_MSG_TYPE_DATA,                      //
     .source_id = source_id,                         //
     .msg_id = id,                                   //
     .data_u8 = { f1, f2, f3, f4, f5, f6, f7, f8 },  //
+    .dlc = num_bytes,                               //
   };
-  msg->dlc = CAN_PACK_CALC_DLC(msg->data);
+  return STATUS_CODE_OK;
 }
 
-void can_pack_impl_u16(CANMessage *msg, uint16_t source_id, CANMessageID id, uint16_t f1,
-                       uint16_t f2, uint16_t f3, uint16_t f4) {
+StatusCode can_pack_impl_u16(CANMessage *msg, uint16_t source_id, CANMessageID id, size_t num_bytes,
+                             uint16_t f1, uint16_t f2, uint16_t f3, uint16_t f4) {
+  if (num_bytes > CAN_PACK_IMPL_MAX_DLC) {
+    return status_msg(STATUS_CODE_INVALID_ARGS, "DLC too large");
+  }
   *msg = (CANMessage){
     .type = CAN_MSG_TYPE_DATA,       //
     .source_id = source_id,          //
     .msg_id = id,                    //
     .data_u16 = { f1, f2, f3, f4 },  //
+    .dlc = num_bytes,                //
   };
-  msg->dlc = CAN_PACK_CALC_DLC(msg->data);
+  return STATUS_CODE_OK;
 }
 
-void can_pack_impl_u32(CANMessage *msg, uint16_t source_id, CANMessageID id, uint32_t f1,
-                       uint32_t f2) {
+StatusCode can_pack_impl_u32(CANMessage *msg, uint16_t source_id, CANMessageID id, size_t num_bytes,
+                             uint32_t f1, uint32_t f2) {
+  if (num_bytes > CAN_PACK_IMPL_MAX_DLC) {
+    return status_msg(STATUS_CODE_INVALID_ARGS, "DLC too large");
+  }
   *msg = (CANMessage){
     .type = CAN_MSG_TYPE_DATA,  //
     .source_id = source_id,     //
     .msg_id = id,               //
     .data_u32 = { f1, f2 },     //
+    .dlc = num_bytes,           //
   };
-  msg->dlc = CAN_PACK_CALC_DLC(msg->data);
+  return STATUS_CODE_OK;
 }
 
-void can_pack_impl_u64(CANMessage *msg, uint16_t source_id, CANMessageID id, uint64_t f1) {
+StatusCode can_pack_impl_u64(CANMessage *msg, uint16_t source_id, CANMessageID id, size_t num_bytes,
+                             uint64_t f1) {
+  if (num_bytes > CAN_PACK_IMPL_MAX_DLC) {
+    return status_msg(STATUS_CODE_INVALID_ARGS, "DLC too large");
+  }
   *msg = (CANMessage){
     .type = CAN_MSG_TYPE_DATA,  //
     .source_id = source_id,     //
     .msg_id = id,               //
     .data = f1,                 //
+    .dlc = num_bytes,           //
   };
-  msg->dlc = CAN_PACK_CALC_DLC(msg->data);
+  return STATUS_CODE_OK;
 }
