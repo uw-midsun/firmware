ed4cc97a280ecffc06772e283b11652a3de68008
Titus Chow <tituschow@users.noreply.github.com>
2017-09-02 16:05:34 -0700

    ELEC-280: CAN ACK device bitset (#90)
    Switched to expected device bitset instead of num_expected for CAN ACK requests. Use CAN_ACK_EXPECTED_DEVICES(...) to build the bitset.

--- libraries/libcore/inc/misc.h
+++ libraries/libcore/inc/misc.h
@@ -2,6 +2,7 @@
 // Common helper macros
 
 #define SIZEOF_ARRAY(arr) (sizeof((arr)) / sizeof((arr)[0]))
+#define SIZEOF_FIELD(type, field) (sizeof(((type *)0)->field))
 // Casts void * to uint8_t *
 #define VOID_PTR_UINT8(x) (_Generic((x), void * : (uint8_t *)(x), default : (x)))
 
