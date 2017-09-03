ba8fa7195b8c7d01f7284d08e32c336468258782
Titus Chow <tituschow96@gmail.com>
2017-09-02 19:28:27 -0700

    ELEC-218: Updated python script, added DLC, echo back on TX

--- projects/telemetry/scripts/interface.py
+++ projects/telemetry/scripts/interface.py
@@ -1,31 +1,61 @@
 #!/usr/bin/env python3
-import serial
+"""
+Test RX interface for telemetry's semi-binary protocol
+"""
 import struct
-import ctypes
+import serial
 
-class CANIdBits(ctypes.LittleEndianStructure):
-  _fields_ = [
-    ('source_id', ctypes.c_uint16, 4),
-    ('type', ctypes.c_uint16, 1),
-    ('msg_id', ctypes.c_uint16, 6)
-  ]
+# Struct format code:
+# * Little endian
+# * 3 characters (RX[)
+# * uint16_t (id)
+# * uint8_t (dlc)
+# * uint64_t (data)
+# * 2 characters (]\n)
+# See https://docs.python.org/3/library/struct.html#format-characters
+MSG_FORMAT = '<3cHBQ2c'
 
-class CANId(ctypes.Union):
-  _fields_ = [
-    ('b', CANIdBits),
-    ('as_u16', ctypes.c_uint16)
-  ]
+def extract_can_id(raw_id):
+    """ Parses a raw CAN ID for our message format.
 
-MSG_FORMAT = '<3cHQcc'
+    See can_msg.h for the full message format.
 
-with serial.Serial('/dev/ttyACM0', 115200) as ser:
-  while 1:
-    line = ser.readline()
-    try:
-      raw_id, data = struct.unpack(MSG_FORMAT, line)[3:5]
-      can_id = CANId()
-      can_id.as_u16 = raw_id
+    Args:
+        raw_id: 11-bit integer
 
-      print('RX {0.b.msg_id} from {0.b.source_id} ({0.b.type}) - 0x{1:08x}'.format(can_id, data))
-    except struct.error:
-      pass
+    Returns:
+        Dictionary: keys will correspond to the CAN ID fields.
+    """
+    id_fmt = [('source_id', 4), ('type', 1), ('msg_id', 6)]
+
+    ret = {}
+
+    pos = 0
+    for name, size in id_fmt:
+        mask = ((1 << size) - 1) << pos
+        ret[name] = (mask & raw_id) >> pos
+        pos += size
+
+    return ret
+
+def main():
+    """ Main event loop """
+    with serial.Serial('/dev/ttyACM1', 115200) as ser:
+        while 1:
+            line = ser.readline()
+            try:
+                raw_id, dlc, data = struct.unpack(MSG_FORMAT, line)[3:6]
+                can_id = extract_can_id(raw_id)
+
+                # pylint: disable=invalid-format-index
+                print('RX {0[msg_id]} from {0[source_id]} ({0[type]})'
+                      ' - {1} bytes: 0x{2:08x}'.format(can_id, dlc, data))
+            except struct.error:
+                print(line)
+            except KeyboardInterrupt:
+                break
+
+
+
+if __name__ == '__main__':
+    main()
--- projects/telemetry/src/translate.c
+++ projects/telemetry/src/translate.c
@@ -2,11 +2,11 @@
 #include <inttypes.h>
 #include <stdio.h>
 #include <string.h>
+#include "log.h"
 
 static StatusCode prv_can_rx(const CANMessage *msg, void *context, CANAckStatus *ack_reply) {
-  // Note that this currently responds to ACKs
   UARTPort uart = (UARTPort)context;
-  uint8_t buffer[] = "RX[\0\0\0\0\0\0\0\0\0\0]\n";
+  uint8_t buffer[] = "RX[\0\0\0\0\0\0\0\0\0\0\0]\n";
 
   CANId id = {
     .source_id = msg->source_id,  //
@@ -15,25 +15,35 @@ static StatusCode prv_can_rx(const CANMessage *msg, void *context, CANAckStatus 
   };
 
   memcpy(&buffer[3], &id.raw, sizeof(id.raw));
-  memcpy(&buffer[5], &msg->data, sizeof(msg->data));
+  memcpy(&buffer[5], &msg->dlc, 1);
+  memcpy(&buffer[6], &msg->data, sizeof(msg->data));
 
   uart_tx(uart, buffer, sizeof(buffer) - 1);
 
   return STATUS_CODE_OK;
 }
 
+// Note that this function has not been tested.
 void prv_uart_rx(const uint8_t *rx_arr, size_t len, void *context) {
+  UARTPort uart = (UARTPort)context;
+
   CANMessage msg = {.type = CAN_MSG_TYPE_DATA };
-  if (len == 14 && rx_arr[0] == 'T' && rx_arr[1] == 'X' && rx_arr[2] == '[' && rx_arr[13] == ']') {
+  if (len == 15 && rx_arr[0] == 'T' && rx_arr[1] == 'X' && rx_arr[2] == '[' && rx_arr[14] == ']') {
     memcpy(&msg.msg_id, &rx_arr[3], sizeof(msg.msg_id));
-    memcpy(&msg.data, &rx_arr[5], sizeof(msg.data));
-    can_transmit(&msg, NULL);
+    memcpy(&msg.dlc, &rx_arr[5], sizeof(msg.data));
+    memcpy(&msg.data, &rx_arr[6], sizeof(msg.data));
+    StatusCode ret = can_transmit(&msg, NULL);
+
+    uint8_t buffer[] = "TX ret \0\n";
+    memcpy(&buffer[7], &ret, 1);
+    uart_tx(uart, buffer, sizeof(buffer) - 1);
+    LOG_DEBUG("Requested CAN TX - msg %d: %d\n", msg.msg_id, ret);
   }
 }
 
 StatusCode translate_init(UARTPort uart) {
   can_register_rx_default_handler(prv_can_rx, (void *)uart);
-  uart_set_rx_handler(uart, prv_uart_rx, NULL);
+  uart_set_rx_handler(uart, prv_uart_rx, (void *)uart);
 
   return STATUS_CODE_OK;
 }
