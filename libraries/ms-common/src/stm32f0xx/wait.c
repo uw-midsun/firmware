5b017f947e106e1b1be71bc46a08efaa6b50b14f
ckitagawa <calderk2010@gmail.com>
2017-09-21 15:45:27 -0400

    ELEC-288: fix race condition in wait.h for the stm32f0xx

--- libraries/ms-common/src/stm32f0xx/wait.c
+++ libraries/ms-common/src/stm32f0xx/wait.c
@@ -1,7 +1,12 @@
 #include "wait.h"
 
+#include <stdbool.h>
+
+#include "critical_section.h"
 #include "stm32f0xx.h"
 
 void wait(void) {
+  bool disabled = critical_section_start();
   __WFI();
+  critical_section_end(disabled);
 }
