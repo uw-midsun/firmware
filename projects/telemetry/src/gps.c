8a4e356706f0b4caf3ef70a7bbdf37d8b5b7627f
Neil Parikh <parikh.neil@me.com>
2017-10-04 21:39:09 -0400

    some more work

--- projects/telemetry/src/gps.c
+++ projects/telemetry/src/gps.c
@@ -1,29 +1,38 @@
 #include "status.h"
 #include "gps.h"
 #include "gpio.h"
+#include "uart_mcu.h"
+
 // This is the address used to send messages to the gps chip
-static GPIOAddress s_tx = {
+static GPIOAddress s_tx_address = {
   .port = 5;//
   .pin = 1;//Todo: update numbers
 };
 
+static const UARTPort port = UART_PORT_1;
+
 // This is the address used to recieve messages to the gps chip
-static GPIOAddress s_rx = {
+static GPIOAddress s_rx_address = {
   .port = 4;//
   .pin = 1;//Todo: update numbers
 };
-static UARTSettings s_read_from_chip = {
+
+static void s_nmea_read(const uint8_t *rx_arr, size_t len, void *context){
+
+}
+
+static UARTSettings s_settings = {
   .baudrate = 9600,//
   .rx_handler = s_nmea_read,
-  .tx = s_tx,
-  .rx = s_rx,
+
+  .tx = s_tx_address,
+  .rx = s_rx_address,
   .alt_fn = GPIO_ALTFN_NONE,
 };
-static void s_nmea_read(const uint8_t *rx_arr, size_t len, void *context){
-  
-}
+
+static UARTStorage s_storage = { 0 };
+
 StatusCode evm_gps_init(void) {
+  uart_init(port, s_settings, s_storage)
   return STATUS_CODE_UNIMPLEMENTED;
-  // uart_init(UARTPort uart, s_read_from_chip, UARTStorage *storage)
 }
-// uart_tx(UARTPort uart, uint8_t *tx_data, size_t len);
