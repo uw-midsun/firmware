79b0ee038db6ad048a51c2a991b1419246adf88d
Neil Parikh <parikh.neil@me.com>
2017-10-07 14:32:31 -0400

    add rules.mk & main.c and add some testing code to the gps driver

--- projects/telemetry/src/gps.c
+++ projects/telemetry/src/gps.c
@@ -1,26 +1,34 @@
+#include <stdio.h>
 #include "status.h"
 #include "gps.h"
 #include "gpio.h"
 #include "uart_mcu.h"
+#include "misc.h"
 
-static const UARTPort port = UART_PORT_1;
+static const UARTPort port = UART_PORT_2;
 
-static void s_nmea_read(const uint8_t *rx_arr, size_t len, void *context){
-
+static void s_nmea_read(const uint8_t *rx_arr, size_t len, void *context) {
+  printf("recieved data with len %d\n", len);
+  for (uint8_t i = 0; i < len; i++) {
+    printf("byte %d: %d\n", i, rx_arr[i]);
+  }
 }
 
+// FIXME: move this to main.c and pass it in as a arg to evm_gps_init
 static UARTSettings s_settings = {
-  .baudrate = 9600,//
+  .baudrate = 9600,
   .rx_handler = s_nmea_read,
 
-  .tx = { .port = GPIO_PORT_A , .pin = 1 },
-  .rx = { .port = GPIO_PORT_A , .pin = 1 },
-  .alt_fn = GPIO_ALTFN_NONE,
+  .tx = { .port = GPIO_PORT_A , .pin = 2 },
+  .rx = { .port = GPIO_PORT_A , .pin = 3 },
+  .alt_fn = GPIO_ALTFN_1,
 };
 
 static UARTStorage s_storage = { 0 };
 
 StatusCode evm_gps_init(void) {
-  uart_init(port, s_settings, s_storage)
-  return status_code(STATUS_CODE_UNIMPLEMENTED);
+  uart_init(port, &s_settings, &s_storage);
+  uint8_t data[2] = { 42, 24 };
+  uart_tx(port, data, SIZEOF_ARRAY(data));
+  return STATUS_CODE_OK;
 }
---
+++ projects/telemetry/src/main.c
@@ -0,0 +1,42 @@
+// Example program for STM32F072 Controller board or Discovery Board.
+// Blinks the LEDs sequentially.
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "gpio.h"        // General Purpose I/O control.
+#include "soft_timer.h"  // Software timers for scheduling future events.
+#include "interrupt.h"   // For enabling interrupts.
+#include "gps.h"
+
+// Controller board LEDs
+static const GPIOAddress pins[] = {
+  {.port = GPIO_PORT_A, .pin = 2 },   //
+  {.port = GPIO_PORT_A, .pin = 3 },   //
+};
+
+int main(void) {
+  // Enable various peripherals
+  interrupt_init();
+  soft_timer_init();
+  gpio_init();
+
+  GPIOSettings settings_tx = {
+    .direction = GPIO_DIR_OUT,        // The pin needs to output.
+    .state = GPIO_STATE_HIGH,         // Start in the "on" state.
+    .alt_function = GPIO_ALTFN_1,  // No connections to peripherals.
+    .resistor = GPIO_RES_NONE,        // No need of a resistor to modify floating logic levels.
+  };
+
+  GPIOSettings settings_rx = {
+    .direction = GPIO_DIR_IN,        // The pin needs to output.
+    .state = GPIO_STATE_HIGH,         // Start in the "on" state.
+    .alt_function = GPIO_ALTFN_1,  // No connections to peripherals.
+    .resistor = GPIO_RES_NONE,        // No need of a resistor to modify floating logic levels.
+  };
+
+  gpio_init_pin(&pins[0], &settings_tx);
+  gpio_init_pin(&pins[1], &settings_rx);
+
+  return 0;
+}
