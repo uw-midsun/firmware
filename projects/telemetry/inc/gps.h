b160711bd798088a5dcb3ec460ae1b17132bdf3e
Misha Zharov <mishazharov1@gmail.com>
2017-10-08 00:38:13 -0400

    Started working on parsing data
    NMEA sentence ID's are now parsed. Also git is doing some funny stuff
    with line endings, hoping this is not commited with CR LF.

--- projects/telemetry/inc/gps.h
+++ projects/telemetry/inc/gps.h
@@ -4,4 +4,22 @@
 #include "status.h"
 #include "uart.h"
 
+// This enum contains the list of supported NMEA sentences that are supported by our GPS chip
+typedef enum {
+  GGA = 0,
+  GLL = 1,
+  GSA = 2,
+  GSV = 3,
+  RMC = 4,
+  VTG = 5,
+  NUM_MESSAGE_IDS = 6,
+} NMEAMessageID;
+
+// Info passed from the GPS chip should be dropped into this struct (more fields coming soon)
+typedef struct {
+  NMEAMessageID message_id;
+} NMEASentence;
+
+// TODO, make this private and add methods to read from this
+NMEASentence result;
 StatusCode evm_gps_init(void);
--- projects/telemetry/src/gps.c
+++ projects/telemetry/src/gps.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "status.h"
 #include "gps.h"
 #include "gpio.h"
@@ -6,12 +7,47 @@
 #include "misc.h"
 
 static const UARTPort port = UART_PORT_2;
-
 static void s_nmea_read(const uint8_t *rx_arr, size_t len, void *context) {
-  printf("recieved data with len %d\n", len);
+  printf("recieved data with len %zu\n", len);
   for (uint8_t i = 0; i < len; i++) {
     printf("byte %d: %d\n", i, rx_arr[i]);
   }
+  
+  // Should use context somewhere here to make sure the message is for us
+  
+  if(len < 1 && rx_arr[0] != '$'){
+    printf("Something is fishy, first character of a NMEA sentence should be a $, it is a %c", rx_arr[0]);
+  }
+  if(len < 3 && (rx_arr[1] != 'G' || rx_arr[2] != 'P')){
+    printf("Something is fishy, the second and third characters of a NMEA sentence should be a GP. They are %c%c", rx_arr[1], rx_arr[2]);
+  }
+  // Array index 3 should be 0
+  uint8_t message_id [4] = {0};
+  for(int i = 3; i < 6; i++){
+    if(rx_arr[i] == ','){
+     break; 
+    }
+    message_id[i - 3] = rx_arr[i];
+  }
+  
+  //Making sure array index 3 is \n
+  message_id[3] = '\n';
+  
+  //Parsing which type of NMEA message this is
+  if(strcmp((char *)message_id, "GGA") == 0){
+    result.message_id = GGA;
+  }else if(strcmp((char *)message_id, "GLL") == 0){
+    result.message_id = GLL;
+  }else if(strcmp((char *)message_id, "GSA") == 0){
+    result.message_id = GSA;
+  }else if(strcmp((char *)message_id, "GSV") == 0){
+    result.message_id = GSV;
+  }else if(strcmp((char *)message_id, "RMC") == 0){
+    result.message_id = RMC;
+  }else if(strcmp((char *)message_id, "VTG") == 0){
+    result.message_id = VTG;
+  }
+  // Parse message_id here
 }
 
 // FIXME: move this to main.c and pass it in as a arg to evm_gps_init
@@ -27,8 +63,10 @@ static UARTSettings s_settings = {
 static UARTStorage s_storage = { 0 };
 
 StatusCode evm_gps_init(void) {
-  uart_init(port, &s_settings, &s_storage);
+  // Makes sure that status codes are handled
+  status_ok_or_return(uart_init(port, &s_settings, &s_storage));
+  
   uint8_t data[2] = { 42, 24 };
-  uart_tx(port, data, SIZEOF_ARRAY(data));
+  status_ok_or_return(uart_tx(port, data, SIZEOF_ARRAY(data)));
   return STATUS_CODE_OK;
 }
