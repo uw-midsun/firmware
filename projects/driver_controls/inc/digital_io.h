e63c785b0357016ffbce22312b1dd8a5d7d2e7b3
Fidel Musa <fxmusa@gmail.com>
2017-10-20 14:21:10 -0400

    ELEC-262: Driver Controls Cleanup (#76)
    * ELEC-261: Added -Wextra, fixed new warnings
    
    * ELEC-261: Actually output warnings, add -Wextra
    
    * ELEC-261: Addressed all errors on stm32 (untested)
    
    * ELEC-261: Fixed warnings on x86
    
    * ELEC-261: Removed padding, tested on x86
    
    * ELEC-261: Disable discarded qualifiers, fix FIFO NULL handling
    
    * ELEC-261: UART is volatile again - TODO determine if this is necessary
    
    * ELEC-262: Fixed error in prv_get_event
    
    * ELEC-262: Modified InputEventData for new message scheme
    
    * ELEC-262: Added common function to raise CAN events
    
    * ELEC-262: Added unit tests for new event contract
    
    * ELEC-262: Fixed for lint
    
    * ELEC-262: Fixed for lint
    
    * ELEC-262: Updated IO input init functions
    
    * ELEC-262: Removed commented event
    
    * ELEC-262: Removed CAN FSM from event arbiter
    
    * ELEC-262: CAN FSM no longer relies on the event queue to store events
    
    * ELEC-262: Removed obsolete function
    
    * ELEC-262: Fixed CAN FSM event reception
    
    * ELEC-262: Fixed for unit test
    
    * ELEC-262: Fixed merge errors
    
    * ELEC-262: Initialized state variables with enums
    
    * ELEC-262: Removed CAN FSM and renamed module
    
    * ELEC-262: Modified for new CAN module
    
    * ELEC-262: Forgot files
    
    * ELEC-262: Fixed incorrect header
    
    * ELEC-262: Style errors
    
    * ELEC-262: Fixed header
    
    * ELEC-262: Updated CAN unit tests
    
    * ELEC-262: Fixed unit test hardfaults
    
    * ELEC-262: Fixed for lint
    
    * ELEC-262: Renamed CAN enums
    
    * ELEC-262: Fixed naming and comment errors
    
    * ELEC-262: Added test for event filter output
    
    * ELEC-262: Altered for new unit test
    
    * ELEC-262: Removed EventData union
    
    * ELEC-262: CANOutputDevice enum name change
    
    * ELEC-262: Output function definition change
    
    * ELEC-262: Renamed file
    
    * ELEC-262: Updated unit tests
    
    * ELEC-262: Changed enum names
    
    * ELEC-262: Removed fsm parameter from output function
    
    * ELEC-262: Forgot to save merge changes
    
    * ELEC-262: Fixed for lint
    
    * ELEC-262: Added comments for FSM output data
    
    * ELEC-262: Fixed comment to match renamed enum
    
    * ELEC-262: Changed names for the last time
    
    * ELEC-262: Actually the last time this time
    
    * ELEC-262: Joined light-related CAN message ids into one message
    
    * ELEC-262: Added charge state to the power FSM
    
    * ELEC-262: Fixed errors with the power FSM
    
    * ELEC-262: Updated unit tests
    
    * ELEC-262: Reworked to prevent CAN message collisions
    
    * ELEC-262: Added comments
    
    * ELEC-262: Added comments documenting FSM flow
    
    * ELEC-262: Make format fix
    
    * ELEC-262: Documentation update

--- projects/driver_controls/inc/digital_io.h
+++ projects/driver_controls/inc/digital_io.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// Module for managing digital IO devices
+// Module for managing digital IO devices and events
 
 // Initializes digital pins
 void digital_io_init(void);
--- projects/driver_controls/inc/event_arbiter.h
+++ projects/driver_controls/inc/event_arbiter.h
@@ -13,11 +13,19 @@
 // Arbitrary FSM cap
 #define EVENT_ARBITER_MAX_FSMS 10
 
+typedef struct EventArbiterOutputData {
+  uint8_t id;
+  uint8_t state;
+  uint16_t data;
+} EventArbiterOutputData;
+
 // Typedef for the FSM arbitration functions
 typedef bool (*EventArbiterCheck)(const Event *e);
 
-// Initializes the event arbiter to the default state
-StatusCode event_arbiter_init(void);
+typedef void (*EventArbiterOutput)(EventArbiterOutputData data);
+
+// Initializes the event arbiter to the default state with a given output function
+StatusCode event_arbiter_init(EventArbiterOutput output);
 
 // Registers an FSM and sets the context pointer to point to the given event check function
 // NULL pointers are treated as no-ops that return true
@@ -25,3 +33,5 @@ EventArbiterCheck *event_arbiter_add_fsm(FSM *fsm, EventArbiterCheck default_che
 
 // Process an event if allowed in the current state
 bool event_arbiter_process_event(Event *e);
+
+StatusCode event_arbiter_output(EventArbiterOutputData data);
