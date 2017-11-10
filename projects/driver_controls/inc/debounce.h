98a07c1f4f2c67d4c72678a62ef95c1b07a21060
Fidel Musa <fxmusa@gmail.com>
2017-11-09 20:26:06 -0500

    ELEC-231: Added debounce module

--- projects/driver_controls/inc/debounce.h
+++ projects/driver_controls/inc/debounce.h
@@ -1,8 +1,9 @@
 #pragma once
-
+// Blocking debouncer for steering wheel interrupts
 // GPIO and soft timers must be initialized
 
 #include "gpio.h"
 #include "status.h"
 
-GPIOState debounce(GPIOAddress address, GPIOState* );
+// Polls the input until it is steady and return the value
+GPIOState debounce(GPIOAddress address);
--- projects/driver_controls/src/debounce.c
+++ projects/driver_controls/src/debounce.c
@@ -1,8 +1,10 @@
 #include "debounce.h"
+#include "soft_timer.h"
 
 #define CHECK_INTERVAL_MSEC 5
 #define STABLE_INTERVAL_MSEC 50
 
+static bool debounced;
 static uint8_t s_num_readings = 0;
 static GPIOState debounced_state = 0;
 
@@ -10,23 +12,32 @@ static void prv_debounce(SoftTimerID timer_id, void* context) {
   GPIOAddress* address = context;
   GPIOState current_state = GPIO_STATE_LOW;
 
-  gpio_get_state(address, current_state);
+  // Read the current pin value and compare to the previous input
+  gpio_get_state(address, &current_state);
 
   if (debounced_state == current_state) {
-    s_num_readings ++;
+    s_num_readings++;
+    
+    if (s_num_readings == STABLE_INTERVAL_MSEC) {
+      debounced = true;
+      return;
+    }
   } else {
     s_num_readings = 0;
     debounced_state = current_state;
   }
+
+  // Continue polling if the input has not been held steady for long enough
+  soft_timer_start(CHECK_INTERVAL_MSEC, prv_debounce, address, &timer_id);
+    
 }
 
 GPIOState debounce(GPIOAddress address) {
   SoftTimerID timer_id;
 
-  while (s_num_readings < (STABLE_INTERVAL_MSEC/CHECK_INTERVAL_MSEC)) {
-    soft_timer_start(CHECK_INTERVAL_MSEC, prv_debounce, &address, *timer_id);
-    while (soft_timer_inuse()) {}
-  }
+  soft_timer_start(CHECK_INTERVAL_MSEC, prv_debounce, &address, &timer_id);
+
+  while (!debounced) { }
 
   return debounced_state;
 }
