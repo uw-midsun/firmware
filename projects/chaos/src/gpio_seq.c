a8edde08fda811966e7bc24ea45998116d8a02f4
ckitagawa <calderk2010@gmail.com>
2018-01-06 12:04:54 -0500

    ELEC-105: fixed typo

--- projects/chaos/src/gpio_seq.c
+++ projects/chaos/src/gpio_seq.c
@@ -9,7 +9,7 @@
 
 StatusCode gpio_seq_init_pins(const GPIOAddress *addrs, size_t num_addrs,
                               const GPIOSettings *settings, uint32_t delay_time_us) {
-  for (size_t i = 0; i < n_addrs; i++) {
+  for (size_t i = 0; i < num_addrs; i++) {
     status_ok_or_return(gpio_init_pin(&addrs[i], settings));
     if (delay_time_us) {
       delay_us(delay_time_us);
@@ -20,7 +20,7 @@ StatusCode gpio_seq_init_pins(const GPIOAddress *addrs, size_t num_addrs,
 
 StatusCode gpio_seq_set_state(const GPIOAddress *addrs, size_t num_addrs, GPIOState state,
                               uint32_t delay_time_us) {
-  for (size_t i = 0; i < n_addrs; i++) {
+  for (size_t i = 0; i < num_addrs; i++) {
     status_ok_or_return(gpio_set_state(&addrs[i], state));
     if (delay_time_us) {
       delay_us(delay_time_us);
