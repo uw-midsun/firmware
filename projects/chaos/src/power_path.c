aae8d4008e40ab86061921a9ecf83393749bf5de
Calder Kitagawa <calderk2010@gmail.com>
2018-01-09 20:13:05 -0500

    ELEC-105: Chaos Power Path implementation (#108)

---
+++ projects/chaos/src/power_path.c
@@ -0,0 +1,134 @@
+#include "power_path.h"
+
+#include <stdbool.h>
+#include <string.h>
+
+#include "adc.h"
+#include "chaos_events.h"
+#include "event_queue.h"
+#include "gpio.h"
+#include "gpio_it.h"
+#include "interrupt.h"
+#include "soft_timer.h"
+#include "status.h"
+
+// Evaluates if two GPIO addresses are equal.
+static bool prv_addr_eq(GPIOAddress addr1, GPIOAddress addr2) {
+  return ((addr1.port == addr2.port) && (addr1.pin == addr2.pin));
+}
+
+// Interrupt handler for over and under voltage warnings.
+static void prv_voltage_warning(const GPIOAddress *addr, void *context) {
+  PowerPathCfg *pp = context;
+  if (prv_addr_eq(*addr, pp->dcdc.uv_ov_pin) && pp->dcdc.monitoring_active) {
+    event_raise(CHAOS_EVENT_CAN_UV_OV, POWER_PATH_SOURCE_ID_DCDC);
+  } else if (prv_addr_eq(*addr, pp->aux_bat.uv_ov_pin) && pp->aux_bat.monitoring_active) {
+    event_raise(CHAOS_EVENT_CAN_UV_OV, POWER_PATH_SOURCE_ID_AUX_BAT);
+  }
+}
+
+static void prv_adc_read(SoftTimerID timer_id, void *context) {
+  PowerPathSource *pps = context;
+  if (pps->timer_id != timer_id || !pps->monitoring_active) {
+    // Guard against accidentally starting multiple timers to monitor the same source concurrently.
+    return;
+  }
+
+  uint16_t value = 0;
+  ADCChannel chan = NUM_ADC_CHANNELS;
+  // Read and convert the current values.
+  adc_get_channel(pps->current_pin, &chan);
+  adc_read_raw(chan, &value);
+  pps->readings.current = pps->current_convert_fn(value);
+
+  // Read and convert the voltage values.
+  adc_get_channel(pps->voltage_pin, &chan);
+  adc_read_raw(chan, &value);
+  pps->readings.voltage = pps->voltage_convert_fn(value);
+
+  // Start the next timer.
+  soft_timer_start(pps->period_us, prv_adc_read, pps, &pps->timer_id);
+}
+
+StatusCode power_path_init(PowerPathCfg *pp) {
+  GPIOSettings settings = {
+    .direction = GPIO_DIR_OUT,
+    .state = GPIO_STATE_HIGH,
+    .resistor = GPIO_RES_NONE,
+    .alt_function = GPIO_ALTFN_NONE,
+  };
+
+  status_ok_or_return(gpio_init_pin(&pp->enable_pin, &settings));
+  status_ok_or_return(gpio_init_pin(&pp->shutdown_pin, &settings));
+
+  // Configure the Interrupt Pins
+  settings.direction = GPIO_DIR_IN;
+  settings.state = GPIO_STATE_LOW;
+  status_ok_or_return(gpio_init_pin(&pp->aux_bat.uv_ov_pin, &settings));
+  status_ok_or_return(gpio_init_pin(&pp->dcdc.uv_ov_pin, &settings));
+
+  // Register interrupts to the same function.
+  const InterruptSettings it_settings = {
+    .type = INTERRUPT_TYPE_INTERRUPT,
+    .priority = INTERRUPT_PRIORITY_NORMAL,
+  };
+  status_ok_or_return(gpio_it_register_interrupt(&pp->aux_bat.uv_ov_pin, &it_settings,
+                                                 INTERRUPT_EDGE_RISING, prv_voltage_warning, pp));
+  status_ok_or_return(gpio_it_register_interrupt(&pp->dcdc.uv_ov_pin, &it_settings,
+                                                 INTERRUPT_EDGE_RISING, prv_voltage_warning, pp));
+
+  // Configure the ADC Pins
+  settings.alt_function = GPIO_ALTFN_ANALOG;
+  status_ok_or_return(gpio_init_pin(&pp->aux_bat.voltage_pin, &settings));
+  status_ok_or_return(gpio_init_pin(&pp->aux_bat.current_pin, &settings));
+  status_ok_or_return(gpio_init_pin(&pp->dcdc.voltage_pin, &settings));
+  return gpio_init_pin(&pp->dcdc.current_pin, &settings);
+}
+
+StatusCode power_path_source_monitor_enable(PowerPathSource *source, uint32_t period_us) {
+  // Update the period.
+  source->period_us = period_us;
+
+  // Avoid doing anything else if monitoring
+  if (source->monitoring_active) {
+    return STATUS_CODE_OK;
+  }
+
+  // Set up adc current/voltage monitoring
+  ADCChannel chan = NUM_ADC_CHANNELS;
+  adc_get_channel(source->current_pin, &chan);
+  status_ok_or_return(adc_set_channel(chan, true));
+  adc_get_channel(source->voltage_pin, &chan);
+  status_ok_or_return(adc_set_channel(chan, true));
+
+  source->monitoring_active = true;
+
+  return soft_timer_start(source->period_us, prv_adc_read, source, &source->timer_id);
+}
+
+StatusCode power_path_source_monitor_disable(PowerPathSource *source) {
+  if (!source->monitoring_active) {
+    return STATUS_CODE_OK;
+  }
+
+  source->monitoring_active = false;
+  // Ignore this return since we don't care if the timer is already disabled.
+  soft_timer_cancel(source->timer_id);
+
+  // Disable the ADCs
+  ADCChannel chan = NUM_ADC_CHANNELS;
+  adc_get_channel(source->current_pin, &chan);
+  status_ok_or_return(adc_set_channel(chan, false));
+  adc_get_channel(source->voltage_pin, &chan);
+  return adc_set_channel(chan, false);
+}
+
+StatusCode power_path_read_source(const PowerPathSource *source, PowerPathVCReadings *values) {
+  if (!source->monitoring_active) {
+    // Fail if not actively monitoring as the data is not up to date.
+    return status_code(STATUS_CODE_UNINITIALIZED);
+  }
+
+  *values = source->readings;
+  return STATUS_CODE_OK;
+}
