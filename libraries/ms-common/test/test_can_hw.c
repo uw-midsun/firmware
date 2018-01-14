1a06dfda00a2c68ba242e6605dd466bbe711fb05
Titus Chow <tituschow96@gmail.com>
2018-01-14 01:09:30 -0500

    ELEC-324: Fixed extended filters wrong bit and filter still active

--- libraries/ms-common/test/test_can_hw.c
+++ libraries/ms-common/test/test_can_hw.c
@@ -1,5 +1,7 @@
 #include "can_hw.h"
 #include "interrupt.h"
+#include "soft_timer.h"
+#include "delay.h"
 #include "log.h"
 #include "test_helpers.h"
 #include "unity.h"
@@ -12,6 +14,7 @@ static volatile size_t s_rx_len;
 
 static void prv_handle_rx(void *context) {
   while (can_hw_receive(&s_rx_id, &s_extended, &s_rx_data, &s_rx_len)) {
+    LOG_DEBUG("RX msg %lx (extended %d) %d bytes\n", s_rx_id, s_extended, s_rx_len);
     s_msg_rx++;
   }
 }
@@ -25,6 +28,9 @@ static void prv_wait_rx(size_t wait_for) {
 
 void setup_test(void) {
   interrupt_init();
+
+  // Only used for delay - not needed for actual testing
+  soft_timer_init();
 
   CANHwSettings can_settings = {
     .bitrate = CAN_HW_BITRATE_125KBPS,
@@ -70,19 +76,23 @@ void test_can_hw_filter(void) {
   StatusCode ret = can_hw_transmit(0x3, false, 0, 0);
   TEST_ASSERT_OK(ret);
 
+  delay_ms(1);
+
   // 0b0101 - pass
   ret = can_hw_transmit(0x5, false, 0, 0);
   TEST_ASSERT_OK(ret);
+
+  delay_ms(1);
 
   // 0b00111001 - pass
   ret = can_hw_transmit(0x39, false, 0, 0);
   TEST_ASSERT_OK(ret);
 
+  delay_ms(1);
+
   // extended ID with std ID 0b1 - fail
   ret = can_hw_transmit(0x40000, true, 0, 0);
   TEST_ASSERT_OK(ret);
-
-  prv_wait_rx(2);
 
   TEST_ASSERT_EQUAL(2, s_msg_rx);
   TEST_ASSERT_EQUAL(false, s_extended);
@@ -112,14 +122,21 @@ void test_can_hw_extended_filter(void) {
   StatusCode ret = can_hw_transmit(0x1234547, true, 0, 0);
   TEST_ASSERT_OK(ret);
 
+  delay_ms(1);
+
   // No match standard - fail
   ret = can_hw_transmit(0x123, false, 0, 0);
   TEST_ASSERT_OK(ret);
+
+  delay_ms(1);
 
   // Partial invalid - fail
   ret = can_hw_transmit(0x0004567, true, 0, 0);
   TEST_ASSERT_OK(ret);
 
+  delay_ms(1);
+
+  // Full match - success
   ret = can_hw_transmit(0x1234567, true, 0, 0);
   TEST_ASSERT_OK(ret);
 
