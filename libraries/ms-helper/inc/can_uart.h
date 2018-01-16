bb3d1f55af130534b28e42d7e9d86ad6c64b5ba9
Titus Chow <tituschow96@gmail.com>
2018-01-16 15:05:22 -0500

    WIP saving work

--- libraries/ms-helper/inc/can_uart.h
+++ libraries/ms-helper/inc/can_uart.h
@@ -1,9 +1,11 @@
 #pragma once
+#include "uart.h"
+#include "can_hw.h"
 
 // Generic CAN HW <-> UART protocol
 // Requires CAN HW, UART to be initialized
 
-// Defined protocol: first u32
+// Defined protocol: Header (u32), ID (u32), Data (u64), End (u8)
 // Bits  | Field
 // ------|--------------------
 // 0:23  | marker (CTX || CRX)
@@ -13,7 +15,7 @@
 // 27    | parity
 // 28:31 | dlc
 
-typedef struct CanUart {
+typedef struct CanUartPacket {
   union {
     struct {
       uint32_t start:24;
@@ -28,4 +30,21 @@ typedef struct CanUart {
   uint32_t id;
   uint64_t data;
   uint8_t end;
+} CanUartPacket;
+
+typedef struct CanUart {
+  UARTPort uart;
+  CanUartRxCb rx_cb;
+  void *context;
+  bool hook_can_hw; // whether the module should hook directly into the CAN HW module
 } CanUart;
+
+typedef void (*CanUartRxCb)(const CanUart *can_uart, uint32_t id, bool extended,
+                            const uint8_t *data, size_t len, void *context);
+
+void can_uart_init(CanUart *can_uart);
+
+// Intended to request a TX on the receiver
+// i.e. from Master to Slave
+void can_uart_req_tx(const CanUart *can_uart, uint32_t id, bool extended,
+                     const uint8_t *data, size_t len);
