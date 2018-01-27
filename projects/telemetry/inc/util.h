07d263568c111a8a196f31d28a173322c757cada
Neil Parikh <parikh.neil@me.com>
2018-01-27 14:19:00 -0500

    ELEC-71: Clean up documentation for util methods

--- projects/telemetry/inc/util.h
+++ projects/telemetry/inc/util.h
@@ -2,12 +2,16 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-// Computes the checksum for the given message.
-// The method takes a NMEA message, it automatically stops at *
-// out must be a pointer to at least 3 chars
+// Computes the checksum for a given NMEA message.
+//
+// Starts at the second characer of `message`,
+// and continues until the `*` character.
+//
+// `out` must be a pointer to at least 3 `char`s.
 void evm_gps_compute_checksum(char *message, char *out);
 
-// computes the checksum for the message, and compares that against
-// the checksum in the message
-// returns false if the message does not have a checksum at the end
+// Computes the checksum for a given NMEA message, and compares the computed
+// checksum against the checksum in the message.
+//
+// Returns false if the given message does not have a checksum included.
 bool evm_gps_compare_checksum(char *message);
