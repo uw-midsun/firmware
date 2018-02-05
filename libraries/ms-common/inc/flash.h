d18207686f1579534fcb2943c5833f0b5c25884e
Titus Chow <tituschow96@gmail.com>
2018-02-05 17:25:32 +0000

    ELEC-333: Improved module documentation, fixed wrong page size

--- libraries/ms-common/inc/flash.h
+++ libraries/ms-common/inc/flash.h
@@ -1,19 +1,14 @@
 #pragma once
+// Raw Flash memory API
+// Warning: Offers no protection against overwriting program code
+//
+// Note that due to the way flash works, writes can only flip bits from 1 -> 0 and must be aligned
+// In order to switch bits from 0 -> 1, the entire page must be erased
 #include <stdint.h>
 #include <stddef.h>
 
 #include "status.h"
 #include "flash_mcu.h"
-// Flash memory API - note that this is a raw API.
-// Writes must be aligned
-// note that write can only flip 1 -> 0
-// erase flips entire page to 1
-// each page is 2 kB (stm32f072) - 05x = 1kB
-// each sector is 2 pages - always 4k total
-
-// main flash memory can be programmed 16 bits at a time, but requires PG bit set to do so
-// for ease of use, we'll just require u32s
-// flash must be erased - can be done page by page
 
 #define FLASH_ADDR_TO_PAGE(addr) (((uintptr_t)(addr) - (uintptr_t)FLASH_BASE_ADDR) / FLASH_PAGE_BYTES)
 #define FLASH_PAGE_TO_ADDR(page) ((uintptr_t)(page) * FLASH_PAGE_BYTES + (uintptr_t)FLASH_BASE_ADDR)
@@ -24,7 +19,10 @@ StatusCode flash_init(void);
 
 StatusCode flash_read(uintptr_t address, size_t read_bytes, uint8_t *buffer, size_t buffer_len);
 
+// Note that writes can only flip bits from 1 -> 0 - you must erase the page to switch from 0 -> 1.
+// address must be aligned to FLASH_WRITE_BYTES and
 // buffer_len must be a multiple of FLASH_WRITE_BYTES
 StatusCode flash_write(uintptr_t address, uint8_t *buffer, size_t buffer_len);
 
+// Flips the entire page to 1's
 StatusCode flash_erase(FlashPage page);
