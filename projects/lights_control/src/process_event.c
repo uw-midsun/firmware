4b5389244139ca958519e771a2a03790fe126807
Arshankhanifar <arshankhanifar@gmail.com>
2018-02-14 14:05:51 -0500

    cleanup

--- projects/lights_control/src/process_event.c
+++ projects/lights_control/src/process_event.c
@@ -1,36 +1,10 @@
-#include "status.h"
 #include "event_queue.h"
-#include "lights_events.h"
 #include "fsm.h"
-#include "process_periph.h"
-
+#include "lights_events.h"
+#include "lights_periph.h"
+#include "status.h"
 
 static FSM s_signal_fsm_front;
-
-// returns true if event's data is false (used for turning off a signal)
-// static bool s_turn_off_guard(const FSM *fsm, const Event *e, void *context) {
-//  return (bool) !e->data;
-//}
-//
-// static bool s_turn_on_guard(const FSM *fsm, const Event *e, void *context) {
-//  return (bool)e->data;
-//}
-
-// FSM_DECLARE_STATE(signal_left);
-// FSM_DECLARE_STATE(signal_right);
-// FSM_DECLARE_STATE(signal_hazard);
-// FSM_DECLARE_STATE(signal_none);
-
-// signal_left transition table
-// FSM_STATE_TRANSITION(signal_left) {
-//   FSM_ADD_GUARDED_TRANSITION(EVENT_SIGNAL_LEFT, s_turn_off_guard, signal_none);
-//   FSM_ADD_GUARDED_TRANSITION(EVENT_SIGNAL_HAZARD, s_turn_on_guard, signal_hazard);
-// }
-
-// FSM_STATE_TRANSITION(signal_right) {
-//   FSM_ADD_GUARDED_TRANSITION(EVENT_SIGNAL_RIGHT, s_turn_off_guard, signal_none);
-//   FSM_ADD_GUARDED_TRANSITION(EVENT_SIGNAL_HAZARD, s_turn_on_guard, signal_hazard);
-// }
 
 StatusCode process_event(Event e) {
   InputEvent event = e.id;
@@ -39,12 +13,10 @@ StatusCode process_event(Event e) {
     case EVENT_HEADLIGHTS:
     case EVENT_BRAKES:
     case EVENT_STROBE:
-      return simple_peripheral(e);
+      return lights_periph_simple(e);
       break;
     default:
       return STATUS_CODE_OK;
       break;
   }
 }
-
-
--- projects/lights_control/test/test_can_settings.c
+++ projects/lights_control/test/test_can_settings.c
@@ -1,20 +1,18 @@
+#include "can.h"
+#include "event_queue.h"
+#include "interrupt.h"
 #include "log.h"
 #include "status.h"
 #include "test_helpers.h"
-#include "event_queue.h"
-#include "interrupt.h"
 #include "unity.h"
-#include "can.h"
 
-#include <stdio.h>
-
-#include "can_settings.h"
-#include "structs.h"
+#include "can_setup.h"
 #include "lights_events.h"
+#include "structs.h"
 
 // test for initializing the CAN settings
 
-static CANMessageID s_msg_id = 0x1; 
+static CANMessageID s_msg_id = 0x1;
 
 void setup_test(void) {
   gpio_init();
@@ -26,21 +24,14 @@ void setup_test(void) {
 void teardown_test(void) {}
 
 void test_can_rx_front(void) {
-  initialize_can_settings(LIGHTS_BOARD_FRONT);
-  CANMessage msg = {
-    .msg_id = 0x1,              
-    .type = CAN_MSG_TYPE_DATA,
-    .data = 0x4,               
-    .dlc = 2
-  };
+  can_setup_init(LIGHTS_BOARD_FRONT);
+  CANMessage msg = { .msg_id = 0x1, .type = CAN_MSG_TYPE_DATA, .data = 0x4, .dlc = 2 };
   uint16_t test_messages[] = { 0x0, 0x101, 0x002, 0x103, 0x004 };
-  uint16_t assertion_values[][2] = {
-    { EVENT_SIGNAL_RIGHT, 0 },
-    { EVENT_SIGNAL_LEFT, 1 },
-    { EVENT_SIGNAL_HAZARD, 0 },
-    { EVENT_HORN, 1 },
-    { EVENT_HEADLIGHTS, 0 }
-  };
+  uint16_t assertion_values[][2] = { { EVENT_SIGNAL_RIGHT, 0 },
+                                     { EVENT_SIGNAL_LEFT, 1 },
+                                     { EVENT_SIGNAL_HAZARD, 0 },
+                                     { EVENT_HORN, 1 },
+                                     { EVENT_HEADLIGHTS, 0 } };
 
   for (uint8_t i = 0; i < SIZEOF_ARRAY(test_messages); i++) {
     msg.data = test_messages[i];
@@ -48,16 +39,11 @@ void test_can_rx_front(void) {
     Event e = { 0 };
     int ret;
     do {
-      if (e.id == EVENT_CAN_RX)
-        fsm_process_event(CAN_FSM, &e);
-      if (e.id == EVENT_CAN_TX)
-        fsm_process_event(CAN_FSM, &e);
+      if (e.id == EVENT_CAN_RX) fsm_process_event(CAN_FSM, &e);
+      if (e.id == EVENT_CAN_TX) fsm_process_event(CAN_FSM, &e);
       ret = event_process(&e);
-    } while (ret != STATUS_CODE_OK 
-                    || e.id == EVENT_CAN_RX || 
-                    e.id == EVENT_CAN_TX);
-    TEST_ASSERT_EQUAL(assertion_values[i][0], e.id); 
-    TEST_ASSERT_EQUAL(assertion_values[i][1], e.data); 
+    } while (ret != STATUS_CODE_OK || e.id == EVENT_CAN_RX || e.id == EVENT_CAN_TX);
+    TEST_ASSERT_EQUAL(assertion_values[i][0], e.id);
+    TEST_ASSERT_EQUAL(assertion_values[i][1], e.data);
   }
 }
-
