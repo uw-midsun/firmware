05332e52168246458cb4c133a71fe361262eb280
Ashkan Farmahin <a3farmah@edu.uwaterloo.ca>
2018-02-16 18:18:46 +0000

    ELEC-217: Took ads1015 related code out from main.c in driver_controls. It would not build in x86

--- projects/driver_controls/src/main.c
+++ projects/driver_controls/src/main.c
@@ -43,21 +43,6 @@ int main() {
   interrupt_init();
   gpio_it_init();
 
-  GPIOAddress ready_pin = { GPIO_PORT_B, 2 };
-  I2CSettings i2c_settings = {
-    .speed = I2C_SPEED_FAST, .scl = { GPIO_PORT_B, 10 }, .sda = { GPIO_PORT_B, 11 }
-  };
-
-  Ads1015Storage data;
-  i2c_init(I2C_PORT_2, &i2c_settings);
-
-  ads1015_init(&data, I2C_PORT_2, ADS1015_ADDRESS_GND, &ready_pin);
-  ads1015_configure_channel(&data, ADS1015_CHANNEL_0, true, NULL, &data);
-  ads1015_configure_channel(&data, ADS1015_CHANNEL_1, true, NULL, &data);
-  ads1015_configure_channel(&data, ADS1015_CHANNEL_2, true, NULL, &data);
-  ads1015_configure_channel(&data, ADS1015_CHANNEL_0, false, NULL, &data);
-  ads1015_configure_channel(&data, ADS1015_CHANNEL_1, false, NULL, &data);
-
   adc_init(ADC_MODE_CONTINUOUS);
 
   digital_io_init();
@@ -78,13 +63,7 @@ int main() {
   push_to_talk_fsm_init(&fsm_group.push_to_talk);
 
   int16_t adc_data[NUM_ADS1015_CHANNELS];
-  while (1) {
-    for (uint8_t i = 0; i < NUM_ADS1015_CHANNELS; i++) {
-      ads1015_read_converted(&data, i, &adc_data[i]);
-    }
-    printf("[ %d\t%d\t%d\t%d ]\n", adc_data[ADS1015_CHANNEL_0], adc_data[ADS1015_CHANNEL_1],
-           adc_data[ADS1015_CHANNEL_2], adc_data[ADS1015_CHANNEL_3]);
-  }
+
   for (;;) {
     if (status_ok(event_process(&e))) {
       // Process the event with the input FSMs
