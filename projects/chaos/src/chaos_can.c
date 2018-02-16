5f96552a261770261cc2b34cbc1a9937c52eee1e
ckitagawa <calderk2010@gmail.com>
2018-02-15 21:16:22 -0500

    ELEC-105: Chaos CAN FSM

---
+++ projects/chaos/src/chaos_can.c
@@ -0,0 +1,59 @@
+#include "chaos_can.h"
+
+#include <stddef.h>
+
+#include "can.h"
+#include "can_ack.h"
+#include "can_msg_defs.h"
+#include "can_unpack.h"
+#include "chaos_events.h"
+#include "event_queue.h"
+#include "misc.h"
+#include "status.h"
+
+#define NO_DATA 0
+#define CHAOS_CAN_NUM_RX_HANDLERS 1
+
+static CANStorage s_can_storage;
+static CANRxHandler s_can_rx_handlers[CHAOS_CAN_NUM_RX_HANDLERS];
+
+// TODO: Determine if there are any other messages we care about. BPS_FAULT comes to mind but if we
+// are driving at 100 kph the driver not the car should switch to idle to cut power.
+
+static StatusCode prv_power_state_callback(const CANMessage *msg, void *context,
+                                           CANAckStatus *ack_reply) {
+  ChaosCanPowerState power_state = NUM_CHAOS_CAN_POWER_STATES;
+  CAN_UNPACK_POWER_STATE(msg, (uint8_t *)&power_state);
+  switch (power_state) {
+    // TODO: If the selected state cannot be transitioned to consider returning invalid. While not
+    // actionable this could at least provide feedback to the driver that the transition selected
+    // was invalid. Theoretically this should be enforced on the driver controls side though.
+    case CHAOS_CAN_POWER_STATE_IDLE:
+      event_raise(CHAOS_EVENT_SEQUENCE_IDLE, NO_DATA);
+      break;
+    case CHAOS_CAN_POWER_STATE_CHARGE:
+      event_raise(CHAOS_EVENT_SEQUENCE_CHARGE, NO_DATA);
+      break;
+    case CHAOS_CAN_POWER_STATE_DRIVE:
+      event_raise(CHAOS_EVENT_SEQUENCE_DRIVE, NO_DATA);
+      break;
+    case NUM_CHAOS_CAN_POWER_STATES:
+    default:
+      *ack_reply = CAN_ACK_STATUS_INVALID;
+      return status_code(STATUS_CODE_INVALID_ARGS);
+  }
+  return STATUS_CODE_OK;
+}
+
+StatusCode chaos_can_init(CANSettings *settings) {
+  status_ok_or_return(
+      can_init(settings, &s_can_storage, s_can_rx_handlers, SIZEOF_ARRAY(s_can_rx_handlers)));
+
+  status_ok_or_return(
+      can_register_rx_handler(CAN_MESSAGE_POWER_STATE, prv_power_state_callback, NULL));
+  return STATUS_CODE_OK;
+}
+
+bool chaos_can_process_event(const Event *e) {
+  return fsm_process_event(CAN_FSM, e);
+}
---
+++ projects/chaos/test/test_chaos_can.h
@@ -0,0 +1,134 @@
+#include "chaos_can.h"
+
+#include <stddef.h>
+
+#include "can_ack.h"
+#include "can_msg_defs.h"
+#include "can_transmit.h"
+#include "chaos_events.h"
+#include "event_queue.h"
+#include "interrupt.h"
+#include "log.h"
+#include "soft_timer.h"
+#include "status.h"
+#include "test_helpers.h"
+#include "unity.h"
+
+#define TEST_NULL_ID UINT16_MAX
+
+static CANAckRequests s_can_ack_requests;
+
+static StatusCode prv_ack_callback(CANMessageID msg_id, uint16_t device, CANAckStatus status,
+                                   uint16_t num_remaining, void *context) {
+  TEST_ASSERT_EQUAL(0, num_remaining);
+  TEST_ASSERT_EQUAL(CAN_DEVICE_CHAOS, device);
+  TEST_ASSERT_EQUAL(CAN_MESSAGE_POWER_STATE, msg_id);
+  CANAckStatus *expected_status = context;
+  TEST_ASSERT_EQUAL(*expected_status, status);
+  return STATUS_CODE_OK;
+}
+
+void setup_test(void) {
+  event_queue_init();
+  interrupt_init();
+  soft_timer_init();
+
+  CANSettings settings = {
+    .device_id = CAN_DEVICE_CHAOS,
+    .bitrate = CAN_HW_BITRATE_125KBPS,
+    .rx_event = CHAOS_EVENT_CAN_RX,
+    .tx_event = CHAOS_EVENT_CAN_TX,
+    .fault_event = CHAOS_EVENT_CAN_FAULT,
+    .tx = { GPIO_PORT_A, 12 },
+    .rx = { GPIO_PORT_A, 11 },
+    .loopback = true,
+  };
+
+  TEST_ASSERT_OK(chaos_can_init(&settings));
+  can_ack_init(&s_can_ack_requests);
+}
+
+void teardown_test(void) {}
+
+typedef struct TestChaosCanParams {
+  ChaosCanPowerState power_state;
+  Event expected_output;
+  CANAckStatus expected_status;
+  size_t expected_can_cnt;
+  size_t expected_events_cnt;
+} TestChaosCanParams;
+
+void test_chaos_can(void) {
+  uint32_t expected_bitset = CAN_ACK_EXPECTED_DEVICES(CAN_DEVICE_CHAOS);
+  CANAckStatus expected_status = NUM_ACK_STATUSES;
+  const CANAckRequest req = {
+    .context = &expected_status,
+    .callback = prv_ack_callback,
+    .expected_bitset = expected_bitset,
+  };
+
+  const TestChaosCanParams params[] = {
+    { .power_state = CHAOS_CAN_POWER_STATE_IDLE,
+      .expected_output = { .id = CHAOS_EVENT_SEQUENCE_IDLE },
+      .expected_can_cnt = 2,
+      .expected_events_cnt = 1,
+      .expected_status = CAN_ACK_STATUS_OK },
+    { .power_state = CHAOS_CAN_POWER_STATE_CHARGE,
+      .expected_output = { .id = CHAOS_EVENT_SEQUENCE_CHARGE },
+      .expected_can_cnt = 2,
+      .expected_events_cnt = 1,
+      .expected_status = CAN_ACK_STATUS_OK },
+    { .power_state = CHAOS_CAN_POWER_STATE_DRIVE,
+      .expected_output = { .id = CHAOS_EVENT_SEQUENCE_DRIVE },
+      .expected_can_cnt = 2,
+      .expected_events_cnt = 1,
+      .expected_status = CAN_ACK_STATUS_OK },
+    { .power_state = NUM_CHAOS_CAN_POWER_STATES,
+      .expected_output = { .id = TEST_NULL_ID },
+      .expected_can_cnt = 2,
+      .expected_events_cnt = 0,
+      .expected_status = CAN_ACK_STATUS_INVALID },
+  };
+  volatile Event e = { 0 };
+  volatile StatusCode status = NUM_STATUS_CODES;
+
+  for (uint16_t i = 0; i < SIZEOF_ARRAY(params); i++) {
+    LOG_DEBUG("Loop %u\n", i);
+    TEST_ASSERT_OK(CAN_TRANSMIT_POWER_STATE(&req, params[i].power_state));
+
+    // Request
+    // TX
+    do {
+      status = event_process(&e);
+    } while (status != STATUS_CODE_OK);
+    TEST_ASSERT_TRUE(chaos_can_process_event(&e));
+    // RX
+    do {
+      status = event_process(&e);
+    } while (status != STATUS_CODE_OK);
+    TEST_ASSERT_TRUE(chaos_can_process_event(&e));
+
+    size_t can_event_cnt = 0;
+    size_t output_cnt = 0;
+
+    // Ack and Process (non-deterministic order)
+    expected_status = params[i].expected_status;
+    for (size_t j = 0; j < params[i].expected_can_cnt + params[i].expected_events_cnt; j++) {
+      do {
+        status = event_process(&e);
+      } while (status != STATUS_CODE_OK);
+      if (e.id == CHAOS_EVENT_CAN_RX || e.id == CHAOS_EVENT_CAN_TX) {
+        TEST_ASSERT_TRUE(chaos_can_process_event(&e));
+        can_event_cnt++;
+      } else {
+        if (params[i].expected_output.id != TEST_NULL_ID) {
+          // Check event
+          TEST_ASSERT_EQUAL(params[i].expected_output.id, e.id);
+          output_cnt++;
+        }
+      }
+    }
+    TEST_ASSERT_EQUAL(params[i].expected_can_cnt, can_event_cnt);
+    TEST_ASSERT_EQUAL(params[i].expected_events_cnt, output_cnt);
+  }
+}
