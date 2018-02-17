63eccdf4c7582c541a7e649b94b5b2d2fd4c1b55
ckitagawa <calderk2010@gmail.com>
2018-02-17 17:18:18 -0500

    ELEC-353: Rollforward codegen-tooling fixing namespace issue

--- libraries/codegen-tooling/inc/can_pack.h
+++ libraries/codegen-tooling/inc/can_pack.h
@@ -4,116 +4,135 @@
 #include "can_pack_impl.h"
 
 #define CAN_PACK_BPS_FAULT(msg_ptr) \
-  can_pack_impl_empty((msg_ptr), CAN_DEVICE_PLUTUS, CAN_MESSAGE_BPS_FAULT, 0)
+  can_pack_impl_empty((msg_ptr), SYSTEM_CAN_DEVICE_PLUTUS, CAN_MESSAGE_BPS_FAULT)
 
-#define CAN_PACK_BATTERY_RELAY(msg_ptr, relay_state_u8)                                         \
-  can_pack_impl_u8((msg_ptr), CAN_DEVICE_CHAOS, CAN_MESSAGE_BATTERY_RELAY, 1, (relay_state_u8), \
-                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,               \
-                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,               \
-                   CAN_PACK_IMPL_EMPTY)
-
-#define CAN_PACK_MAIN_RELAY(msg_ptr, relay_state_u8)                                         \
-  can_pack_impl_u8((msg_ptr), CAN_DEVICE_CHAOS, CAN_MESSAGE_MAIN_RELAY, 1, (relay_state_u8), \
-                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,            \
-                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,            \
-                   CAN_PACK_IMPL_EMPTY)
-
-#define CAN_PACK_SOLAR_RELAY(msg_ptr, relay_state_u8)                                         \
-  can_pack_impl_u8((msg_ptr), CAN_DEVICE_CHAOS, CAN_MESSAGE_SOLAR_RELAY, 1, (relay_state_u8), \
-                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,             \
-                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,             \
-                   CAN_PACK_IMPL_EMPTY)
-
-#define CAN_PACK_POWER_STATE(msg_ptr, power_state_u8)                                 \
-  can_pack_impl_u8((msg_ptr), CAN_DEVICE_DRIVER_CONTROLS, CAN_MESSAGE_POWER_STATE, 1, \
-                   (power_state_u8), CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,        \
-                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,     \
+#define CAN_PACK_BATTERY_RELAY(msg_ptr, relay_state_u8)                              \
+  can_pack_impl_u8((msg_ptr), SYSTEM_CAN_DEVICE_CHAOS, CAN_MESSAGE_BATTERY_RELAY, 1, \
+                   (relay_state_u8), CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,       \
+                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,    \
                    CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY)
 
-#define CAN_PACK_OVUV_DCDC_AUX(msg_ptr, dcdc_ov_flag_u8, dcdc_uv_flag_u8, aux_bat_ov_flag_u8,    \
-                               aux_bat_uv_flag_u8)                                               \
-  can_pack_impl_u8((msg_ptr), CAN_DEVICE_CHAOS, CAN_MESSAGE_OVUV_DCDC_AUX, 4, (dcdc_ov_flag_u8), \
-                   (dcdc_uv_flag_u8), (aux_bat_ov_flag_u8), (aux_bat_uv_flag_u8),                \
-                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,                \
+#define CAN_PACK_MAIN_RELAY(msg_ptr, relay_state_u8)                              \
+  can_pack_impl_u8((msg_ptr), SYSTEM_CAN_DEVICE_CHAOS, CAN_MESSAGE_MAIN_RELAY, 1, \
+                   (relay_state_u8), CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,    \
+                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, \
+                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY)
+
+#define CAN_PACK_SOLAR_RELAY_REAR(msg_ptr, relay_state_u8)                              \
+  can_pack_impl_u8((msg_ptr), SYSTEM_CAN_DEVICE_CHAOS, CAN_MESSAGE_SOLAR_RELAY_REAR, 1, \
+                   (relay_state_u8), CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,          \
+                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,       \
+                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY)
+
+#define CAN_PACK_SOLAR_RELAY_FRONT(msg_ptr, relay_state_u8)                              \
+  can_pack_impl_u8((msg_ptr), SYSTEM_CAN_DEVICE_CHAOS, CAN_MESSAGE_SOLAR_RELAY_FRONT, 1, \
+                   (relay_state_u8), CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,           \
+                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,        \
+                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY)
+
+#define CAN_PACK_POWER_STATE(msg_ptr, power_state_u8)                                        \
+  can_pack_impl_u8((msg_ptr), SYSTEM_CAN_DEVICE_DRIVER_CONTROLS, CAN_MESSAGE_POWER_STATE, 1, \
+                   (power_state_u8), CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,               \
+                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,            \
+                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY)
+
+#define CAN_PACK_OVUV_DCDC_AUX(msg_ptr, dcdc_ov_flag_u8, dcdc_uv_flag_u8, aux_bat_ov_flag_u8, \
+                               aux_bat_uv_flag_u8)                                            \
+  can_pack_impl_u8((msg_ptr), SYSTEM_CAN_DEVICE_CHAOS, CAN_MESSAGE_OVUV_DCDC_AUX, 4,          \
+                   (dcdc_ov_flag_u8), (dcdc_uv_flag_u8), (aux_bat_ov_flag_u8),                \
+                   (aux_bat_uv_flag_u8), CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,            \
+                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY)
+
+#define CAN_PACK_MC_ERROR_LIMITS(msg_ptr, error_id_u16, limits_u16)                                \
+  can_pack_impl_u16((msg_ptr), SYSTEM_CAN_DEVICE_MOTOR_CONTROLLER, CAN_MESSAGE_MC_ERROR_LIMITS, 4, \
+                    (error_id_u16), (limits_u16), CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY)
+
+#define CAN_PACK_MOTOR_CONTROLS(msg_ptr, throttle_u16, steering_angle_u16)                       \
+  can_pack_impl_u16((msg_ptr), SYSTEM_CAN_DEVICE_DRIVER_CONTROLS, CAN_MESSAGE_MOTOR_CONTROLS, 4, \
+                    (throttle_u16), (steering_angle_u16), CAN_PACK_IMPL_EMPTY,                   \
+                    CAN_PACK_IMPL_EMPTY)
+
+#define CAN_PACK_LIGHTS_STATES(msg_ptr, light_id_u8, light_state_u8)                           \
+  can_pack_impl_u8((msg_ptr), SYSTEM_CAN_DEVICE_DRIVER_CONTROLS, CAN_MESSAGE_LIGHTS_STATES, 2, \
+                   (light_id_u8), (light_state_u8), CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,  \
+                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,              \
                    CAN_PACK_IMPL_EMPTY)
 
-#define CAN_PACK_MC_ERROR_LIMITS(msg_ptr, error_id_u32, limits_u32)                         \
-  can_pack_impl_u32((msg_ptr), CAN_DEVICE_MOTOR_CONTROLLER, CAN_MESSAGE_MC_ERROR_LIMITS, 8, \
-                    (error_id_u32), (limits_u32))
-
-#define CAN_PACK_THROTTLE(msg_ptr) \
-  can_pack_impl_empty((msg_ptr), CAN_DEVICE_DRIVER_CONTROLS, CAN_MESSAGE_THROTTLE, 0)
-
-#define CAN_PACK_MOTOR_CRUISE(msg_ptr) \
-  can_pack_impl_empty((msg_ptr), CAN_DEVICE_DRIVER_CONTROLS, CAN_MESSAGE_MOTOR_CRUISE, 0)
-
-#define CAN_PACK_DIRECTION_SELECTOR(msg_ptr) \
-  can_pack_impl_empty((msg_ptr), CAN_DEVICE_DRIVER_CONTROLS, CAN_MESSAGE_DIRECTION_SELECTOR, 0)
-
-#define CAN_PACK_LIGHTS_STATES(msg_ptr, hazard_u8, left_turn_u8, right_turn_u8, brakes_u8,     \
-                               headlights_u8)                                                  \
-  can_pack_impl_u8((msg_ptr), CAN_DEVICE_DRIVER_CONTROLS, CAN_MESSAGE_LIGHTS_STATES, 5,        \
-                   (hazard_u8), (left_turn_u8), (right_turn_u8), (brakes_u8), (headlights_u8), \
-                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY)
-
-#define CAN_PACK_HORN(msg_ptr, state_u8)                                                   \
-  can_pack_impl_u8((msg_ptr), CAN_DEVICE_DRIVER_CONTROLS, CAN_MESSAGE_HORN, 1, (state_u8), \
-                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,          \
-                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,          \
+#define CAN_PACK_HORN(msg_ptr, state_u8)                                                          \
+  can_pack_impl_u8((msg_ptr), SYSTEM_CAN_DEVICE_DRIVER_CONTROLS, CAN_MESSAGE_HORN, 1, (state_u8), \
+                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,                 \
+                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,                 \
                    CAN_PACK_IMPL_EMPTY)
 
-#define CAN_PACK_MECHANICAL_BRAKE(msg_ptr) \
-  can_pack_impl_empty((msg_ptr), CAN_DEVICE_DRIVER_CONTROLS, CAN_MESSAGE_MECHANICAL_BRAKE, 0)
+#define CAN_PACK_MECHANICAL_BRAKE(msg_ptr, state_u8)                                              \
+  can_pack_impl_u8((msg_ptr), SYSTEM_CAN_DEVICE_DRIVER_CONTROLS, CAN_MESSAGE_MECHANICAL_BRAKE, 1, \
+                   (state_u8), CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,     \
+                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,                 \
+                   CAN_PACK_IMPL_EMPTY)
+
+#define CAN_PACK_CHARGING_REQ(msg_ptr) \
+  can_pack_impl_empty((msg_ptr), SYSTEM_CAN_DEVICE_CHARGER, CAN_MESSAGE_CHARGING_REQ)
+
+#define CAN_PACK_CHARGING_PERMISSION(msg_ptr, allowed_u8)                                       \
+  can_pack_impl_u8((msg_ptr), SYSTEM_CAN_DEVICE_CHAOS, CAN_MESSAGE_CHARGING_PERMISSION, 1,      \
+                   (allowed_u8), CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, \
+                   CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY,               \
+                   CAN_PACK_IMPL_EMPTY)
 
 #define CAN_PACK_BATTERY_SOC(msg_ptr) \
-  can_pack_impl_empty((msg_ptr), CAN_DEVICE_PLUTUS, CAN_MESSAGE_BATTERY_SOC, 0)
+  can_pack_impl_empty((msg_ptr), SYSTEM_CAN_DEVICE_PLUTUS, CAN_MESSAGE_BATTERY_SOC)
 
 #define CAN_PACK_BATTERY_VCT(msg_ptr, module_id_u16, voltage_u16, current_u16, temperature_u16) \
-  can_pack_impl_u16((msg_ptr), CAN_DEVICE_PLUTUS, CAN_MESSAGE_BATTERY_VCT, 8, (module_id_u16),  \
-                    (voltage_u16), (current_u16), (temperature_u16))
+  can_pack_impl_u16((msg_ptr), SYSTEM_CAN_DEVICE_PLUTUS, CAN_MESSAGE_BATTERY_VCT, 8,            \
+                    (module_id_u16), (voltage_u16), (current_u16), (temperature_u16))
 
 #define CAN_PACK_MOTOR_CONTROLLER_VC(msg_ptr, mc_voltage_1_u16, mc_current_1_u16,               \
                                      mc_voltage_2_u16, mc_current_2_u16)                        \
-  can_pack_impl_u16((msg_ptr), CAN_DEVICE_MOTOR_CONTROLLER, CAN_MESSAGE_MOTOR_CONTROLLER_VC, 8, \
-                    (mc_voltage_1_u16), (mc_current_1_u16), (mc_voltage_2_u16),                 \
-                    (mc_current_2_u16))
+  can_pack_impl_u16((msg_ptr), SYSTEM_CAN_DEVICE_MOTOR_CONTROLLER,                              \
+                    CAN_MESSAGE_MOTOR_CONTROLLER_VC, 8, (mc_voltage_1_u16), (mc_current_1_u16), \
+                    (mc_voltage_2_u16), (mc_current_2_u16))
 
-#define CAN_PACK_MOTOR_VELOCITY(msg_ptr) \
-  can_pack_impl_empty((msg_ptr), CAN_DEVICE_MOTOR_CONTROLLER, CAN_MESSAGE_MOTOR_VELOCITY, 0)
+#define CAN_PACK_MOTOR_VELOCITY_L(msg_ptr, vehicle_velocity_u32, angular_freq_u32)               \
+  can_pack_impl_u32((msg_ptr), SYSTEM_CAN_DEVICE_MOTOR_CONTROLLER, CAN_MESSAGE_MOTOR_VELOCITY_L, \
+                    8, (vehicle_velocity_u32), (angular_freq_u32))
 
-#define CAN_PACK_MOTOR_CONTROLLER_TEMPS(msg_ptr) \
-  can_pack_impl_empty((msg_ptr), CAN_DEVICE_MOTOR_CONTROLLER, CAN_MESSAGE_MOTOR_CONTROLLER_TEMPS, 0)
+#define CAN_PACK_MOTOR_VELOCITY_R(msg_ptr, vehicle_velocity_u32, angular_freq_u32)               \
+  can_pack_impl_u32((msg_ptr), SYSTEM_CAN_DEVICE_MOTOR_CONTROLLER, CAN_MESSAGE_MOTOR_VELOCITY_R, \
+                    8, (vehicle_velocity_u32), (angular_freq_u32))
 
-#define CAN_PACK_MOTOR_AMP_HR(msg_ptr) \
-  can_pack_impl_empty((msg_ptr), CAN_DEVICE_MOTOR_CONTROLLER, CAN_MESSAGE_MOTOR_AMP_HR, 0)
+#define CAN_PACK_MOTOR_TEMPS(msg_ptr, motor_temp_l_u32, motor_temp_r_u32)                      \
+  can_pack_impl_u32((msg_ptr), SYSTEM_CAN_DEVICE_MOTOR_CONTROLLER, CAN_MESSAGE_MOTOR_TEMPS, 8, \
+                    (motor_temp_l_u32), (motor_temp_r_u32))
 
-#define CAN_PACK_ODOMETER(msg_ptr) \
-  can_pack_impl_empty((msg_ptr), CAN_DEVICE_MOTOR_CONTROLLER, CAN_MESSAGE_ODOMETER, 0)
+#define CAN_PACK_MOTOR_AMP_HR(msg_ptr, motor_amp_hr_l_u32, motor_amp_hr_r_u32)                  \
+  can_pack_impl_u32((msg_ptr), SYSTEM_CAN_DEVICE_MOTOR_CONTROLLER, CAN_MESSAGE_MOTOR_AMP_HR, 8, \
+                    (motor_amp_hr_l_u32), (motor_amp_hr_r_u32))
 
-#define CAN_PACK_AUX_DCDC_VC(msg_ptr, aux_voltage_u16, aux_current_u16, dcdc_voltage_u16,       \
-                             dcdc_current_u16)                                                  \
-  can_pack_impl_u16((msg_ptr), CAN_DEVICE_CHAOS, CAN_MESSAGE_AUX_DCDC_VC, 8, (aux_voltage_u16), \
-                    (aux_current_u16), (dcdc_voltage_u16), (dcdc_current_u16))
+#define CAN_PACK_ODOMETER(msg_ptr, odometer_val_u32)                                        \
+  can_pack_impl_u32((msg_ptr), SYSTEM_CAN_DEVICE_MOTOR_CONTROLLER, CAN_MESSAGE_ODOMETER, 4, \
+                    (odometer_val_u32), CAN_PACK_IMPL_EMPTY)
 
-#define CAN_PACK_DCDC_TEMPS(msg_ptr, temp_1_u16, temp_2_u16)                              \
-  can_pack_impl_u16((msg_ptr), CAN_DEVICE_CHAOS, CAN_MESSAGE_DCDC_TEMPS, 4, (temp_1_u16), \
+#define CAN_PACK_AUX_DCDC_VC(msg_ptr, aux_voltage_u16, aux_current_u16, dcdc_voltage_u16, \
+                             dcdc_current_u16)                                            \
+  can_pack_impl_u16((msg_ptr), SYSTEM_CAN_DEVICE_CHAOS, CAN_MESSAGE_AUX_DCDC_VC, 8,       \
+                    (aux_voltage_u16), (aux_current_u16), (dcdc_voltage_u16), (dcdc_current_u16))
+
+#define CAN_PACK_DCDC_TEMPS(msg_ptr, temp_1_u16, temp_2_u16)                                     \
+  can_pack_impl_u16((msg_ptr), SYSTEM_CAN_DEVICE_CHAOS, CAN_MESSAGE_DCDC_TEMPS, 4, (temp_1_u16), \
                     (temp_2_u16), CAN_PACK_IMPL_EMPTY, CAN_PACK_IMPL_EMPTY)
 
-#define CAN_PACK_SOLAR_DATA_FRONT(msg_ptr, module_id_u16, voltage_u16, current_u16,            \
-                                  temperature_u16)                                             \
-  can_pack_impl_u16((msg_ptr), CAN_DEVICE_SOLAR_MASTER_FRONT, CAN_MESSAGE_SOLAR_DATA_FRONT, 8, \
-                    (module_id_u16), (voltage_u16), (current_u16), (temperature_u16))
+#define CAN_PACK_SOLAR_DATA_FRONT(msg_ptr, module_id_u16, voltage_u16, current_u16,                \
+                                  temperature_u16)                                                 \
+  can_pack_impl_u16((msg_ptr), SYSTEM_CAN_DEVICE_SOLAR_MASTER_FRONT, CAN_MESSAGE_SOLAR_DATA_FRONT, \
+                    8, (module_id_u16), (voltage_u16), (current_u16), (temperature_u16))
 
-#define CAN_PACK_SOLAR_DATA_REAR(msg_ptr, module_id_u16, voltage_u16, current_u16,           \
-                                 temperature_u16)                                            \
-  can_pack_impl_u16((msg_ptr), CAN_DEVICE_SOLAR_MASTER_REAR, CAN_MESSAGE_SOLAR_DATA_REAR, 8, \
-                    (module_id_u16), (voltage_u16), (current_u16), (temperature_u16))
-
-#define CAN_PACK_STEERING_ANGLE(msg_ptr) \
-  can_pack_impl_empty((msg_ptr), CAN_DEVICE_DRIVER_CONTROLS, CAN_MESSAGE_STEERING_ANGLE, 0)
+#define CAN_PACK_SOLAR_DATA_REAR(msg_ptr, module_id_u16, voltage_u16, current_u16,               \
+                                 temperature_u16)                                                \
+  can_pack_impl_u16((msg_ptr), SYSTEM_CAN_DEVICE_SOLAR_MASTER_REAR, CAN_MESSAGE_SOLAR_DATA_REAR, \
+                    8, (module_id_u16), (voltage_u16), (current_u16), (temperature_u16))
 
 #define CAN_PACK_LINEAR_ACCELERATION(msg_ptr) \
-  can_pack_impl_empty((msg_ptr), CAN_DEVICE_SENSOR_BOARD, CAN_MESSAGE_LINEAR_ACCELERATION, 0)
+  can_pack_impl_empty((msg_ptr), SYSTEM_CAN_DEVICE_SENSOR_BOARD, CAN_MESSAGE_LINEAR_ACCELERATION)
 
 #define CAN_PACK_ANGULAR_ROTATION(msg_ptr) \
-  can_pack_impl_empty((msg_ptr), CAN_DEVICE_SENSOR_BOARD, CAN_MESSAGE_ANGULAR_ROTATION, 0)
+  can_pack_impl_empty((msg_ptr), SYSTEM_CAN_DEVICE_SENSOR_BOARD, CAN_MESSAGE_ANGULAR_ROTATION)
