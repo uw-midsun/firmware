eec4dcfce944670ebf007460a52d6f8fd7eca30d
Ashkan <a3farmah@edu.uwaterloo.ca>
2018-03-07 20:45:06 +0000

    ELEC-350: changed some namings. Added comments.

--- projects/driver_controls/inc/throttle.h
+++ projects/driver_controls/inc/throttle.h
@@ -28,21 +28,22 @@ typedef enum {
 } ThrottleZone;
 
 // A measure in a 12 bit scale of how far within a zone a pedal is pressed.
-typedef uint16_t ThrottleFraction;
+// I.e. the numerator of a fraction with denominator of 2^12.
+typedef uint16_t ThrottleNumerator;
 
 typedef struct ThrottlePosition {
   ThrottleZone zone;
-  ThrottleFraction fraction;
+  ThrottleNumerator numerator;
 } ThrottlePosition;
 
 // Data that needs to be calibrated.
 typedef struct ThrottleCalibrationData {
-  int16_t main_bottom_thresh;
+  int16_t main_bottom_thresh; // min
   int16_t main_brake_thresh;
   int16_t main_coast_thresh;
-  int16_t main_accel_thresh;
-  int16_t secondary_bottom_thresh;
-  int16_t secondary_accel_thresh;
+  int16_t main_accel_thresh; // max
+  int16_t secondary_bottom_thresh; // min
+  int16_t secondary_accel_thresh; //max
   int16_t channel_readings_tolerance;
 } ThrottleCalibrationData;
 
