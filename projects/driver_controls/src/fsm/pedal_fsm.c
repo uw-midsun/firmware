94ba2ce5baa54436d4228a573773d2845a971ae3
Titus Chow <tituschow96@gmail.com>
2018-03-09 13:15:42 -0500

    ELEC-354: Added ability to exit cruise through throttle

--- projects/driver_controls/src/fsm/pedal_fsm.c
+++ projects/driver_controls/src/fsm/pedal_fsm.c
@@ -12,6 +12,7 @@
 // the update request to update the drive output module.
 // We probably still want to support regen braking if mechanical braking is active
 // todo: move cruise control out of the pedal FSM?
+// We can exit cruise control by entering coast mode on the throttle
 #include "pedal_fsm.h"
 #include "drive_output.h"
 #include "event_arbiter.h"
@@ -25,6 +26,8 @@ FSM_DECLARE_STATE(state_brake);
 FSM_DECLARE_STATE(state_coast);
 FSM_DECLARE_STATE(state_driving);
 FSM_DECLARE_STATE(state_cruise_control);
+// Entered braking state while in cruise
+FSM_DECLARE_STATE(state_cruise_braking);
 
 // Pedal FSM transition table definitions
 
@@ -63,17 +66,34 @@ FSM_STATE_TRANSITION(state_cruise_control) {
 
   // Since the cruise control increase/decrease events have information that needs to be output to
   // CAN, they will cause repeat transitions
-  // TODO: these should be moved somewhere else?
-  // FSM_ADD_TRANSITION(INPUT_EVENT_CRUISE_CONTROL_INC, state_cruise_control);
-  // FSM_ADD_TRANSITION(INPUT_EVENT_CRUISE_CONTROL_DEC, state_cruise_control);
+  FSM_ADD_TRANSITION(INPUT_EVENT_CRUISE_CONTROL_INC, state_cruise_control);
+  FSM_ADD_TRANSITION(INPUT_EVENT_CRUISE_CONTROL_DEC, state_cruise_control);
 
   // Cruise control exits either through hitting the cruise control switch or by engaging the
   // mechanical brake
   FSM_ADD_TRANSITION(INPUT_EVENT_MECHANICAL_BRAKE_PRESSED, state_brake);
-  // TODO: verify what behavior is expected from exiting cruise control - it should probably be
-  // based on the actual behavior
   // We enter coast as a safe default, but the next throttle command should update the proper state.
   FSM_ADD_TRANSITION(INPUT_EVENT_CRUISE_CONTROL, state_coast);
+  // If we enter the brake state, we'll still be in cruise but we want to allow exit through coast
+  FSM_ADD_TRANSITION(INPUT_EVENT_PEDAL_BRAKE, state_cruise_braking);
+}
+
+FSM_DECLARE_STATE(state_cruise_braking) {
+  FSM_ADD_TRANSITION(INPUT_EVENT_DRIVE_UPDATE_REQUESTED, state_cruise_control);
+
+  // Since the cruise control increase/decrease events have information that needs to be output to
+  // CAN, they will cause repeat transitions
+  FSM_ADD_TRANSITION(INPUT_EVENT_CRUISE_CONTROL_INC, state_cruise_control);
+  FSM_ADD_TRANSITION(INPUT_EVENT_CRUISE_CONTROL_DEC, state_cruise_control);
+
+  // Cruise control exits either through hitting the cruise control switch or by engaging the
+  // mechanical brake
+  FSM_ADD_TRANSITION(INPUT_EVENT_MECHANICAL_BRAKE_PRESSED, state_brake);
+  // We enter coast as a safe default, but the next throttle command should update the proper state.
+  FSM_ADD_TRANSITION(INPUT_EVENT_CRUISE_CONTROL, state_coast);
+  // We allow exiting cruise control by entering coast mode after we've released the pedal
+  // We do not allow exiting directly into brake to prevent extremely strong braking on cruise disable
+  FSM_ADD_TRANSITION(INPUT_EVENT_PEDAL_COAST, state_coast);
 }
 
 // Pedal FSM output functions
@@ -106,6 +126,7 @@ static void prv_cruise_output(FSM *fsm, const Event *e, void *context) {
 
   // TODO actually get target value
   // set throttle position to 0? might be nice to keep the actual position for debug
+  // TODO: start with current speed
   drive_output_update(storage, DRIVE_OUTPUT_SOURCE_CRUISE, 1234);
   drive_output_update(storage, DRIVE_OUTPUT_SOURCE_THROTTLE, 0);
 }
@@ -114,7 +135,8 @@ StatusCode pedal_fsm_init(FSM *fsm, EventArbiterStorage *storage) {
   fsm_state_init(state_brake, prv_state_output);
   fsm_state_init(state_coast, prv_state_output);
   fsm_state_init(state_driving, prv_state_output);
-  fsm_state_init(state_cruise_control, prv_state_output);
+  fsm_state_init(state_cruise_control, prv_cruise_output);
+  fsm_state_init(state_cruise_braking, prv_cruise_output);
 
   EventArbiter *arbiter = event_arbiter_add_fsm(storage, fsm, NULL);
 
