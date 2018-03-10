5f4789e80e8167aee2082e6e85123e5c44912097
Neil Parikh <parikh.neil@me.com>
2018-03-10 15:31:56 -0500

    ELEC-71: add more test cases for nmea_compute_checksum

--- projects/telemetry/test/test_nmea.c
+++ projects/telemetry/test/test_nmea.c
@@ -39,10 +39,26 @@ void test_compute_checksum(void) {
 }
 
 void test_compare_checksum(void) {
-  char *input =
+  char *input;
+  size_t message_len;
+
+  input =
       "$GPGGA,053740.000,2503.6319,N,12136.0099,E,1,08,1.1,63.8,"
       "M,15.2,M,,0000*64";
-
-  size_t message_len = strlen(input) + 1;
+  message_len = strlen(input) + 1;
   TEST_ASSERT_TRUE(nmea_compare_checksum(input, message_len));
+
+  input =
+      "$GPGGA,053740.000,2503.6319,N,12136.0099,E,1,08,1.1,63.8,"
+      "M,15.2,M,,0000*00";
+  message_len = strlen(input) + 1;
+  // should return false because checksum is incorrect
+  TEST_ASSERT_FALSE(nmea_compare_checksum(input, message_len));
+
+  input =
+      "$GPGGA,053740.000,2503.6319,N,12136.0099,E,1,08,1.1,63.8,"
+      "M,15.2,M,,0000*";
+  message_len = strlen(input) + 1;
+  // should return false because the given message does not contain a checksum
+  TEST_ASSERT_FALSE(nmea_compare_checksum(input, message_len));
 }
