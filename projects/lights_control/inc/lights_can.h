18b0b74b2d3600c324e9cbe7fb13e311f7bb4c1b
Arshankhanifar <arshankhanifar@gmail.com>
2018-03-13 21:27:28 -0400

    ELEC-372: Added the source files

---
+++ projects/lights_control/inc/lights_can.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "can.h"
+#include "lights_events.h"
+#include "lights_gpio.h"
+
+#define CAN_NUM_RX_HANDLERS 1
+
+void lights_can_init(BoardType type, bool loopback);
+StatusCode send_sync(void);
+
+typedef enum {
+  ACTION_SIGNAL_RIGHT = 0,
+  ACTION_SIGNAL_LEFT,
+  ACTION_SIGNAL_HAZARD,
+  ACTION_HORN,
+  ACTION_HEADLIGHTS,
+  ACTION_BRAKES,
+  ACTION_STROBE,
+  ACTION_SYNC,
+  NUM_ACTION_ID
+} ActionID;
+
