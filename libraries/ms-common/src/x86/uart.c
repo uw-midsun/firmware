8a52ac55357a03697ea86e18c9495c2e274c453b
Thomas Kidd <tjkidd@uwaterloo.ca>
2018-04-13 02:24:18 +0000

    ELEC-346: Users can now open as many uart ports as they want. There is still an outstanding issue with using the read function to check if a connection is open as it becomes blocking if a connection exists.

--- libraries/ms-common/src/x86/uart.c
+++ libraries/ms-common/src/x86/uart.c
@@ -1,5 +1,6 @@
 #include "uart.h"
 #include "log.h"
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -8,6 +9,7 @@
 #include <pthread.h>
 
 typedef struct {
+  UARTPort port;
   int sockfd;
   int listenfd;
   int connectfd;
@@ -15,44 +17,43 @@ typedef struct {
   UARTStorage storage;
 } SocketData;
 
-static SocketData sock = {
-  .sockfd = 0,
-  .listenfd = 0,
-  .connectfd = 0,
-};
+static SocketData *sock;
+static unsigned long num_sock = 0;
 
 static void *accept_handler(void *unused);
 static void *thread_handler(void *unused);
+static int get_sock(UARTPort uart);
 
-void *accept_handler(void *unused) {
+void *accept_handler(void *port) {
 
+  int current_sock = *((int*)port);
   pthread_t receiver_thread;
+  sock[current_sock].connectfd = accept(sock[current_sock].listenfd, (struct sockaddr*)NULL, NULL);
+  close(sock[current_sock].listenfd);
 
-  sock.connectfd = accept(sock.listenfd, (struct sockaddr*)NULL, NULL);
-  close(sock.listenfd);
+  sock[current_sock].sockfd = sock[current_sock].connectfd;
 
-  sock.sockfd = sock.connectfd;
-
-  if(pthread_create(&receiver_thread, NULL, thread_handler, NULL) < 0) {
+  if(pthread_create(&receiver_thread, NULL, thread_handler, port) < 0) {
     LOG_DEBUG("Error creating receiving thread.\n");
   }
 
   pthread_exit(NULL);
 }
 
-void *thread_handler(void *unused) {
+void *thread_handler(void *port) {
 
+  int current_sock = *(int*)port;
   int n = 0;
   uint8_t buf[1024];
   int done = 0;
 
   while (!done) {
 
-    n = read(sock.sockfd, buf, sizeof(buf)-1);
+    n = read(sock[current_sock].sockfd, buf, sizeof(buf)-1);
 
     if (n) {
       buf[n] = 0;
-      sock.storage.rx_handler(buf, sizeof(buf), sock.storage.context);
+      sock[current_sock].storage.rx_handler(buf, sizeof(buf), sock[current_sock].storage.context);
     }
 
     if(buf[0] == 'z') {
@@ -63,43 +64,64 @@ void *thread_handler(void *unused) {
   return NULL;
 }
 
+int get_sock(UARTPort uart) {
+
+  for(unsigned long current_sock = 0; current_sock < num_sock; current_sock++) {
+    if(sock[current_sock].port == uart) {
+      return (int)current_sock;
+    }
+  }
+
+  return -1;
+}
+
 StatusCode uart_init(UARTPort uart, UARTSettings *settings, UARTStorage *storage) {
 
   int one = 1;
+  unsigned long latest_sock = num_sock;
   pthread_t receiver_thread;
-  sock.storage.rx_handler = settings->rx_handler;
-  sock.storage.context = settings->context;
 
-  memset(&sock.addr, '0', sizeof(sock.addr));
+  // append another SocketData struct to the array sock[]
+  SocketData *tmp = sock;
+  num_sock += 1;
+  sock = (SocketData*)calloc(num_sock, sizeof(SocketData));
+  memcpy(sock, tmp, sizeof(SocketData)*latest_sock);
 
-  if((sock.sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    LOG_DEBUG("Error: Could not create sock.\n");
+
+  sock[latest_sock].port = uart;
+  sock[latest_sock].storage.rx_handler = settings->rx_handler;
+  sock[latest_sock].storage.context = settings->context;
+
+  memset(&sock[latest_sock].addr, '0', sizeof(sock[latest_sock].addr));
+
+  if((sock[latest_sock].sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    LOG_DEBUG("Error: Could not create sock[uart].\n");
     return status_code(STATUS_CODE_INTERNAL_ERROR);
   }
 
-  sock.addr.sin_family = AF_INET;
-  sock.addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  sock.addr.sin_port = htons(5000);
+  sock[latest_sock].addr.sin_family = AF_INET;
+  sock[latest_sock].addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  sock[latest_sock].addr.sin_port = htons(5000);
 
-  if(connect(sock.sockfd, (struct sockaddr*)&sock.addr, sizeof(sock.addr)) < 0) {
+  if(connect(sock[latest_sock].sockfd, (struct sockaddr*)&sock[latest_sock].addr, sizeof(sock[latest_sock].addr)) < 0) {
     LOG_DEBUG("Could not connect to an open socket, listening for incoming connections.\n");
 
-    sock.listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    memset(&sock.addr, '0', sizeof(sock.addr));
+    sock[latest_sock].listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&sock[latest_sock].addr, '0', sizeof(sock[latest_sock].addr));
 
-    sock.addr.sin_family = AF_INET;
-    sock.addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    sock.addr.sin_port = htons(5000);
+    sock[latest_sock].addr.sin_family = AF_INET;
+    sock[latest_sock].addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    sock[latest_sock].addr.sin_port = htons(5000);
     int one = 1;
-    setsockopt(sock.listenfd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
+    setsockopt(sock[latest_sock].listenfd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
 
-    if(bind(sock.listenfd, (struct sockaddr*)&sock.addr, sizeof(sock.addr)) < 0) {
+    if(bind(sock[latest_sock].listenfd, (struct sockaddr*)&sock[latest_sock].addr, sizeof(sock[latest_sock].addr)) < 0) {
       LOG_DEBUG("Could not bind to an open sock.\n");
       return status_code(STATUS_CODE_INTERNAL_ERROR);
     }
-    listen(sock.listenfd, 1);
+    listen(sock[latest_sock].listenfd, 1);
 
-    if(pthread_create(&receiver_thread, NULL, accept_handler, NULL) < 0) {
+    if(pthread_create(&receiver_thread, NULL, accept_handler, (void*)&sock[latest_sock].port) < 0) {
       LOG_DEBUG("Error creating receiving thread.\n");
       return status_code(STATUS_CODE_INTERNAL_ERROR);
     }
@@ -107,7 +129,7 @@ StatusCode uart_init(UARTPort uart, UARTSettings *settings, UARTStorage *storage
     return STATUS_CODE_UNINITIALIZED;
 
   } else {
-    if(pthread_create(&receiver_thread, NULL, thread_handler, NULL) < 0) {
+    if(pthread_create(&receiver_thread, NULL, thread_handler, (void*)&sock[latest_sock].port) < 0) {
       LOG_DEBUG("Error creating receiving thread.\n");
       return status_code(STATUS_CODE_INTERNAL_ERROR);
     }
@@ -116,15 +138,39 @@ StatusCode uart_init(UARTPort uart, UARTSettings *settings, UARTStorage *storage
 }
 
 StatusCode uart_set_rx_handler(UARTPort uart, UARTRxHandler rx_handler, void *context) {
-  sock.storage.rx_handler = rx_handler;
-  sock.storage.context = context;
+
+  // check if the port has been initilized
+  int current_sock;
+  if((current_sock = get_sock(uart)) < 0) {
+    LOG_DEBUG("UART Port %d has not been initialized.\n", uart);
+    return status_code(STATUS_CODE_INTERNAL_ERROR);
+  }
+
+  sock[current_sock].storage.rx_handler = rx_handler;
+  sock[current_sock].storage.context = context;
 
   return STATUS_CODE_OK;
 }
 
 StatusCode uart_tx(UARTPort uart, uint8_t *tx_data, size_t len) {
 
-  if(write(sock.sockfd, tx_data, len) < 0) {
+  // check if the port has been initilized
+  int current_sock;
+  if((current_sock = get_sock(uart)) < 0) {
+    LOG_DEBUG("UART Port %d has not been initialized.\n", uart);
+    return status_code(STATUS_CODE_INTERNAL_ERROR);
+  }
+
+  // check if a connection is open
+  int n = 0;
+  uint8_t buf[1];
+  /*
+  if((n = read(sock[current_sock].sockfd, buf, sizeof(buf))) < 0) {
+    LOG_DEBUG("No port to write to.\n");
+    return status_code(STATUS_CODE_INTERNAL_ERROR);
+  }
+  LOG_DEBUG("Can read %d\n.", n); */
+  if(write(sock[current_sock].sockfd, tx_data, len) < 0) {
     LOG_DEBUG("Error sending data.\n");
     return status_code(STATUS_CODE_INTERNAL_ERROR);
   }
