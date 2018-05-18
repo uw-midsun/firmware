e7625ea8138f6400e46f5ff8ef47f7148f152e89
Arshan Khanifar <arshankhanifar@gmail.com>
2018-05-18 02:59:11 -0400

    ELEC-376: Lights GPIO (#154)
    * ELEC-376: lights_gpio abstracts away controlling of the gpio pins by relating to them to events that can be passed to it (EVENTS_LIGHTS_GPIO_*).

---
+++ projects/lights_control/src/lights_gpio_config.c
@@ -0,0 +1,20 @@
+#include "misc.h"
+
+#include "gpio.h"
+#include "lights_events.h"
+#include "lights_gpio_config.h"
+#include "lights_gpio_config_front.h"
+#include "lights_gpio_config_rear.h"
+
+static const LightsGpio *s_lights_gpio;
+
+StatusCode lights_gpio_config(LightsGpioConfigBoardType board_type) {
+  s_lights_gpio = (board_type == LIGHTS_GPIO_CONFIG_BOARD_TYPE_FRONT)
+                      ? lights_gpio_config_front_load()
+                      : lights_gpio_config_rear_load();
+  return STATUS_CODE_OK;
+}
+
+LightsGpio *lights_gpio_config_load(void) {
+  return s_lights_gpio;
+}
