557a8f8e89436687f7f24e0d3635897c242bdc14
Ash Raji <ashrafraji@hotmail.com>
2018-05-19 18:47:06 -0400

    ELEC-234: Formatting (Automated commit)

--- projects/driver_controls/src/main.c
+++ projects/driver_controls/src/main.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 
 #include "adc.h"
-#include "gpio_it.h"
-#include "i2c.h"
-#include "interrupt.h"
 #include "ads1015.h"
 #include "ads1015_def.h"
-#include "delay.h"
-#include "log.h"
-#include "unity.h"
 #include "analog_io.h"
+#include "delay.h"
 #include "digital_io.h"
 #include "event_arbiter.h"
+#include "gpio_it.h"
+#include "i2c.h"
 #include "input_event.h"
+#include "interrupt.h"
+#include "log.h"
+#include "unity.h"
 
 #include "direction_fsm.h"
 #include "hazard_light_fsm.h"
@@ -34,34 +34,28 @@ static void prv_callback_channel(Ads1015Channel channel, void *context) {
   *s_callback_called = true;
 }
 
-
-static bool prv_channel_reading_void(int16_t reading){
- return (reading < (ADS1015_CURRENTFSR /2)) && ( reading >= 0);
-
+static bool prv_channel_reading_void(int16_t reading) {
+  return (reading < (ADS1015_CURRENTFSR / 2)) && (reading >= 0);
 }
 
 int main() {
- gpio_init();
- interrupt_init();
- gpio_it_init();
- soft_timer_init();
- I2CSettings i2c_settings ={ 
-   .speed = I2C_SPEED_FAST,
-   .scl =  {.port = GPIO_PORT_B, .pin = 10  },
-   .sda =  {.port = GPIO_PORT_B, .pin = 11 },
-};
- i2c_init(I2C_PORT_2,&i2c_settings);
- GPIOAddress ready_pin = {
-    .port = GPIO_PORT_B,
-    .pin = 2
- };
- ads1015_init(&s_storage, I2C_PORT_2, Ads1015Address, &ready_pin); 
+  gpio_init();
+  interrupt_init();
+  gpio_it_init();
+  soft_timer_init();
+  I2CSettings i2c_settings = {
+    .speed = I2C_SPEED_FAST,
+    .scl = { .port = GPIO_PORT_B, .pin = 10 },
+    .sda = { .port = GPIO_PORT_B, .pin = 11 },
+  };
+  i2c_init(I2C_PORT_2, &i2c_settings);
+  GPIOAddress ready_pin = { .port = GPIO_PORT_B, .pin = 2 };
+  ads1015_init(&s_storage, I2C_PORT_2, Ads1015Address, &ready_pin);
 
- ads1015_configure_channel(&s_storage, ADS1015_CHANNEL_0, true, prv_callback_channel, &s_callback_called[ADS1015_CHANNEL_0])
+  ads1015_configure_channel(&s_storage, ADS1015_CHANNEL_0, true, prv_callback_channel,
+                            &s_callback_called[ADS1015_CHANNEL_0])
 
- int16_t reading = 0;
+      int16_t reading = 0;
 
- ads1015_read_raw(&s_storage, ADS1015_CHANNEL_0, &reading);
- 
+  ads1015_read_raw(&s_storage, ADS1015_CHANNEL_0, &reading);
 }
-
