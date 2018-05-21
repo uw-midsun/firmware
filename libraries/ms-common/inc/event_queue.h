7661b28985133ddd285d6b5a8d535bf1f8e43fec
ckitagawa <calderk2010@gmail.com>
2018-05-21 19:19:40 -0400

    ELEC-425: Priority Fifo Event Queue

--- libraries/ms-common/inc/event_queue.h
+++ libraries/ms-common/inc/event_queue.h
@@ -1,7 +1,7 @@
 #pragma once
 // Global event queue
 //
-// Uses a minimum priority queue to prioritize events of lower ID. Only one global instance exists.
+// Uses an array of FIFOs to prioritize events of lower priority. Only one global instance exists.
 #include <stdint.h>
 
 #include "objpool.h"
@@ -9,6 +9,15 @@
 
 #define EVENT_QUEUE_SIZE 20
 typedef uint16_t EventID;
+
+typedef enum {
+  EVENT_PRIORITY_HIGHEST = 0,
+  EVENT_PRIORITY_HIGH,
+  EVENT_PRIORITY_NORMAL,
+  EVENT_PRIORITY_LOW,
+  EVENT_PRIORITY_LOWEST,
+  NUM_EVENT_PRIORITIES,
+} EventPriority;
 
 typedef struct Event {
   EventID id;
@@ -18,8 +27,12 @@ typedef struct Event {
 // Initializes the event queue.
 void event_queue_init(void);
 
-// Raises an event in the global event queue.
-StatusCode event_raise(EventID id, uint16_t data);
+// Raises an event in the global event queue at the default priority.
+StatusCode event_raise_priority(EventPriority priority, EventID id, uint16_t data);
+
+// Raises an event in the global event queue at the default priority. Provided for legacy
+// compatibility.
+#define event_raise(id, data) event_raise_priority(EVENT_PRIORITY_NORMAL, (id), (data))
 
 // Returns the next event to be processed.
 // Note that events are processed by priority.
--- libraries/ms-common/test/test_event_queue.c
+++ libraries/ms-common/test/test_event_queue.c
@@ -13,6 +13,7 @@ void setup_test(void) {
 
 void teardown_test(void) {}
 
+// Test a single priority level.
 void test_event_queue_raise(void) {
   // Fill the event queue
   for (int i = EVENT_QUEUE_SIZE; i > 0; i--) {
@@ -23,11 +24,11 @@ void test_event_queue_raise(void) {
   TEST_ASSERT_EQUAL(STATUS_CODE_RESOURCE_EXHAUSTED, prv_raise_event(0));
 
   Event e;
-  uint16_t i = 1;  // Start at 1 since the insertion of 0 should've failed
+  uint16_t i = EVENT_QUEUE_SIZE;
   while (status_ok(event_process(&e))) {
     TEST_ASSERT_EQUAL(i, e.id);
     TEST_ASSERT_EQUAL(i * 100, e.data);
-    i++;
+    i--;
   }
 
   TEST_ASSERT_OK(prv_raise_event(5));
@@ -35,6 +36,38 @@ void test_event_queue_raise(void) {
 
   TEST_ASSERT_EQUAL(5, e.id);
   TEST_ASSERT_EQUAL(5 * 100, e.data);
+
+  TEST_ASSERT_EQUAL(STATUS_CODE_EMPTY, event_process(&e));
+}
+
+void test_event_queue_raise_priority(void) {
+  // Test each priority.
+  for (int i = NUM_EVENT_PRIORITIES - 1; i >= 0; i--) {
+    TEST_ASSERT_OK(event_raise_priority(i, i * 10, i * 100));
+  }
+
+  Event e;
+  uint16_t i = 0;
+  while (status_ok(event_process(&e))) {
+    TEST_ASSERT_EQUAL(i * 10, e.id);
+    TEST_ASSERT_EQUAL(i * 100, e.data);
+    i++;
+  }
+  TEST_ASSERT_EQUAL(STATUS_CODE_EMPTY, event_process(&e));
+
+  // Spot check.
+  TEST_ASSERT_OK(event_raise_priority(EVENT_PRIORITY_NORMAL, 1, 10));
+  TEST_ASSERT_OK(event_raise_priority(EVENT_PRIORITY_HIGHEST, 1, 11));
+  TEST_ASSERT_OK(event_process(&e));
+  TEST_ASSERT_EQUAL(1, e.id);
+  TEST_ASSERT_EQUAL(11, e.data);
+  TEST_ASSERT_OK(event_raise_priority(EVENT_PRIORITY_HIGHEST, 1, 12));
+  TEST_ASSERT_OK(event_process(&e));
+  TEST_ASSERT_EQUAL(1, e.id);
+  TEST_ASSERT_EQUAL(12, e.data);
+  TEST_ASSERT_OK(event_process(&e));
+  TEST_ASSERT_EQUAL(1, e.id);
+  TEST_ASSERT_EQUAL(10, e.data);
 
   TEST_ASSERT_EQUAL(STATUS_CODE_EMPTY, event_process(&e));
 }
