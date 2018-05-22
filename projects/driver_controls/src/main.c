77e08d99203ce482933902d9480f479248b2fbbb
Ash Raji <ashrafraji@hotmail.com>
2018-05-22 19:58:26 -0400

    ELEC-234: fixed callback function

--- projects/driver_controls/src/main.c
+++ projects/driver_controls/src/main.c
@@ -4,31 +4,29 @@
 #include "adc.h"
 #include "ads1015.h"
 #include "ads1015_def.h"
-#include "analog_io.h"
 #include "delay.h"
-#include "digital_io.h"
 #include "event_arbiter.h"
 #include "gpio_it.h"
 #include "i2c.h"
 #include "input_event.h"
 #include "interrupt.h"
 #include "log.h"
+#include "soft_timer.h"
 #include "unity.h"
+#include <stdbool.h>
 
-static Ads1015Storage s_storage;
 
-static bool s_callback_called[NUM_ADS1015_CHANNELS];
 
 static void prv_callback_channel(Ads1015Channel channel, void *context) {
-  bool *s_callback_called = context;
-  *s_callback_called = true;
+  Ads1015Storage *storage = context;
+  int16_t reading = 0;
+  ads1015_read_raw(storage, channel, &reading);
+  printf("%d\n", reading);
 }
 
-static bool prv_channel_reading_void(int16_t reading) {
-  return (reading < (ADS1015_CURRENT_FSR / 2)) && (reading >= 0);
-}
+int main(void) {
+  Ads1015Storage storage;
 
-int main() {
   gpio_init();
   interrupt_init();
   gpio_it_init();
@@ -40,13 +38,15 @@ int main() {
   };
   i2c_init(I2C_PORT_2, &i2c_settings);
   GPIOAddress ready_pin = { .port = GPIO_PORT_A, .pin = 9 };
+ 
+  ads1015_init(&storage, I2C_PORT_2, ADS1015_ADDRESS_GND, &ready_pin);
+ 
+  ads1015_configure_channel(&storage, ADS1015_CHANNEL_0, true, prv_callback_channel,
+                            &storage);
 
-  ads1015_init(&s_storage, I2C_PORT_2, ADS1015_ADDRESS_VDD, &ready_pin);
+  
+  while (true) {
+  }
 
-  ads1015_configure_channel(&s_storage, ADS1015_CHANNEL_0, true, prv_callback_channel,
-                            &s_callback_called[ADS1015_CHANNEL_0]);
-
-  int16_t reading = 0;
-
-  ads1015_read_raw(&s_storage, ADS1015_CHANNEL_0, &reading);
+  return 0;
 }
