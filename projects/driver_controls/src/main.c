a7f2c440c6bbfb7721b6aae580dfd6036d6abeac
Ash Raji <ashrafraji@hotmail.com>
2018-05-28 21:12:43 -0400

    ELEC-234: Formatting (Automated commit)

--- projects/driver_controls/src/main.c
+++ projects/driver_controls/src/main.c
@@ -15,31 +15,29 @@
 #include "soft_timer.h"
 #include "unity.h"
 
-int16_t percentage_converter(int16_t reading_in_lsb, int16_t zero_value, int16_t hundred_value){
-
+int16_t percentage_converter(int16_t reading_in_lsb, int16_t zero_value, int16_t hundred_value) {
   int16_t percentage;
- if(zero_value > hundred_value){
-    percentage = ((100 * (reading_in_lsb  - hundred_value))/(hundred_value - zero_value)) + 100;
- }
- else{
+  if (zero_value > hundred_value) {
+    percentage = ((100 * (reading_in_lsb - hundred_value)) / (hundred_value - zero_value)) + 100;
+  } else {
     percentage = (100 * (reading_in_lsb - zero_value)) / (hundred_value - zero_value);
- }
+  }
 
- if(percentage < 0){
-    percentage = 0; 
- }else if(percentage > 100){
+  if (percentage < 0) {
+    percentage = 0;
+  } else if (percentage > 100) {
     percentage = 100;
- }
+  }
 
- return percentage;
+  return percentage;
 }
 
 static void prv_callback_channel(Ads1015Channel channel, void *context) {
   Ads1015Storage *storage = context;
   int16_t reading = 0;
   ads1015_read_raw(storage, channel, &reading);
-  int16_t percentage = percentage_converter(reading,1253,418);
-  printf("%d %d\n", reading,percentage);
+  int16_t percentage = percentage_converter(reading, 1253, 418);
+  printf("%d %d\n", reading, percentage);
 }
 
 int main(void) {
