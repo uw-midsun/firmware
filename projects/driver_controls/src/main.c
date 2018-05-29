96b9a43d40bc72c4d704a7cbc3822f7dc3234e97
Ash Raji <ashrafraji@hotmail.com>
2018-05-28 21:11:26 -0400

    ELEC-234: outputs percentage given lsb

--- projects/driver_controls/src/main.c
+++ projects/driver_controls/src/main.c
@@ -15,11 +15,31 @@
 #include "soft_timer.h"
 #include "unity.h"
 
+int16_t percentage_converter(int16_t reading_in_lsb, int16_t zero_value, int16_t hundred_value){
+
+  int16_t percentage;
+ if(zero_value > hundred_value){
+    percentage = ((100 * (reading_in_lsb  - hundred_value))/(hundred_value - zero_value)) + 100;
+ }
+ else{
+    percentage = (100 * (reading_in_lsb - zero_value)) / (hundred_value - zero_value);
+ }
+
+ if(percentage < 0){
+    percentage = 0; 
+ }else if(percentage > 100){
+    percentage = 100;
+ }
+
+ return percentage;
+}
+
 static void prv_callback_channel(Ads1015Channel channel, void *context) {
   Ads1015Storage *storage = context;
   int16_t reading = 0;
   ads1015_read_raw(storage, channel, &reading);
-  printf("%d\n", reading);
+  int16_t percentage = percentage_converter(reading,1253,418);
+  printf("%d %d\n", reading,percentage);
 }
 
 int main(void) {
@@ -35,7 +55,7 @@ int main(void) {
     .sda = { .port = GPIO_PORT_B, .pin = 11 },
   };
   i2c_init(I2C_PORT_2, &i2c_settings);
-  GPIOAddress ready_pin = { .port = GPIO_PORT_A, .pin = 9 };
+  GPIOAddress ready_pin = { .port = GPIO_PORT_B, .pin = 2 };
 
   ads1015_init(&storage, I2C_PORT_2, ADS1015_ADDRESS_GND, &ready_pin);
 
