d1151fb868145e1eb2516a074c6f48b7154469ae
Titus Chow <tituschow96@gmail.com>
2018-06-02 04:18:48 +0000

    ELEC-338: Fixed UART overrun, fixed can_slave

--- projects/can_slave/src/main.c
+++ projects/can_slave/src/main.c
@@ -11,6 +11,15 @@
 #include "uart.h"
 #include "wait.h"
 
+#define CAN_SLAVE_UART_BAUDRATE 115200
+#define CAN_SLAVE_UART_PORT UART_PORT_3
+#define CAN_SLAVE_UART_TX \
+  { .port = GPIO_PORT_B, .pin = 10 }
+#define CAN_SLAVE_UART_RX \
+  { .port = GPIO_PORT_B, .pin = 11 }
+#define CAN_SLAVE_UART_ALTFN GPIO_ALTFN_4
+#define CAN_SLAVE_CAN_BITRATE CAN_HW_BITRATE_500KBPS
+
 static UARTStorage s_uart_storage;
 
 static void prv_init_periph(void) {
@@ -18,42 +27,42 @@ static void prv_init_periph(void) {
   interrupt_init();
   soft_timer_init();
 
+  UARTSettings uart_settings = {
+    .baudrate = CAN_SLAVE_UART_BAUDRATE,  //
+    .tx = CAN_SLAVE_UART_TX,              //
+    .rx = CAN_SLAVE_UART_RX,              //
+    .alt_fn = CAN_SLAVE_UART_ALTFN        //
+  };
+  uart_init(CAN_SLAVE_UART_PORT, &uart_settings, &s_uart_storage);
+
   CANHwSettings can_hw_settings = {
     .tx = { .port = GPIO_PORT_A, .pin = 12 },  //
     .rx = { .port = GPIO_PORT_A, .pin = 11 },  //
-    .bitrate = CAN_HW_BITRATE_500KBPS,         //
+    .bitrate = CAN_SLAVE_CAN_BITRATE,          //
     .loopback = false,                         //
   };
 
   can_hw_init(&can_hw_settings);
+}
 
-  UARTSettings uart_settings = {
-    .baudrate = 115200,                       //
-    .tx = { .port = GPIO_PORT_A, .pin = 3 },  //
-    .rx = { .port = GPIO_PORT_A, .pin = 2 },  //
-    .alt_fn = GPIO_ALTFN_1                    //
-  };
-  uart_init(UART_PORT_2, &uart_settings, &s_uart_storage);
+static void prv_rx_cb(const struct CanUart *can_uart, uint32_t id, bool extended,
+                      const uint64_t *data, size_t dlc, void *context) {
+  LOG_DEBUG("RX id %ld\n", id);
 }
 
 int main(void) {
   prv_init_periph();
 
   CanUart can_uart = {
-    .uart = UART_PORT_2,  //
-    .rx_cb = NULL,        // Ignore RX'd messages from the master
-    .context = NULL       //
+    .uart = CAN_SLAVE_UART_PORT,  //
+    .rx_cb = prv_rx_cb,                // Ignore RX'd messages from the master
+    .context = NULL               //
   };
   can_uart_init(&can_uart);
-  can_uart_enable_passthrough(&can_uart);
-
-  GPIOAddress led = { .port = GPIO_PORT_A, .pin = 15 };
-  GPIOSettings settings = { .direction = GPIO_DIR_OUT, .state = GPIO_STATE_LOW };
-  gpio_init_pin(&led, &settings);
+  // can_uart_enable_passthrough(&can_uart);
 
   while (true) {
-    delay_s(1);
-    gpio_toggle_state(&led);
+    wait();
   }
 
   return 0;
