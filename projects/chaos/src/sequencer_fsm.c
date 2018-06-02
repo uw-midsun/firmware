b2c451ecb6880941dfdcac612def3e89fd843d01
ckitagawa <calderk2010@gmail.com>
2018-06-01 21:12:55 -0400

    ELEC-105: WIP

--- projects/chaos/src/sequencer_fsm.c
+++ projects/chaos/src/sequencer_fsm.c
@@ -10,6 +10,7 @@
 #include "misc.h"
 #include "power_path.h"
 #include "relay_id.h"
+#include "relay_retry_service.h"
 #include "sequencer.h"
 
 #define NUM_SEQUENCER_FSM_FILTERS 3
@@ -18,9 +19,10 @@
 static FSM s_sequencer_fsm;
 static SequencerStorage s_storage;
 
+static bool s_pending_transition = false;
 static uint16_t s_filtered_ids[NUM_SEQUENCER_FSM_FILTERS] = { 0 };
 
-//
+// Adds a filter on |id| to ignore it until it is cleared.
 static bool prv_add_filter(uint16_t id) {
   for (size_t i = 0; i < NUM_SEQUENCER_FSM_FILTERS; i++) {
     if (s_filtered_ids[i] == NUM_CHAOS_EVENTS) {
@@ -31,14 +33,14 @@ static bool prv_add_filter(uint16_t id) {
   return false;
 }
 
-//
+// Removes all filters from |s_filtered_ids|
 static void prv_clear_filters(void) {
   for (size_t i = 0; i < NUM_SEQUENCER_FSM_FILTERS; i++) {
     s_filtered_ids[i] = NUM_CHAOS_EVENTS;
   }
 }
 
-//
+// Checks if |id| is filtered.
 static bool prv_is_filtered_id(uint16_t id) {
   for (size_t i = 0; i < NUM_SEQUENCER_FSM_FILTERS; i++) {
     if (s_filtered_ids[i] == id) {
@@ -172,8 +174,8 @@ FSM_DECLARE_STATE(sequencer_state_charge);
 FSM_DECLARE_STATE(sequencer_state_drive);
 
 FSM_STATE_TRANSITION(sequencer_state_emergency) {
-  FSM_ADD_TRANSITION(CHAOS_EVENT_SEQUENCE_IDLE, sequencer_state_idle);
   FSM_ADD_TRANSITION(CHAOS_EVENT_SEQUENCE_EMERGENCY, sequencer_state_emergency);
+  FSM_ADD_TRANSITION(CHAOS_EVENT_SEQUENCE_IDLE, sequencer_state_idle);
   FSM_ADD_TRANSITION(CHAOS_EVENT_SEQUENCE_RESET, sequencer_state_emergency);
 }
 
@@ -186,24 +188,42 @@ FSM_STATE_TRANSITION(sequencer_state_idle) {
 
 FSM_STATE_TRANSITION(sequencer_state_charge) {
   FSM_ADD_TRANSITION(CHAOS_EVENT_SEQUENCE_EMERGENCY, sequencer_state_emergency);
+  FSM_ADD_TRANSITION(CHAOS_EVENT_SEQUENCE_DRIVE, sequencer_state_drive);
   FSM_ADD_TRANSITION(CHAOS_EVENT_SEQUENCE_IDLE, sequencer_state_idle);
   FSM_ADD_TRANSITION(CHAOS_EVENT_SEQUENCE_RESET, sequencer_state_charge);
 }
 
 FSM_STATE_TRANSITION(sequencer_state_drive) {
   FSM_ADD_TRANSITION(CHAOS_EVENT_SEQUENCE_EMERGENCY, sequencer_state_emergency);
+  FSM_ADD_TRANSITION(CHAOS_EVENT_SEQUENCE_CHARGE, sequencer_state_charge);
   FSM_ADD_TRANSITION(CHAOS_EVENT_SEQUENCE_IDLE, sequencer_state_idle);
   FSM_ADD_TRANSITION(CHAOS_EVENT_SEQUENCE_RESET, sequencer_state_drive);
+}
+
+// Return true if setup succeeded. Return false if we are blocked waiting on an async event to
+// finish. If this returns false the caller should exit immediately.
+static bool prv_sequencer_setup_common(void) {
+  if (s_storage.awaiting_response) {
+    relay_retry_service_fail_fast();
+    s_pending_transition = true;
+    // We actually transitioned so just reset the state until it succeeds.
+    event_raise(CHAOS_EVENT_SEQUENCE_RESET, SEQUENCER_EMPTY_DATA);
+    return false;
+  }
+  s_pending_transition = false;
+  prv_clear_filters();
+  return true;
 }
 
 // FSM Transitions
 static void prv_sequencer_state_emergency(FSM *fsm, const Event *e, void *context) {
   (void)fsm;
   (void)e;
-  prv_clear_filters();
+  if (!prv_sequencer_setup_common()) {
+    return;
+  }
   prv_add_filter(CHAOS_EVENT_RELAY_ERROR);
   prv_add_filter(CHAOS_EVENT_RELAY_OPENED);
-
   sequencer_init((SequencerStorage *)context, (SequencerEventPair *)s_emergency_events,
                  SIZEOF_ARRAY(s_emergency_events));
 }
@@ -211,7 +231,9 @@ static void prv_sequencer_state_emergency(FSM *fsm, const Event *e, void *contex
 static void prv_sequencer_state_idle(FSM *fsm, const Event *e, void *context) {
   (void)fsm;
   (void)e;
-  prv_clear_filters();
+  if (!prv_sequencer_setup_common()) {
+    return;
+  }
   sequencer_init((SequencerStorage *)context, (SequencerEventPair *)s_idle_events,
                  SIZEOF_ARRAY(s_idle_events));
 }
@@ -219,7 +241,9 @@ static void prv_sequencer_state_idle(FSM *fsm, const Event *e, void *context) {
 static void prv_sequencer_state_charge(FSM *fsm, const Event *e, void *context) {
   (void)fsm;
   (void)e;
-  prv_clear_filters();
+  if (!prv_sequencer_setup_common()) {
+    return;
+  }
   sequencer_init((SequencerStorage *)context, (SequencerEventPair *)s_charge_events,
                  SIZEOF_ARRAY(s_charge_events));
 }
@@ -227,7 +251,9 @@ static void prv_sequencer_state_charge(FSM *fsm, const Event *e, void *context) 
 static void prv_sequencer_state_drive(FSM *fsm, const Event *e, void *context) {
   (void)fsm;
   (void)e;
-  prv_clear_filters();
+  if (!prv_sequencer_setup_common()) {
+    return;
+  }
   sequencer_init((SequencerStorage *)context, (SequencerEventPair *)s_drive_events,
                  SIZEOF_ARRAY(s_drive_events));
 }
@@ -244,7 +270,21 @@ void sequencer_fsm_init(void) {
 }
 
 StatusCode sequencer_fsm_publish_next_event(const Event *previous_event) {
-  if (prv_is_filtered_id(previous_event->id)) {
+  // Try to transition if the event is a sequence transition.
+  if (previous_event->id == CHAOS_EVENT_SEQUENCE_IDLE ||
+      previous_event->id == CHAOS_EVENT_SEQUENCE_CHARGE ||
+      previous_event->id == CHAOS_EVENT_SEQUENCE_DRIVE ||
+      previous_event->id == CHAOS_EVENT_SEQUENCE_EMERGENCY ||
+      previous_event->id == CHAOS_EVENT_SEQUENCE_RESET) {
+    if (!fsm_process_event(&s_sequencer_fsm, previous_event)) {
+      LOG_WARN("Failed transition: %u %u\n", previous_event->id, previous_event->data);
+      return status_code(STATUS_CODE_INVALID_ARGS);
+    }
+    return STATUS_CODE_OK;
+  }
+
+  // Filter out both filtered messages. Also filter all events if there is a pending_transition.
+  if (prv_is_filtered_id(previous_event->id) || s_pending_transition) {
     return STATUS_CODE_OK;
   }
 
@@ -257,19 +297,6 @@ StatusCode sequencer_fsm_publish_next_event(const Event *previous_event) {
     // Not in the range of handled events. This isn't necessarily invalid but won't be handled so
     // return unknown.
     return status_code(STATUS_CODE_UNKNOWN);
-  }
-
-  // Try to transition if the event is a sequence transition.
-  if (previous_event->id == CHAOS_EVENT_SEQUENCE_IDLE ||
-      previous_event->id == CHAOS_EVENT_SEQUENCE_CHARGE ||
-      previous_event->id == CHAOS_EVENT_SEQUENCE_DRIVE ||
-      previous_event->id == CHAOS_EVENT_SEQUENCE_EMERGENCY ||
-      previous_event->id == CHAOS_EVENT_SEQUENCE_RESET) {
-    if (!fsm_process_event(&s_sequencer_fsm, previous_event)) {
-      LOG_WARN("Failed transition: %u %u\n", previous_event->id, previous_event->data);
-      return status_code(STATUS_CODE_INVALID_ARGS);
-    }
-    return STATUS_CODE_OK;
   }
 
   return sequencer_advance(&s_storage, previous_event);
