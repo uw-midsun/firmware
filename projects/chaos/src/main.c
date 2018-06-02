91b94f45ae4246e4053a670fd80da06a86bc87c2
ckitagawa <calderk2010@gmail.com>
2018-06-02 16:54:52 -0400

    ELEC-105: Chaos Main

--- projects/chaos/src/main.c
+++ projects/chaos/src/main.c
@@ -1,46 +1,40 @@
-// WARNING: Temporary test main! Not for use in vehicle.
-
 #include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
 #include <stdlib.h>
 
 #include "adc.h"
+#include "bps_heartbeat.h"
 #include "can.h"
-#include "can_msg_defs.h"
-#include "can_unpack.h"
+#include "can_ack.h"
 #include "chaos_config.h"
 #include "chaos_events.h"
+#include "charger.h"
+#include "delay.h"
+#include "emergency_fault.h"
 #include "event_queue.h"
 #include "gpio.h"
+#include "gpio_fsm.h"
 #include "gpio_it.h"
-#include "gpio_seq.h"
 #include "interrupt.h"
-#include "log.h"
 #include "power_path.h"
+#include "powertrain_heartbeat.h"
+#include "relay.h"
+#include "relay_retry_service.h"
+#include "sequencer_fsm.c"
 #include "soft_timer.h"
+#include "state_handler.h"
 #include "status.h"
+#include "wait.h"
 
-#define CHAOS_NUM_RX_HANDLERS 1
+#define CHAOS_NUM_RX_HANDLERS 10
 
+static CANAckRequests s_can_ack_requests;
 static CANStorage s_can_storage;
 static CANRxHandler s_rx_handlers[CHAOS_NUM_RX_HANDLERS];
+static EmergencyFaultStorage s_emergency_storage;
+static RelayRetryServiceStorage s_retry_storage;
 
-static StatusCode prv_handle_uvov(const CANMessage *msg, void *context, CANAckStatus *ack_reply) {
-  (void)ack_reply;
-  (void)context;
-  uint8_t dcdc_ov = 0;
-  uint8_t dcdc_uv = 0;
-  uint8_t aux_ov = 0;
-  uint8_t aux_uv = 0;
-  CAN_UNPACK_OVUV_DCDC_AUX(msg, &dcdc_ov, &dcdc_uv, &aux_ov, &aux_uv);
-  LOG_DEBUG("Caught UVOV Message | AUX - UV: %d OV: %d | DCDC - UV: %d OV: %d\n", aux_uv, aux_ov,
-            dcdc_uv, dcdc_ov);
-  return STATUS_CODE_OK;
-}
-
-// Temporary debug main for testing.
 int main(void) {
+  // Common
   event_queue_init();
   interrupt_init();
   soft_timer_init();
@@ -48,6 +42,7 @@ int main(void) {
   gpio_it_init();
   adc_init(ADC_MODE_SINGLE);
 
+  // CAN
   CANSettings can_settings = {
     .device_id = SYSTEM_CAN_DEVICE_CHAOS,
     .bitrate = CAN_HW_BITRATE_125KBPS,
@@ -56,59 +51,76 @@ int main(void) {
     .fault_event = CHAOS_EVENT_CAN_FAULT,
     .tx = { GPIO_PORT_A, 12 },
     .rx = { GPIO_PORT_A, 11 },
-    .loopback = true,
+    .loopback = false,
   };
   can_init(&can_settings, &s_can_storage, s_rx_handlers, SIZEOF_ARRAY(s_rx_handlers));
-  can_register_rx_handler(SYSTEM_CAN_MESSAGE_OVUV_DCDC_AUX, prv_handle_uvov, NULL);
+  can_ack_init(&s_can_ack_requests);
 
+  // GPIO
   ChaosConfig *cfg = chaos_config_load();
-  const GPIOAddress addrs[] = {
-    cfg->telemetry_power,   cfg->array_sense_power,     cfg->rear_camera_power,
-    cfg->themis_power,      cfg->driver_display_power,  cfg->front_lights_power,
-    cfg->battery_box_power, cfg->motor_interface_power, cfg->rear_lights_power,
-    cfg->pjb_fan,           cfg->spare_protected_power, cfg->spare_unprotected_power,
+  gpio_fsm_init(cfg);
+
+  // Heartbeats
+  bps_heartbeat_init();  // Use the auto start feature to start the watchdog.
+  powertrain_heartbeat_init();
+
+  // Power Path
+  power_path_init(&cfg->power_path);
+  // AUX Battery Monitoring.
+  power_path_source_monitor_enable(&cfg->power_path.aux_bat, CHAOS_CONFIG_POWER_PATH_PERIOD_MS);
+
+  // Relays
+  RelaySettings relay_settings = {
+    .battery_main_power_pin = cfg->battery_box_power,
+    .battery_slave_power_pin = cfg->battery_box_power,
+    .motor_power_pin = cfg->motor_interface_power,
+    .solar_front_power_pin = cfg->array_sense_power,
+    .solar_rear_power_pin = cfg->array_sense_power,
+    .loopback = false,
   };
+  relay_init(&relay_settings);
 
-  const GPIOSettings settings = {
-    .state = GPIO_STATE_HIGH,
-    .alt_function = GPIO_ALTFN_NONE,
-    .direction = GPIO_DIR_OUT,
-    .resistor = GPIO_RES_NONE,
-  };
+  // Sequencer
+  sequencer_fsm_init();
 
-  StatusCode code =
-      gpio_seq_init_pins(addrs, SIZEOF_ARRAY(addrs), &settings, CHAOS_CONFIG_GPIO_OPEN_DELAY_US);
-  if (!status_ok(code)) {
-    LOG_CRITICAL("Failed to activate GPIO pins. StatusCode: %d\n", code);
-    return EXIT_FAILURE;
-  }
+  // Chaos is considered to be in the Idle state at this point and will only begin to transition
+  // once it receives input from driver controls. To do so we enable the state handler and other
+  // CAN services below now that Chaos is in what is considered to be a valid state.
 
-  code = power_path_init(&cfg->power_path);
-  if (!status_ok(code)) {
-    LOG_CRITICAL("Failed to activate Power Path. StatusCode: %d\n", code);
-    return EXIT_FAILURE;
-  }
+  // CAN services
+  charger_init();
+  emergency_fault_clear(&s_emergency_storage);
+  state_handler_init();
 
-  code =
-      power_path_source_monitor_enable(&cfg->power_path.aux_bat, CHAOS_CONFIG_POWER_PATH_PERIOD_MS);
-  if (!status_ok(code)) {
-    LOG_CRITICAL("Failed to activate Power Path Source AUX BAT. StatusCode: %d\n", code);
-    return EXIT_FAILURE;
-  }
-
-  code = power_path_source_monitor_enable(&cfg->power_path.dcdc, CHAOS_CONFIG_POWER_PATH_PERIOD_MS);
-  if (!status_ok(code)) {
-    LOG_CRITICAL("Failed to activate Power Path Source DCDC. StatusCode: %d\n", code);
-    return EXIT_FAILURE;
-  }
-
-  Event e;
+  // Main loop
+  Event e = { 0 };
+  StatusCode status = NUM_STATUS_CODES;
   while (true) {
+    // Tight event loop
     do {
-      code = event_process(&e);
-    } while (code != STATUS_CODE_OK);
+      status = event_process(&e);
+      // TODO(ELEC-105): Validate nothing gets stuck here.
+      if (status == STATUS_CODE_EMPTY) {
+        wait();
+      }
+    } while (status != STATUS_CODE_OK);
+
+    // Event Processing:
+
+    // TODO(ELEC-105): At least one of the following should respond with either a boolean true or
+    // a STATUS_CODE_OK for each emitted message. Consider adding a requirement that this is the
+    // case with a failure resulting in faulting into Emergency.
     fsm_process_event(CAN_FSM, &e);
+    emergency_fault_process_event(&s_emergency_storage, &e);
+    gpio_fsm_process_event(&e);
+    powertrain_heartbeat_process_event(&e);
+    power_path_process_event(&cfg->power_path, &e);
+    charger_process_event(&e);
+    relay_process_event(&e);
+    relay_retry_service_update(&e);
+    sequencer_fsm_publish_next_event(&e);
   }
 
+  // Not reached.
   return EXIT_SUCCESS;
 }
