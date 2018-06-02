ff16b65243d7a9cb59c14fdddf7201168e196f56
Max Hao <myhao@edu.uwaterloo.ca>
2018-06-02 21:35:25 +0000

    ELEC-314: Modify main.c to fix compiler errors

--- projects/pwm_input_test/src/main.c
+++ projects/pwm_input_test/src/main.c
@@ -1,7 +1,8 @@
-#include "pwm_input.h"
-#include "pwm.h"
 #include "gpio.h"
 #include "interrupt.h"
+#include "pwm.h"
+#include "pwm_input.h"
+#include "stm32f0xx.h"
 #include "wait.h"
 
 uint16_t TIM3_IRQHandler(void) {
@@ -9,28 +10,24 @@ uint16_t TIM3_IRQHandler(void) {
 
   uint16_t IC2Value = TIM_GetCapture2(TIM3);
 
-  if (IC2Value != 0)
-  {
+  if (IC2Value != 0) {
     return (TIM_GetCapture1(TIM3) * 100) / IC2Value;
 
     // Frequency = SystemCoreClock / IC2Value;
-  }
-  else
-  {
+  } else {
     return 0;
   }
 }
 
 int main(void) {
-
   uint16_t period = 1000;
 
-  pwm_init(TIM3, 1000);
-  pwm_set_dc(TIM3, 50);
+  pwm_init(PWM_TIMER_3, 1000);
+  pwm_set_dc(PWM_TIMER_3, 50);
   gpio_init();
 
   GPIOAddress output = {
-    .port = GPIO_PORT_C, 
+    .port = GPIO_PORT_C,
     .pin = 9,
   };
 
@@ -41,5 +38,4 @@ int main(void) {
 
   gpio_init_pin(&output, &output_settings);
   pwm_input_init();
-
 }
