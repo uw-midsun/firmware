e63de0eb855772decadd8f90fa6fafdbe4aa86a4
ckitagawa <calderk2010@gmail.com>
2018-06-02 12:07:40 -0400

    ELEC-105: Add extensive sequencer tests

--- projects/chaos/src/sequencer_fsm.c
+++ projects/chaos/src/sequencer_fsm.c
@@ -21,6 +21,7 @@ static FSM s_sequencer_fsm;
 static SequencerStorage s_storage;
 
 static uint8_t s_retries = 0;
+static bool s_pending_transition = false;
 static uint16_t s_filtered_ids[NUM_SEQUENCER_FSM_FILTERS] = { 0 };
 
 // Adds a filter on |id| to ignore it until it is cleared.
@@ -225,10 +226,12 @@ static bool prv_sequencer_setup_common(void) {
     relay_retry_service_fail_fast();
     // We actually transitioned so just reset the state until it succeeds. Note this needs to be
     // normal priority to allow events to flush. This is to flush an event we are waiting for
-    // (i.e. relay transition) this will take a  mas of 15 ms to succeed.
+    // (i.e. relay transition) this will take a max of 15 ms to succeed.
     event_raise(CHAOS_EVENT_SEQUENCE_RESET, SEQUENCER_EMPTY_DATA);
+    s_pending_transition = true;
     return false;
   }
+  s_pending_transition = false;
   s_retries = 0;
   prv_clear_filters();
   return true;
@@ -243,6 +246,7 @@ static void prv_sequencer_state_emergency(FSM *fsm, const Event *e, void *contex
   }
   prv_add_filter(CHAOS_EVENT_RELAY_ERROR);
   prv_add_filter(CHAOS_EVENT_RELAY_OPENED);
+  event_raise(CHAOS_EVENT_SET_RELAY_RETRIES, RELAY_RETRY_SERVICE_UNLIMITED_ATTEMPTS);
   sequencer_init((SequencerStorage *)context, (SequencerEventPair *)s_emergency_events,
                  SIZEOF_ARRAY(s_emergency_events));
 }
@@ -253,6 +257,7 @@ static void prv_sequencer_state_idle(FSM *fsm, const Event *e, void *context) {
   if (!prv_sequencer_setup_common()) {
     return;
   }
+  event_raise(CHAOS_EVENT_SET_RELAY_RETRIES, RELAY_RETRY_SERVICE_DEFAULT_ATTEMPTS);
   sequencer_init((SequencerStorage *)context, (SequencerEventPair *)s_idle_events,
                  SIZEOF_ARRAY(s_idle_events));
 }
@@ -263,6 +268,7 @@ static void prv_sequencer_state_charge(FSM *fsm, const Event *e, void *context) 
   if (!prv_sequencer_setup_common()) {
     return;
   }
+  event_raise(CHAOS_EVENT_SET_RELAY_RETRIES, RELAY_RETRY_SERVICE_DEFAULT_ATTEMPTS);
   sequencer_init((SequencerStorage *)context, (SequencerEventPair *)s_charge_events,
                  SIZEOF_ARRAY(s_charge_events));
 }
@@ -273,6 +279,7 @@ static void prv_sequencer_state_drive(FSM *fsm, const Event *e, void *context) {
   if (!prv_sequencer_setup_common()) {
     return;
   }
+  event_raise(CHAOS_EVENT_SET_RELAY_RETRIES, RELAY_RETRY_SERVICE_DEFAULT_ATTEMPTS);
   sequencer_init((SequencerStorage *)context, (SequencerEventPair *)s_drive_events,
                  SIZEOF_ARRAY(s_drive_events));
 }
@@ -280,6 +287,7 @@ static void prv_sequencer_state_drive(FSM *fsm, const Event *e, void *context) {
 // Public interface
 void sequencer_fsm_init(void) {
   prv_clear_filters();
+  s_pending_transition = false;
   memset(&s_storage, 0, sizeof(s_storage));
   fsm_state_init(sequencer_state_emergency, prv_sequencer_state_emergency);
   fsm_state_init(sequencer_state_idle, prv_sequencer_state_idle);
@@ -307,8 +315,14 @@ StatusCode sequencer_fsm_publish_next_event(const Event *previous_event) {
     return STATUS_CODE_OK;
   }
 
-  // Handle a totally faulted relay in a special way.
+  // Handle a totally faulted relay in a special way, ignore this kind of failure if we are
+  // transitioning as the relay may not have reached the maximal retry limit.
   if (previous_event->id == CHAOS_EVENT_RELAY_ERROR) {
+    if (s_pending_transition) {
+      // Force the awaiting flag to clear as we the relay failed fast.
+      s_storage.awaiting_response = false;
+      return STATUS_CODE_OK;
+    }
     // If we aren't in the emergency state we need to switch to that state. This event will not be
     // raised in the emergency state.
     return event_raise_priority(EVENT_PRIORITY_HIGH, CHAOS_EVENT_SEQUENCE_EMERGENCY,
--- projects/chaos/test/test_sequencer_fsm.c
+++ projects/chaos/test/test_sequencer_fsm.c
@@ -4,12 +4,23 @@
 
 #include "chaos_events.h"
 #include "event_queue.h"
+#include "interrupt.h"
 #include "log.h"
+#include "soft_timer.h"
 #include "test_helpers.h"
 
+static void prv_raise_with_delay_cb(SoftTimerID id, void *context) {
+  (void)id;
+  Event *e = context;
+  event_raise(e->id, e->data);
+  LOG_DEBUG("Raised: %d:%d\n", e->id, e->data);
+}
+
 void setup_test(void) {
+  interrupt_init();
   event_queue_init();
   sequencer_fsm_init();
+  soft_timer_init();
 }
 
 void teardown_test(void) {}
@@ -49,6 +60,11 @@ void test_sequencer_fsm_run(void) {
       } else {
         LOG_DEBUG("Sequencer Raised Event: %d : %d\n", present_event.id, present_event.data);
       }
+
+      if (present_event.id <= NUM_CHAOS_EVENTS_CAN) {
+        // Skip ignored events.
+        continue;
+      }
       // At this point |present_event| is considered to be |prev_event| as it has passed through all
       // the other FSMs in the event loop.
       prev_event = present_event;
@@ -57,8 +73,8 @@ void test_sequencer_fsm_run(void) {
   }
 }
 
-// Validate that the reset mechanism works as intended.
-void test_sequencer_fsm_reset(void) {
+// Validate that the reset mechanism works as intended for relays.
+void test_sequencer_fsm_reset_relay(void) {
   const Event state_event = { .id = CHAOS_EVENT_SEQUENCE_DRIVE };
   Event prev_event = { 0 };
   Event present_event = { 0 };
@@ -86,9 +102,246 @@ void test_sequencer_fsm_reset(void) {
         present_event.data = prev_event.data;
       }
     }
+    if (present_event.id <= NUM_CHAOS_EVENTS_CAN) {
+      // Skip ignored events.
+      continue;
+    }
     // At this point |present_event| is considered to be |prev_event| as it has passed through all
     // the other FSMs in the event loop.
     prev_event = present_event;
     seq_status = sequencer_fsm_publish_next_event(&present_event);
   }
+  // Ensure the reset actually happened.
+  TEST_ASSERT_FALSE(reset);
+}
+
+// Validate that the reset mechanism works as intended for sequences.
+void test_sequencer_fsm_reset_events(void) {
+  const Event state_event = { .id = CHAOS_EVENT_SEQUENCE_DRIVE };
+  Event prev_event = { 0 };
+  Event present_event = { 0 };
+
+  bool reset = true;
+
+  TEST_ASSERT_OK(event_raise(state_event.id, 0));
+  StatusCode seq_status = STATUS_CODE_OK;
+  StatusCode event_status = STATUS_CODE_OK;
+  while (seq_status != STATUS_CODE_RESOURCE_EXHAUSTED) {
+    TEST_ASSERT_OK(seq_status);
+    event_status = event_process(&present_event);
+    if (event_status != STATUS_CODE_OK) {
+      if (prev_event.id == CHAOS_EVENT_CLOSE_RELAY) {
+        present_event.id = CHAOS_EVENT_RELAY_CLOSED;
+        present_event.data = prev_event.data;
+      } else if (prev_event.id == CHAOS_EVENT_OPEN_RELAY) {
+        present_event.id = CHAOS_EVENT_RELAY_OPENED;
+        present_event.data = prev_event.data;
+      }
+    }
+    // Cause a major error!
+    if (reset && present_event.id == CHAOS_EVENT_GPIO_DRIVE) {
+      present_event.id = CHAOS_EVENT_GPIO_IDLE;
+      TEST_ASSERT_OK(sequencer_fsm_publish_next_event(&present_event));
+      // Check reset event.
+      TEST_ASSERT_OK(event_process(&present_event));
+      // Raise some garbage data to check that flushing is working.
+      TEST_ASSERT_OK(event_raise(NUM_CHAOS_EVENTS_FSM, 99));
+      TEST_ASSERT_OK(
+          event_raise_priority(EVENT_PRIORITY_HIGH, present_event.id, present_event.data));
+      reset = false;
+    } else {
+      if (present_event.id <= NUM_CHAOS_EVENTS_CAN) {
+        // Skip ignored events.
+        continue;
+      }
+      // At this point |present_event| is considered to be |prev_event| as it has passed through all
+      // the other FSMs in the event loop.
+      prev_event = present_event;
+      seq_status = sequencer_fsm_publish_next_event(&present_event);
+    }
+  }
+  // Ensure the reset actually happened.
+  TEST_ASSERT_FALSE(reset);
+  TEST_ASSERT_EQUAL(CHAOS_EVENT_SEQUENCE_DRIVE_DONE, present_event.id);
+}
+
+// Validate that a transition partway through a transition resolves correctly
+void test_sequencer_fsm_interrupted(void) {
+  const Event state_event = { .id = CHAOS_EVENT_SEQUENCE_DRIVE };
+  Event prev_event = { 0 };
+  Event present_event = { 0 };
+
+  bool transition = true;
+
+  TEST_ASSERT_OK(event_raise(state_event.id, 0));
+  StatusCode seq_status = STATUS_CODE_OK;
+  StatusCode event_status = STATUS_CODE_OK;
+  while (seq_status != STATUS_CODE_RESOURCE_EXHAUSTED) {
+    TEST_ASSERT_OK(seq_status);
+    event_status = event_process(&present_event);
+    if (event_status != STATUS_CODE_OK) {
+      if (prev_event.id == CHAOS_EVENT_CLOSE_RELAY) {
+        present_event.id = CHAOS_EVENT_RELAY_CLOSED;
+        present_event.data = prev_event.data;
+      } else if (prev_event.id == CHAOS_EVENT_OPEN_RELAY) {
+        present_event.id = CHAOS_EVENT_RELAY_OPENED;
+        present_event.data = prev_event.data;
+      }
+    }
+    // Cause a transition.
+    if (transition && present_event.id == CHAOS_EVENT_GPIO_DRIVE) {
+      present_event.id = CHAOS_EVENT_SEQUENCE_IDLE;
+      TEST_ASSERT_OK(sequencer_fsm_publish_next_event(&present_event));
+      // Check reset event.
+      TEST_ASSERT_OK(event_process(&present_event));
+      // Raise some garbage data to check that flushing is working.
+      TEST_ASSERT_OK(event_raise(NUM_CHAOS_EVENTS_FSM, 99));
+      TEST_ASSERT_OK(
+          event_raise_priority(EVENT_PRIORITY_HIGH, present_event.id, present_event.data));
+      transition = false;
+    } else {
+      if (present_event.id <= NUM_CHAOS_EVENTS_CAN) {
+        // Skip ignored events.
+        continue;
+      }
+      // At this point |present_event| is considered to be |prev_event| as it has passed through all
+      // the other FSMs in the event loop.
+      prev_event = present_event;
+      seq_status = sequencer_fsm_publish_next_event(&present_event);
+    }
+  }
+  // Ensure the transition actually happened.
+  TEST_ASSERT_FALSE(transition);
+  TEST_ASSERT_EQUAL(CHAOS_EVENT_SEQUENCE_IDLE_DONE, present_event.id);
+}
+
+// Validate that a transition partway through an awaiting transition works.
+void test_sequencer_fsm_interrupted_awaiting(void) {
+  const Event state_event = { .id = CHAOS_EVENT_SEQUENCE_DRIVE };
+  Event prev_event = { 0 };
+  Event present_event = { 0 };
+
+  bool transition = true;
+  Event delayed_event = { .id = CHAOS_EVENT_RELAY_CLOSED };
+
+  TEST_ASSERT_OK(event_raise(state_event.id, 0));
+  StatusCode seq_status = STATUS_CODE_OK;
+  StatusCode event_status = STATUS_CODE_OK;
+  while (seq_status != STATUS_CODE_RESOURCE_EXHAUSTED) {
+    TEST_ASSERT_OK(seq_status);
+    event_status = event_process(&present_event);
+    if (event_status != STATUS_CODE_OK) {
+      if (prev_event.id == CHAOS_EVENT_CLOSE_RELAY) {
+        present_event.id = CHAOS_EVENT_RELAY_CLOSED;
+        present_event.data = prev_event.data;
+      } else if (prev_event.id == CHAOS_EVENT_OPEN_RELAY) {
+        present_event.id = CHAOS_EVENT_RELAY_OPENED;
+        present_event.data = prev_event.data;
+      }
+    }
+    // Cause a transition.
+    if (transition && present_event.id == CHAOS_EVENT_CLOSE_RELAY) {
+      TEST_ASSERT_OK(sequencer_fsm_publish_next_event(&present_event));
+      present_event.id = CHAOS_EVENT_SEQUENCE_IDLE;
+      // Raise some garbage data to check that flushing is working.
+      TEST_ASSERT_OK(event_raise(NUM_CHAOS_EVENTS_FSM, 99));
+      TEST_ASSERT_OK(event_raise_priority(EVENT_PRIORITY_HIGH, present_event.id, 0));
+      delayed_event.data = present_event.data;
+      TEST_ASSERT_OK(soft_timer_start_millis(15, prv_raise_with_delay_cb, &delayed_event, NULL));
+      transition = false;
+    } else {
+      if (present_event.id <= NUM_CHAOS_EVENTS_CAN) {
+        // Skip ignored events.
+        continue;
+      }
+      // At this point |present_event| is considered to be |prev_event| as it has passed through all
+      // the other FSMs in the event loop.
+      prev_event = present_event;
+      seq_status = sequencer_fsm_publish_next_event(&present_event);
+    }
+  }
+  // Ensure the transition actually happened.
+  TEST_ASSERT_FALSE(transition);
+  TEST_ASSERT_EQUAL(CHAOS_EVENT_SEQUENCE_IDLE_DONE, present_event.id);
+}
+
+// Validate that a transition partway through an awaiting transition works in the case of an error.
+void test_sequencer_fsm_interrupted_awaiting_error(void) {
+  const Event state_event = { .id = CHAOS_EVENT_SEQUENCE_DRIVE };
+  Event prev_event = { 0 };
+  Event present_event = { 0 };
+
+  bool transition = true;
+  Event delayed_event = { .id = CHAOS_EVENT_RELAY_ERROR };
+
+  TEST_ASSERT_OK(event_raise(state_event.id, 0));
+  StatusCode seq_status = STATUS_CODE_OK;
+  StatusCode event_status = STATUS_CODE_OK;
+  while (seq_status != STATUS_CODE_RESOURCE_EXHAUSTED) {
+    TEST_ASSERT_OK(seq_status);
+    event_status = event_process(&present_event);
+    if (event_status != STATUS_CODE_OK) {
+      if (prev_event.id == CHAOS_EVENT_CLOSE_RELAY) {
+        present_event.id = CHAOS_EVENT_RELAY_CLOSED;
+        present_event.data = prev_event.data;
+      } else if (prev_event.id == CHAOS_EVENT_OPEN_RELAY) {
+        present_event.id = CHAOS_EVENT_RELAY_OPENED;
+        present_event.data = prev_event.data;
+      }
+    }
+    // Cause a transition.
+    if (transition && present_event.id == CHAOS_EVENT_CLOSE_RELAY) {
+      TEST_ASSERT_OK(sequencer_fsm_publish_next_event(&present_event));
+      present_event.id = CHAOS_EVENT_SEQUENCE_IDLE;
+      // Raise some garbage data to check that flushing is working.
+      TEST_ASSERT_OK(event_raise(NUM_CHAOS_EVENTS_FSM, 99));
+      TEST_ASSERT_OK(event_raise_priority(EVENT_PRIORITY_HIGH, present_event.id, 0));
+      delayed_event.data = present_event.data;
+      TEST_ASSERT_OK(soft_timer_start_millis(15, prv_raise_with_delay_cb, &delayed_event, NULL));
+      transition = false;
+    } else {
+      if (present_event.id <= NUM_CHAOS_EVENTS_CAN && present_event.id != CHAOS_EVENT_RELAY_ERROR) {
+        // Skip ignored events.
+        continue;
+      }
+      // At this point |present_event| is considered to be |prev_event| as it has passed through all
+      // the other FSMs in the event loop.
+      prev_event = present_event;
+      seq_status = sequencer_fsm_publish_next_event(&present_event);
+    }
+  }
+  // Ensure the transition actually happened.
+  TEST_ASSERT_FALSE(transition);
+  TEST_ASSERT_EQUAL(CHAOS_EVENT_SEQUENCE_IDLE_DONE, present_event.id);
+}
+
+// Validate that emergency filters work.
+void test_sequencer_fsm_interrupted_emergency_noawait(void) {
+  const Event state_event = { .id = CHAOS_EVENT_SEQUENCE_EMERGENCY };
+  Event present_event = { 0 };
+
+  Event delayed_event = { .id = CHAOS_EVENT_RELAY_OPENED };
+
+  TEST_ASSERT_OK(event_raise(state_event.id, 0));
+  StatusCode seq_status = STATUS_CODE_OK;
+  while (seq_status != STATUS_CODE_RESOURCE_EXHAUSTED) {
+    TEST_ASSERT_OK(seq_status);
+    event_process(&present_event);
+    if (present_event.id == CHAOS_EVENT_OPEN_RELAY) {
+      TEST_ASSERT_OK(sequencer_fsm_publish_next_event(&present_event));
+      // Emergency doesn't expect opened events.
+      delayed_event.data = present_event.data;
+      TEST_ASSERT_OK(soft_timer_start_millis(15, prv_raise_with_delay_cb, &delayed_event, NULL));
+    } else {
+      if (present_event.id <= NUM_CHAOS_EVENTS_CAN) {
+        // Skip ignored events.
+        continue;
+      }
+      // At this point |present_event| is considered to be |prev_event| as it has passed through all
+      // the other FSMs in the event loop.
+      seq_status = sequencer_fsm_publish_next_event(&present_event);
+    }
+  }
+  // Ensure the transition actually happened.
+  TEST_ASSERT_EQUAL(CHAOS_EVENT_SEQUENCE_EMERGENCY_DONE, present_event.id);
 }
