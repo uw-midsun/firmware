12140f2475abf9076ca4b6daf4c145cf671b918b
Titus Chow <tituschow96@gmail.com>
2018-06-08 02:24:51 +0000

    ELEC-439: Documentation

--- projects/plutus/test/test_bps_heartbeat.c
+++ projects/plutus/test/test_bps_heartbeat.c
@@ -66,7 +66,7 @@ void setup_test(void) {
   can_init(&settings, &s_can, s_rx_handlers, SIZEOF_ARRAY(s_rx_handlers));
   TEST_ASSERT_OK(can_register_rx_handler(SYSTEM_CAN_MESSAGE_BPS_HEARTBEAT, prv_bps_rx, NULL));
 
-  // Closed relay by default
+  // Closed relay for testing since we want to make sure it opens on fault.
   s_relay_state = EE_RELAY_STATE_CLOSE;
   s_ack_status = CAN_ACK_STATUS_OK;
   s_heartbeat_state = NUM_EE_BPS_HEARTBEAT_STATES;
