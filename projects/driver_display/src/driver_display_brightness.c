c358b4256662692cc861d34aff6cb4ee2012aded
Andy Jin <andy.jin17@gmail.com>
2018-06-11 23:04:07 +0000

    ELEC-434: Driver display update

--- projects/driver_display/src/driver_display_brightness.c
+++ projects/driver_display/src/driver_display_brightness.c
@@ -5,16 +5,51 @@
 #include "gpio.h"
 #include "log.h"
 #include "misc.h"
+#include "persist.h"
 #include "soft_timer.h"
+#include "crc32.h"
+#include "status.h"
 
 #include "driver_display_brightness.h"
 #include "driver_display_config.h"
+
+static PersistStorage s_persist;
+static DriverDisplayBrightnessData data;
+
+void driver_display_brightness_timer_callback(SoftTimerID timer_id, void *context){
+  flash_erase(DRIVER_DISPLAY_CONFIG_PERSIST_PAGE);
+  persist_init(&s_persist, DRIVER_DISPLAY_CONFIG_PERSIST_PAGE, &data, sizeof(data));
+}
 
 void driver_display_brightness_calibration(GPIOAddress adc_address) {
   // Configures the lower and upper bounds of the given photodiode
   // Needed to calculate percentage (current brightness value / max brightness value)
   // The percentage is then used in pwm_set_dc which takes in a percentage value
+  ADCChannel adc_channel;
+  adc_get_channel(adc_address, &adc_channel);
+  printf("starting config mode");
+
+  uint16_t reading;
+  soft_timer_start_seconds(DRIVER_DISPLAY_CONFIG_REFRESH_PERIOD, driver_display_brightness_timer_callback, (void*)&reading, DRIVER_DISPLAY_CONFIG_REFRESH_TIMER);
+  data.persistStorage = &s_persist;
+
+  // Loop for certain period of time using soft timer
+  while (soft_timer_remaining_time(0) != 0 ){
+    adc_register_callback(adc_channel, driver_display_brightness_callback, &reading);
+    data.max_brightness = MAX(data.max_brightness, reading);
+    data.min_brightness = MIN(data.max_brightness, reading);
+    data.range_brightness = data.max_brightness - data.min_brightness;
+  }
+     
+  printf("Min: %d, Max: %d, Range: %d", data.min_brightness, data.max_brightness, data.range_brightness);
+  printf("\n");
 }
+
+void driver_display_brightness_init(void){
+  DriverDisplayBrightnessData new_data = {0};
+  persist_init(&s_persist, DRIVER_DISPLAY_CONFIG_PERSIST_PAGE, &new_data, sizeof(new_data));
+  data = new_data;
+} 
 
 void driver_display_brightness_callback(ADCChannel adc_channel, void *context) {
   uint16_t *adc_reading = (uint16_t *)context;
@@ -25,13 +60,14 @@ void driver_display_brightness_callback(ADCChannel adc_channel, void *context) {
 void driver_display_brightness_read(GPIOAddress adc_address) {
   uint16_t reading;
   ADCChannel adc_channel;
+
   adc_get_channel(adc_address, &adc_channel);
   adc_register_callback(adc_channel, driver_display_brightness_callback, &reading);
 
   // Convert the raw reading into a percentage of max reading to then be passed into pwm_set_dc to
   // adjust brightness accordingly
   uint16_t percent_reading =
-      ((reading - DRIVER_DISPLAY_CONFIG_ADC_MIN) * 100) / DRIVER_DISPLAY_CONFIG_ADC_RANGE;
+      ((reading - data.min_brightness) * 100) / data.range_brightness;
 
   // Temporary printing for debug purposes
   printf("adc reading: %d percent reading: %d", reading, percent_reading);
--- projects/driver_display/src/main.c
+++ projects/driver_display/src/main.c
@@ -9,15 +9,22 @@
 #include "interrupt.h"
 #include "pwm.h"
 #include "soft_timer.h"
+#include "flash.h"
+#include "crc32.h"
 
 #include "driver_display_brightness.h"
 #include "driver_display_config.h"
+
+void timer_callback(SoftTimerID timer_id, void *context){
+}
 
 int main(void) {
   // Enable various peripherals
   interrupt_init();
   soft_timer_init();
   gpio_init();
+  flash_init();
+  crc32_init();
 
   adc_init(ADC_MODE_CONTINUOUS);
 
@@ -25,14 +32,39 @@ int main(void) {
   driver_display_config();
   GPIOAddress adc_address = driver_display_config_get_adc();
 
+  // Configuration mode currenty commented out 
+  // Would need to be run but currently no GPIO set to active the config mode 
+  //driver_display_brightness_calibration(adc_address); 
+
+static const DriverDisplayConfigGpio screen_info[] = {
+  [DRIVER_DISPLAY_SCREEN1] = { .address = { DRIVER_DISPLAY_CONFIG_SCREEN1_PORT, DRIVER_DISPLAY_CONFIG_SCREEN1_PIN },
+                               .timer = DRIVER_DISPLAY_CONFIG_SCREEN1_TIMER,
+                               .frequency = DRIVER_DISPLAY_CONFIG_SCREEN1_FREQ },
+};
+
+GPIOSettings screen_pwm_settings = { .direction = GPIO_DIR_OUT,
+                                     .state = GPIO_STATE_HIGH,
+                                     .resistor = GPIO_RES_PULLUP,
+                                     .alt_function = GPIO_ALTFN_4 };
+
+  gpio_init_pin(&screen_info[0].address, &screen_pwm_settings);
+  pwm_init(screen_info[0].timer, 500);  // frequency converted to microseconds
+  pwm_set_dc(screen_info[0].timer, 50);       // set the screen brightness to 50% initially
+
+  //Start the soft timer
+  /*uint8_t reading;
+  driver_display_brightness_init();
+  soft_timer_start_seconds(DRIVER_DISPLAY_CONFIG_REFRESH_PERIOD, timer_callback, (void*)&reading, DRIVER_DISPLAY_CONFIG_REFRESH_TIMER);*/
+
   // Begin superloop
   while (true) {
     // Read the value (need to add delay so it's not reading every moment)
     // Will probably have a refresh rate of something like every 5s
-    driver_display_brightness_read(adc_address);
-    // Adding temporary delay of 5s to test
-    delay_s(5);
+    /*if (soft_timer_remaining_time(DRIVER_DISPLAY_CONFIG_REFRESH_TIMER) == 0 ){
+      // When the timer runs out restart it and read the brightness values 
+      soft_timer_start_seconds(DRIVER_DISPLAY_CONFIG_REFRESH_PERIOD, timer_callback, (void*)&reading, DRIVER_DISPLAY_CONFIG_REFRESH_TIMER);
+      driver_display_brightness_read(adc_address);
+    }*/
   }
-
   return 0;
 }
