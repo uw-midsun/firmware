e3feeea7a241e9d2926f1e779c6fcf2d0f00e917
Andy Jin <andy.jin17@gmail.com>
2018-06-11 23:00:59 +0000

    ELEC-443: Refactor pwm to support microseconds

--- libraries/ms-common/inc/pwm.h
+++ libraries/ms-common/inc/pwm.h
@@ -13,14 +13,14 @@
 #include "pwm_mcu.h"
 #include "status.h"
 
-// Initializes the PWM for a set timer with a period in milliseconds.
-StatusCode pwm_init(PWMTimer timer, uint16_t period_ms);
+// Initializes the PWM for a set timer with a period in microseconds.
+StatusCode pwm_init(PWMTimer timer, uint16_t period_us);
 
 // Gets the current period of a specified PWM timer.
 uint16_t pwm_get_period(PWMTimer timer);
 
-// Sets the pulse width in ms of the PWM timer. Use for high resolution control.
-StatusCode pwm_set_pulse(PWMTimer timer, uint16_t pulse_width_ms);
+// Sets the pulse width in us of the PWM timer. Use for high resolution control.
+StatusCode pwm_set_pulse(PWMTimer timer, uint16_t pulse_width_us);
 
 // Sets the duty cycle, in units of 1%, of the PWM timer. This wraps pwm_set_pulse doing the
 // necessary math to convert from 0-100% to the fraction of the period.
