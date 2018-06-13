cf067d38b2a5ace7951d10a83a0541346530e505
Titus Chow <tituschow96@gmail.com>
2018-06-13 06:52:09 +0000

    ELEC-466: Fixed powertrain heartbeat

--- projects/chaos/src/powertrain_heartbeat.c
+++ projects/chaos/src/powertrain_heartbeat.c
@@ -11,6 +11,7 @@
 #include "event_queue.h"
 #include "soft_timer.h"
 #include "status.h"
+#include "log.h"
 
 // Implementation Details
 //
@@ -54,7 +55,7 @@ static StatusCode prv_ack_cb(CANMessageID id, uint16_t device, CANAckStatus stat
   (void)device;
   (void)context;
   (void)status;
-  if (!num_remaining) {
+  if (num_remaining == 0) {
     prv_kick_watchdog();
   }
   return STATUS_CODE_OK;
