a32838983762092d0345f288bda6674416f5b93e
ckitagawa <calderk2010@gmail.com>
2018-06-12 23:44:07 -0400

    ELEC-465: Allow Delay Service to cancel

--- projects/chaos/test/test_delay_service.c
+++ projects/chaos/test/test_delay_service.c
@@ -6,6 +6,7 @@
 #include "ms_test_helpers.h"
 #include "soft_timer.h"
 #include "status.h"
+#include "test_helpers.h"
 
 void setup_test(void) {
   interrupt_init();
@@ -21,4 +22,15 @@ void test_delay_service(void) {
   TEST_ASSERT_EQUAL(STATUS_CODE_EMPTY, event_process(&e));
   MS_TEST_HELPER_AWAIT_EVENT(e);
   TEST_ASSERT_EQUAL(CHAOS_EVENT_DELAY_DONE, e.id);
+  TEST_ASSERT_EQUAL(STATUS_CODE_EMPTY, event_process(&e));
+}
+
+void test_delay_service_cancel(void) {
+  Event e = { CHAOS_EVENT_DELAY_MS, 10000 };
+  delay_service_process_event(&e);
+  TEST_ASSERT_EQUAL(STATUS_CODE_EMPTY, event_process(&e));
+  test_delay_service_cancel();
+  TEST_ASSERT_OK(event_process(&e));
+  TEST_ASSERT_EQUAL(CHAOS_EVENT_DELAY_DONE, e.id);
+  TEST_ASSERT_EQUAL(STATUS_CODE_EMPTY, event_process(&e));
 }
