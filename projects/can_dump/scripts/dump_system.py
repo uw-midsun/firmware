5b27964ea9ecb1cef6b08c1bc93eccac8418c847
Titus Chow <tituschow96@gmail.com>
2018-06-16 20:58:52 +0000

    ELEC-467: Add system dump script

--- projects/can_dump/scripts/dump_system.py
+++ projects/can_dump/scripts/dump_system.py
@@ -4,6 +4,59 @@ import socket
 import struct
 import binascii
 
+def data_relay(state):
+    """Relay state data format"""
+    return 'close' if state else 'open'
+
+def data_power_state(state):
+    """Power state data format"""
+    return ['idle', 'charge', 'drive'][state]
+
+def data_lights_state(light_id, state):
+    """Lights state data format"""
+    id_name = [
+        'High beams', 'Low beams', 'DRL',
+        'Brakes', 'Left Turn', 'Right Turn',
+        'Hazards', 'BPS Strobe'
+    ][light_id]
+    state_name = 'on' if state else 'off'
+
+    return '{}: {}'.format(id_name, state_name)
+
+def data_battery_vt(module, voltage, temperature):
+    """Battery V/T data format"""
+    return 'C{}: {:.1f}mV aux {:.1f}mV'.format(module, voltage / 10, temperature / 10)
+
+def data_battery_current(raw_lsbs):
+    """Battery Current data format"""
+    # Note that this is just an estimation - it should be updated once we have current calibration
+    return '~{}uA'.format(raw_lsbs / 2)
+
+def data_dump(*args):
+    """Generic data dump format"""
+    return ' '.join(['{}'.format(arg) for arg in args])
+
+# Name, struct format, data format 0, ...
+MESSAGE_LOOKUP = {
+    0: ('BPS Heartbeat', '<B', data_dump),
+    1: ('Chaos Fault', '', data_dump),
+    2: ('Battery relay (Main)', '<B', data_relay),
+    3: ('Battery relay (Slave)', '<B', data_relay),
+    4: ('Motor relay', '<B', data_relay),
+    5: ('Solar relay (Rear)', '<B', data_relay),
+    6: ('Solar relay (Front)', '<B', data_relay),
+    7: ('Power state', '<B', data_power_state),
+    8: ('Chaos Heartbeat', '', data_dump),
+    18: ('Drive Output', '<hhhh', data_dump),
+    19: ('Cruise Target', '<B', data_dump),
+    23: ('Lights Sync', '', data_dump),
+    24: ('Lights State', '<BB', data_lights_state),
+    32: ('Battery V/T', '<HHH', data_battery_vt),
+    33: ('Battery Current', '<i', data_battery_current),
+    36: ('Motor Velocity', '<ii', data_dump),
+    43: ('Aux & DC/DC V/C', '<HHHH', data_dump),
+}
+
 def parse_msg(can_id, data):
     """Parses and prints a system CAN message.
 
@@ -23,26 +76,19 @@ def parse_msg(can_id, data):
     msg_type = (can_id >> 4) & 0x1
     msg_id = (can_id >> 5) & 0x3f
 
-    msg_type_name = 'ACK' if msg_type == 1 else 'data'
+    msg_type_name = 'ACK' if msg_type == 1 else 'DATA'
 
-    if (msg_id == 32 and len(data) == 6):
-        # Battery voltage/temperature
-        vt_fmt = '<HHH'
-        module, voltage, aux = struct.unpack(vt_fmt, data)
-        print('C{}: {}mV aux {}mV'.format(module, voltage / 10, aux / 10))
-    elif (msg_id == 0 and msg_type == 0):
-        # BPS Heartbeat TX
-        print('BPS Heartbeat: 0x{} from {}'.format(binascii.hexlify(data).decode('ascii'),
-                                                   source_id))
-    elif (msg_id == 0 and msg_type == 1):
-        # BPS Heartbeat ACK
-        print('BPS Hearbeat ACK from {}'.format(source_id))
-    elif (msg_id == 2 or msg_id == 3):
-        # Battery relay
-        relay_name = 'main' if msg_id == 2 else 'slave'
-        state = 'open' if data == 0x0 else 'close'
-        print('Battery relay {}: {} ({}) from {}'.format(relay_name, state, msg_type_name,
-                                                         source_id))
+    if msg_id in MESSAGE_LOOKUP:
+        name, fmt, data_fn = MESSAGE_LOOKUP[msg_id]
+        if fmt:
+            unpacked_data = struct.unpack(fmt, data)
+        else:
+            unpacked_data = []
+
+        if msg_type_name == 'ACK':
+            print('{} ACK from {}'.format(name, source_id))
+        else:
+            print('{}: {}'.format(name, data_fn(*unpacked_data)))
     else:
         # Unrecognized message
         print('{} from {} ({}): 0x{}'.format(msg_id, source_id, msg_type_name,
