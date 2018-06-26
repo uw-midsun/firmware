1e31287a1a220f74fda8daad6d857294782a6855
Titus Chow <tituschow96@gmail.com>
2018-06-25 22:54:43 -0400

    ELEC-455: Added signal output FSMs

--- projects/driver_controls/src/main.c
+++ projects/driver_controls/src/main.c
@@ -26,6 +26,11 @@
 #include "pedal_fsm.h"
 #include "power_fsm.h"
 
+#include "headlight_fsm.h"
+#include "turn_signal_fsm.h"
+#include "hazards_fsm.h"
+#include "horn_fsm.h"
+
 #include "cruise.h"
 #include "drive_output.h"
 
@@ -40,7 +45,11 @@ typedef enum {
   DRIVER_CONTROLS_FSM_PEDAL,
   DRIVER_CONTROLS_FSM_DIRECTION,
   DRIVER_CONTROLS_FSM_MECH_BRAKE,
-  NUM_DRIVER_CONTROLS_FSMS
+  DRIVER_CONTROLS_FSM_HEADLIGHT,
+  DRIVER_CONTROLS_FSM_TURN_SIGNALS,
+  DRIVER_CONTROLS_FSM_HAZARDS,
+  DRIVER_CONTROLS_FSM_HORN,
+  NUM_DRIVER_CONTROLS_FSMS,
 } DriverControlsFsm;
 
 static GpioExpanderStorage s_console_expander;
@@ -122,9 +131,12 @@ int main(void) {
   heartbeat_rx_register_handler(&s_powertrain_heartbeat, SYSTEM_CAN_MESSAGE_POWERTRAIN_HEARTBEAT,
                                 heartbeat_rx_auto_ack_handler, NULL);
 
+  brake_signal_init();
+
   event_arbiter_init(&s_event_arbiter);
   DriverControlsFsmInitFn init_fns[] = {
     cruise_fsm_init, direction_fsm_init, mechanical_brake_fsm_init, pedal_fsm_init, power_fsm_init,
+    headlight_fsm_init, turn_signal_fsm_init, hazards_fsm_init, horn_fsm_init,
   };
   for (size_t i = 0; i < NUM_DRIVER_CONTROLS_FSMS; i++) {
     init_fns[i](&s_fsms[i], &s_event_arbiter);
@@ -155,6 +167,7 @@ int main(void) {
       power_distribution_controller_retry(&e);
       cruise_handle_event(cruise_global(), &e);
       event_arbiter_process_event(&s_event_arbiter, &e);
+      brake_signal_process_event(&e);
     }
   }
 }
