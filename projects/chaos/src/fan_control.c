2cc5c3e3039aa08433c4569238349e1fb688537b
ckitagawa <calderk2010@gmail.com>
2018-06-27 21:29:55 -0400

    ELEC-490: Add fan control message

---
+++ projects/chaos/src/fan_control.c
@@ -0,0 +1,14 @@
+#include "fan_control.h"
+
+#include <stdbool.h>
+
+#include "can_transmit.h"
+#include "chaos_events.h"
+#include "event_queue.h"
+#include "exported_enums.h"
+
+bool fan_controls_process_event(const Event *e) {
+  if (e->id == CHAOS_EVENT_SEQUENCE_IDLE_DONE) {
+    CAN_TRANSMIT_FAN_CONTROL(EE_FAN_CONTROL_STATE_DISABLE);
+  }
+}
