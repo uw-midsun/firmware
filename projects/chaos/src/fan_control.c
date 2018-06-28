9622e20f49ad17cced9cd20da963d7c2f0db3709
ckitagawa <calderk2010@gmail.com>
2018-06-27 23:56:18 -0400

    ELEC-490: typo

--- projects/chaos/src/fan_control.c
+++ projects/chaos/src/fan_control.c
@@ -7,7 +7,7 @@
 #include "event_queue.h"
 #include "exported_enums.h"
 
-bool fan_controls_process_event(const Event *e) {
+bool fan_control_process_event(const Event *e) {
   if (e->id == CHAOS_EVENT_SEQUENCE_IDLE_DONE) {
     CAN_TRANSMIT_FAN_CONTROL(EE_FAN_CONTROL_STATE_DISABLE);
     return true;
