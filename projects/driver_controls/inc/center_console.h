5d8b011e85e61f12298fa766a6556c84505f3c4f
Titus Chow <tituschow@users.noreply.github.com>
2018-06-27 21:04:52 -0400

    ELEC-455: Use signal outputs (#271)

--- projects/driver_controls/inc/center_console.h
+++ projects/driver_controls/inc/center_console.h
@@ -6,15 +6,11 @@
 // Also dedicates another MCP23008 to LED output for state notification.
 //
 // Raises INPUT_EVENT_CENTER_CONSOLE_* events with empty data fields.
-// Assumes all inputs other than hazards are non-latching. The power button is required to be held
-// down for the specified period before a power event will be raised.
+// Assumes all inputs other than hazards are non-latching.
 #include <assert.h>
 #include <stdint.h>
 #include "gpio_expander.h"
 #include "soft_timer.h"
-
-// How long the power button must be held to count as a press
-#define CENTER_CONSOLE_POWER_HOLD_MS 2000
 
 typedef enum {
   CENTER_CONSOLE_INPUT_POWER = 0,
@@ -28,7 +24,6 @@ typedef enum {
 } CenterConsoleInput;
 
 typedef struct CenterConsoleStorage {
-  SoftTimerID hold_timer;
 } CenterConsoleStorage;
 
 // Sets up the expander as inputs to raise the associated events
