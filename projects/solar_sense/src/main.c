ba754fb0e83e0fd90c017d7bf99c3704e94786ff
Arshankhanifar <arshankhanifar@gmail.com>
2018-07-02 20:06:21 -0400

    ELEC-502: not working piece of shit

--- projects/solar_sense/src/main.c
+++ projects/solar_sense/src/main.c
@@ -8,12 +8,16 @@
 #include "misc.h"        
 #include "soft_timer.h"  
 #include "i2c.h"
+#include "log.h"
 
 #define SOLAR_SENSE_I2C_BUS_SDA \
   { GPIO_PORT_B, 9 }
+
 #define SOLAR_SENSE_I2C_BUS_SCL \
   { GPIO_PORT_B, 8 }
 
+#define SOLAR_SENSE_ADDR 0x68
+#define SOLAR_SENSE_DATA_LEN 3
 // Controller board LEDs
 static const GPIOAddress leds[] = {
   { .port = GPIO_PORT_B, .pin = 5 },   //
@@ -37,10 +41,10 @@ int main(void) {
 
   // TODO: 
   // 1. i2c speed? 
-  // 2. port? 
+  // 2. 
 
   const I2CSettings i2c_settings = {
-    .speed = I2C_SPEED_FAST,    //
+    .speed = I2C_SPEED_STANDARD,    //
     .sda = SOLAR_SENSE_I2C_BUS_SDA,  //
     .scl = SOLAR_SENSE_I2C_BUS_SCL,  //
   };
@@ -50,14 +54,24 @@ int main(void) {
   for (size_t i = 0; i < SIZEOF_ARRAY(leds); i++) {
     gpio_init_pin(&leds[i], &led_settings);
   }
+  // Reading config register
 
+  uint8_t data[SOLAR_SENSE_DATA_LEN];
+
+  StatusCode s = i2c_read(I2C_PORT_1, SOLAR_SENSE_ADDR, data, SOLAR_SENSE_DATA_LEN);
+
+  if (!status_ok(s)) {
+    LOG_DEBUG("i2c read fail\n");
+  } else {
+    uint8_t cfg_byte = data[2];
+    LOG_DEBUG("cfg_byte: %02hhx\n", cfg_byte);
+  }
   while (true) {
     for (size_t i = 0; i < SIZEOF_ARRAY(leds); i++) {
       gpio_toggle_state(&leds[i]);
       delay_ms(50);
     }
   }
-
   return 0;
 }
 
