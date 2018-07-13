712f4fb436279bd37ca019bae4eb06a1e030fbdc
Titus Chow <tituschow96@gmail.com>
2018-07-13 13:48:33 -0500

    WIP: enable hazards while car is off?

--- projects/driver_controls/src/fsm/power_fsm.c
+++ projects/driver_controls/src/fsm/power_fsm.c
@@ -75,7 +75,7 @@ static bool prv_guard_off(const Event *e) {
     case INPUT_EVENT_POWER_STATE_DRIVE:
     case INPUT_EVENT_HEADLIGHT_STATE_OFF:
     case INPUT_EVENT_HAZARDS_STATE_OFF:
-    case INPUT_EVENT_HAZARDS_STATE_ON:
+    case INPUT_EVENT_CENTER_CONSOLE_HAZARDS_PRESSED:
       return true;
     default:
       return false;
