d7e9415a7f2ee307799d361f53451b3c1165e391
Karl <karlding@users.noreply.github.com>
2018-09-30 21:51:41 -0700

    ELEC-538: Run format over FreeRTOSConfig.h

--- libraries/ms-freertos/inc/FreeRTOSConfig.h
+++ libraries/ms-freertos/inc/FreeRTOSConfig.h
@@ -6,73 +6,73 @@
 extern uint32_t SystemCoreClock;
 #endif
 
-#define configUSE_PREEMPTION                      1
-#define configUSE_PORT_OPTIMISED_TASK_SELECTION   0
-#define configUSE_TICKLESS_IDLE                   0
-#define configCPU_CLOCK_HZ                        (SystemCoreClock)
-#define configTICK_RATE_HZ                        ((TickType_t)1000)
-#define configMAX_PRIORITIES                      5
-#define configMINIMAL_STACK_SIZE                  ((uint16_t)128)
-#define configMAX_TASK_NAME_LEN                   16
-#define configUSE_16_BIT_TICKS                    0
-#define configIDLE_SHOULD_YIELD                   0
-#define configUSE_TASK_NOTIFICATIONS              1
-#define configUSE_MUTEXES                         1
-#define configUSE_RECURSIVE_MUTEXES               1
-#define configUSE_COUNTING_SEMAPHORES             1
-#define configQUEUE_REGISTRY_SIZE                 8
-#define configUSE_QUEUE_SETS                      0
-#define configUSE_TIME_SLICING                    1
-#define configUSE_NEWLIB_REENTRANT                0
-#define configENABLE_BACKWARD_COMPATIBILITY       0
-#define configNUM_THREAD_LOCAL_STORAGE_POINTERS   5
+#define configUSE_PREEMPTION 1
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION 0
+#define configUSE_TICKLESS_IDLE 0
+#define configCPU_CLOCK_HZ (SystemCoreClock)
+#define configTICK_RATE_HZ ((TickType_t)1000)
+#define configMAX_PRIORITIES 5
+#define configMINIMAL_STACK_SIZE ((uint16_t)128)
+#define configMAX_TASK_NAME_LEN 16
+#define configUSE_16_BIT_TICKS 0
+#define configIDLE_SHOULD_YIELD 0
+#define configUSE_TASK_NOTIFICATIONS 1
+#define configUSE_MUTEXES 1
+#define configUSE_RECURSIVE_MUTEXES 1
+#define configUSE_COUNTING_SEMAPHORES 1
+#define configQUEUE_REGISTRY_SIZE 8
+#define configUSE_QUEUE_SETS 0
+#define configUSE_TIME_SLICING 1
+#define configUSE_NEWLIB_REENTRANT 0
+#define configENABLE_BACKWARD_COMPATIBILITY 0
+#define configNUM_THREAD_LOCAL_STORAGE_POINTERS 5
 
-// Memory allocatoin related definitions
-#define configSUPPORT_STATIC_ALLOCATION           1
-#define configSUPPORT_DYNAMIC_ALLOCATION          0
-#define configTOTAL_HEAP_SIZE                     0
-#define configAPPLICATION_ALLOCATED_HEAP          0
+// Memory allocation related definitions
+#define configSUPPORT_STATIC_ALLOCATION 1
+#define configSUPPORT_DYNAMIC_ALLOCATION 0
+#define configTOTAL_HEAP_SIZE 0
+#define configAPPLICATION_ALLOCATED_HEAP 0
 
 // Hook function related definitions
-#define configUSE_IDLE_HOOK                       0
-#define configUSE_TICK_HOOK                       0
-#define configCHECK_FOR_STACK_OVERFLOW            0
-#define configUSE_MALLOC_FAILED_HOOK              0
-#define configUSE_DAEMON_TASK_STARTUP_HOOK        0
+#define configUSE_IDLE_HOOK 0
+#define configUSE_TICK_HOOK 0
+#define configCHECK_FOR_STACK_OVERFLOW 0
+#define configUSE_MALLOC_FAILED_HOOK 0
+#define configUSE_DAEMON_TASK_STARTUP_HOOK 0
 
 // Run time and task stats gathering related definitions
-#define configGENERATE_RUN_TIME_STATS             0
-#define configUSE_TRACE_FACILITY                  0
-#define configUSE_STATS_FORMATTING_FUNCTIONS      0
+#define configGENERATE_RUN_TIME_STATS 0
+#define configUSE_TRACE_FACILITY 0
+#define configUSE_STATS_FORMATTING_FUNCTIONS 0
 
 // Co-routine definitions
-#define configUSE_CO_ROUTINES                     0
-#define configMAX_CO_ROUTINE_PRIORITIES           2
+#define configUSE_CO_ROUTINES 0
+#define configMAX_CO_ROUTINE_PRIORITIES 2
 
 // Software timer related functions
-#define configUSE_TIMERS                          0
-#define configTIMER_TASK_PRIORITY                 3
-#define configTIMER_QUEUE_LENGTH                  10
-#define configTIMER_TASK_STACK_DEPTH              configMINIMAL_STACK_SIZE
+#define configUSE_TIMERS 0
+#define configTIMER_TASK_PRIORITY 3
+#define configTIMER_QUEUE_LENGTH 10
+#define configTIMER_TASK_STACK_DEPTH configMINIMAL_STACK_SIZE
 
 // Optional functions - most linkers will remove unused functions anyway
-#define INCLUDE_vTaskPrioritySet                  1
-#define INCLUDE_uxTaskPriorityGet                 1
-#define INCLUDE_vTaskDelete                       1
-#define INCLUDE_vTaskSuspend                      1
-#define INCLUDE_xResumeFromISR                    1
-#define INCLUDE_vTaskDelayUntil                   1
-#define INCLUDE_vTaskDelay                        0
-#define INCLUDE_xTaskGetSchedulerState            1
-#define INCLUDE_xTaskGetCurrentTaskHandle         1
-#define INCLUDE_uxTaskGetStackHighWaterMark       0
-#define INCLUDE_xTaskGetIdleTaskHandle            0
-#define INCLUDE_eTaskGetState                     0
-#define INCLUDE_xEventGroupSetBitFromISR          1
-#define INCLUDE_xTimerPendFunctionCall            0
-#define INCLUDE_xTaskAbortDelay                   0
-#define INCLUDE_xTaskGetHandle                    0
-#define INCLUDE_xTaskResumeFromISR                1
+#define INCLUDE_vTaskPrioritySet 1
+#define INCLUDE_uxTaskPriorityGet 1
+#define INCLUDE_vTaskDelete 1
+#define INCLUDE_vTaskSuspend 1
+#define INCLUDE_xResumeFromISR 1
+#define INCLUDE_vTaskDelayUntil 1
+#define INCLUDE_vTaskDelay 0
+#define INCLUDE_xTaskGetSchedulerState 1
+#define INCLUDE_xTaskGetCurrentTaskHandle 1
+#define INCLUDE_uxTaskGetStackHighWaterMark 0
+#define INCLUDE_xTaskGetIdleTaskHandle 0
+#define INCLUDE_eTaskGetState 0
+#define INCLUDE_xEventGroupSetBitFromISR 1
+#define INCLUDE_xTimerPendFunctionCall 0
+#define INCLUDE_xTaskAbortDelay 0
+#define INCLUDE_xTaskGetHandle 0
+#define INCLUDE_xTaskResumeFromISR 1
 
 // Interrupt nesting behaviour configuration
 
@@ -86,7 +86,7 @@ extern uint32_t SystemCoreClock;
 
 // The lowest interrupt priority that can be used in a call to a "set priority"
 // function
-#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY   3
+#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY 3
 
 // The highest interrupt priority that can be used by any interrupt service
 // routine that makes calls to interrupt safe FreeRTOS API functions.
