c4dae43ab72192bc75601b23b1f0bec1081a4781
Misha Zharov <mishazharov1@gmail.com>
2018-11-17 12:20:38 -0500

    pulling parser from master

--- projects/can_dump/scripts/dump_system.py
+++ projects/can_dump/scripts/dump_system.py
@@ -2,179 +2,87 @@
 """ Dumps CAN information from SocketCAN/serial """
 import argparse
 from abc import abstractmethod
-import binascii
 import datetime
 import logging
 import os
 import socket
 import struct
 import serial
+import serial.tools.list_ports
 from cobs import cobs
-import math
 
-DATA_POWER_STATE = ['idle', 'charge', 'drive']
-LIGHTS_ID_NAME = [
-    'High beams', 'Low beams', 'DRL', 'Brakes',
-    'Left Turn', 'Right Turn', 'Hazards', 'BPS Strobe'
-]
-HEARTBEAT_REASON_BITSET = [
-    'Killswitch', 'AFE Cell', 'AFE Temp', 'AFE FSM', 'Current', 'ACK Timeout'
-]
+from can_message import CanMessage
 
-def data_bps_heartbeat(state):
-    """BPS heartbeat data format"""
-    codes = []
-    for i, name in enumerate(HEARTBEAT_REASON_BITSET):
-        mask = 1 << i
-        if state & mask == mask:
-            codes.append(name)
-
-    if not codes:
-        return 'Ok'
-    else:
-        return '{} = {}'.format(state, ', '.join(codes))
-
-def data_relay(state):
-    """Relay state data format"""
-    return 'close' if state else 'open'
-
-def data_power_state(state):
-    """Power state data format"""
-    return DATA_POWER_STATE[state]
-
-def data_lights_state(light_id, state):
-    """Lights state data format"""
-    id_name = LIGHTS_ID_NAME[light_id]
-    state_name = 'on' if state else 'off'
-    return '{}: {}'.format(id_name, state_name)
-
-def data_battery_vt(module, voltage, temperature):
-    """Battery V/T data format"""
-    supply = 50000
-    r_fixed = 10000
-    resistance = (supply - temperature) * r_fixed / temperature
-    beta = 3380
-    r_0 = 10000
-    t_0 = 298.15
-    r_inf = r_0 * math.exp(-beta/t_0)
-    temp = beta / math.log(resistance / r_inf) - 273.15
-    return 'C{}: {:.1f}mV aux {:.1f}mV ({:.1f}C)'.format(module, voltage / 10, temperature / 10, temp)
-
-def data_battery_voltage_current(voltage, current):
-    """Battery total voltage/current data format"""
-    return '{:.4f}V {:.4f}A'.format(voltage / 10000, current / 1000000)
-
-def data_speed(left_cms, right_cms):
-    avg_cms = left_cms + right_cms / 2
-    avg_mph = 0.0224 * avg_cms
-
-    return '{:.2f} MPH'.format(avg_mph)
-
-def data_dump(*args):
-    """Generic data dump format"""
-    return ' '.join(['{}'.format(arg) for arg in args])
-
-# Name, struct format, data format 0, ...
-MESSAGE_LOOKUP = {
-    0: ('BPS Heartbeat', '<B', data_bps_heartbeat),
-    1: ('Chaos Fault', '', data_dump),
-    2: ('Battery relay (Main)', '<B', data_relay),
-    3: ('Battery relay (Slave)', '<B', data_relay),
-    4: ('Motor relay', '<B', data_relay),
-    5: ('Solar relay (Rear)', '<B', data_relay),
-    6: ('Solar relay (Front)', '<B', data_relay),
-    7: ('Power state', '<B', data_power_state),
-    8: ('Chaos Heartbeat', '', data_dump),
-    18: ('Drive Output', '<hhhh', data_dump),
-    19: ('Cruise Target', '<B', data_dump),
-    21: ('Set Discharge Bitset', '<Q', data_dump),
-    22: ('Discharge Bitset', '<Q', data_dump),
-    23: ('Lights Sync', '', data_dump),
-    24: ('Lights State', '<BB', data_lights_state),
-    26: ('Charger state', '<B', data_dump),
-    27: ('Charger relay', '<B', data_relay),
-    32: ('Battery V/T', '<HHH', data_battery_vt),
-    33: ('Battery Voltage/Current', '<ii', data_battery_voltage_current),
-    35: ('Motor Bus Measurement', '<hhhh', data_dump),
-    36: ('Motor Velocity', '<hh', data_speed),
-    37: ('Motor Debug', '<HHHH', data_dump),
-    38: ('Motor Temps', '<HHHH', data_dump),
-    43: ('Aux & DC/DC V/C', '<HHHH', data_dump),
-}
-
-def parse_msg(can_id, data, masked):
-    """Parses and prints a system CAN message.
+def select_device():
+    """User-provided serial device selector.
 
     Args:
-        can_id: Raw standard CAN ID.
-        data: Message data. Up to 8 bytes.
-        masked: List of masked messages.
+        None
 
     Returns:
-        None
+        The selected serial device as ListPortInfo.
     """
-    # System CAN ID format:
-    # [0:3] Source ID
-    # [4] Message Type (ACK/DATA)
-    # [5:10] Message ID
-    source_id = can_id & 0xf
-    msg_type = (can_id >> 4) & 0x1
-    msg_id = (can_id >> 5) & 0x3f
+    while True:
+        print('Pick the serial device:')
+        ports = serial.tools.list_ports.comports()
+        for i, port in enumerate(ports):
+            print('{}: {}'.format(i, port))
 
-    msg_type_name = 'ACK' if msg_type == 1 else 'DATA'
-
-    if msg_id in masked:
-        return
-    elif msg_id in MESSAGE_LOOKUP:
-        name, fmt, data_fn = MESSAGE_LOOKUP[msg_id]
-        if fmt:
-            try:
-                unpacked_data = struct.unpack(fmt, data)
-            except struct.error:
-                print('Invalid {}'.format(msg_id))
-                return
-        else:
-            unpacked_data = []
-
-        if msg_type_name == 'ACK':
-            print('{} ACK from {}'.format(name, source_id))
-        else:
-            print('{}: {}'.format(name, data_fn(*unpacked_data)))
-    else:
-        # Unrecognized message
-        print('{} from {} ({}): 0x{}'.format(msg_id, source_id, msg_type_name,
-                                             binascii.hexlify(data).decode('ascii')))
+        try:
+            chosen_port = ports[int(input())]
+            print('Selected {}'.format(chosen_port))
+            return chosen_port
+        except IndexError:
+            print('Invalid device!')
+            continue
 
 class CanDataSource:
-    """Abstract class for CAN logger sources"""
+    """An abstract class representing a CAN data source.
+
+    A CAN data source is an interface that can be listened on for a CAN message
+    representation.
+    """
     def __init__(self, masked=None):
         self.masked = masked or []
 
     @abstractmethod
     def get_packet(self):
-        """Retrieves the CAN ID and data field of the next CAN packet.
+        """Fetch the next packet from the CAN data source.
 
         Args:
             None
 
         Returns:
-            can_id: Integer representation of the received CAN ID.
-            data: Byte representation of the received CAN data.
+            A tuple containing the system CAN bus message ID and the message
+            data.
         """
         pass
 
     def run(self):
-        """Main loop for CAN data logging"""
+        """Start reading from the CAN data source.
+
+        This reads messages from the data source, parses each one and prints
+        to stdout, before logging into a log file until the process terminates.
+
+        Args:
+            None
+
+        Returns:
+            Does not return.
+        """
         while True:
             # CAN ID, data, DLC
             can_id, data = self.get_packet()
-            parse_msg(can_id, data, self.masked)
+            if can_id not in self.masked:
+                can_msg = CanMessage(can_id, data)
+                can_msg.parse()
 
-            logging.info('0x%x,0x%s,%d', can_id, data.hex(), len(data))
+            logging.info('{},{},{}'.format(can_id, data, len(data))) #pylint: disable=logging-format-interpolation
 
 class SocketCanDataSource(CanDataSource):
-    """SocketCAN data source"""
-
+    """
+    A CAN data source that reads from a bound SocketCAN interface (slcan0)
+    """
     CAN_FRAME_FMT = "<IB3x8s"
     def __init__(self, masked):
         super().__init__(masked)
@@ -190,8 +98,9 @@ class SocketCanDataSource(CanDataSource):
         return can_id, data
 
 class SerialCanDataSource(CanDataSource):
-    """Serial (COBS encoded) data source"""
-
+    """
+    A CAN datasource that reads from a bound serial port interface.
+    """
     def __init__(self, masked, device):
         super().__init__(masked)
         self.ser = serial.Serial(device, 115200)
@@ -244,13 +153,14 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-l', '--log_dir', help='Directory for storing logs',
                         nargs='?', default='logs')
-    parser.add_argument('-m', '--mask', help='Mask message ID from being parsed', action='append', type=int)
+    parser.add_argument('-m', '--mask', help='Mask message ID from being parsed', action='append')
     parser.add_argument('device', help='Serial device or "slcan0"')
     args = parser.parse_args()
 
-    log_file = '{}/system_can_{}.log'.format(args.log_dir, datetime.datetime.now())
+    date_formatted = datetime.datetime.now().strftime('%Y-%m-%d %H.%M.%S.%f')
+    log_file = '{}/system_can_{}.log'.format(args.log_dir, date_formatted)
     os.makedirs(os.path.dirname(log_file), exist_ok=True)
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s,%(message)s', filename=log_file)
+    logging.basicConfig(level=logging.DEBUG, format='"%(asctime)s",%(message)s', filename=log_file)
 
     print('Masking IDs {}'.format(args.mask))
     if args.device == 'slcan0':
