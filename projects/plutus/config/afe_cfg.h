6548a0303a54ffe5ddfa9d645ae1ca5ffbb22517
Ash Raji <ashrafraji@hotmail.com>
2019-01-16 10:31:00 -0500

    changed number of devices to 1

--- projects/plutus/config/afe_cfg.h
+++ projects/plutus/config/afe_cfg.h
@@ -22,28 +22,24 @@
 #define PLUTUS_CFG_INPUT_BITSET_FIRST 0x3F
 
 #ifdef PLUTUS_CFG_DEBUG_PACK
-#define PLUTUS_CFG_AFE_DEVICES_IN_CHAIN 4
-#define PLUTUS_CFG_AFE_TOTAL_CELLS 48
+#define PLUTUS_CFG_AFE_DEVICES_IN_CHAIN 1
+#define PLUTUS_CFG_AFE_TOTAL_CELLS 12
 // We're using 18 modules per box -> 2 AFEs each
 // clang-format off
   #define PLUTUS_CFG_CELL_BITSET_ARR                                  \
-    { PLUTUS_CFG_INPUT_BITSET_FULL, PLUTUS_CFG_INPUT_BITSET_FULL,    \
-      PLUTUS_CFG_INPUT_BITSET_FULL, PLUTUS_CFG_INPUT_BITSET_FULL, }
+    { PLUTUS_CFG_INPUT_BITSET_FULL}
   #define PLUTUS_CFG_AUX_BITSET_ARR                                   \
-    { PLUTUS_CFG_INPUT_BITSET_FULL, PLUTUS_CFG_INPUT_BITSET_FULL,    \
-      PLUTUS_CFG_INPUT_BITSET_FULL, PLUTUS_CFG_INPUT_BITSET_FULL, }
+    { PLUTUS_CFG_INPUT_BITSET_FULL}
 // clang-format on
 #else
 // number of devices in daisy chain (including master)
-#define PLUTUS_CFG_AFE_DEVICES_IN_CHAIN 4
-#define PLUTUS_CFG_AFE_TOTAL_CELLS 36
+#define PLUTUS_CFG_AFE_DEVICES_IN_CHAIN 1
+#define PLUTUS_CFG_AFE_TOTAL_CELLS 12
 // We're using 18 modules per box -> 2 AFEs each
 // clang-format off
   #define PLUTUS_CFG_CELL_BITSET_ARR                                  \
-    { PLUTUS_CFG_INPUT_BITSET_FULL, PLUTUS_CFG_INPUT_BITSET_SPLIT,    \
-      PLUTUS_CFG_INPUT_BITSET_FULL, PLUTUS_CFG_INPUT_BITSET_SPLIT, }
+    { PLUTUS_CFG_INPUT_BITSET_FULL }
   #define PLUTUS_CFG_AUX_BITSET_ARR                                   \
-    { PLUTUS_CFG_INPUT_BITSET_FULL, PLUTUS_CFG_INPUT_BITSET_FIRST,    \
-      PLUTUS_CFG_INPUT_BITSET_FULL, PLUTUS_CFG_INPUT_BITSET_FIRST, }
+    { PLUTUS_CFG_INPUT_BITSET_FULL}
 // clang-format on
 #endif
