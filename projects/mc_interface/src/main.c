b028aeb323bb6c7c236136b97a1f08b13000ee1d
Misha Zharov <mishazharov1@gmail.com>
2019-01-16 21:14:03 -0500

    ELEC-497: Fixing build (x2)

--- projects/mc_interface/src/main.c
+++ projects/mc_interface/src/main.c
@@ -1,12 +1,11 @@
 #include "can.h"
 #include "can_msg_defs.h"
 #include "drive_can.h"
-#include "generic_can_mcp2515.h"
+#include "generic_can_uart.h"
 #include "gpio.h"
 #include "heartbeat_rx.h"
 #include "interrupt.h"
 #include "mc_cfg.h"
-#include "mcp2515.h"
 #include "motor_controller.h"
 #include "sequenced_relay.h"
 #include "uart.h"
@@ -47,8 +46,9 @@ static void prv_setup_motor_can(void) {
     .rx = MC_CFG_CAN_UART_RX,
     .alt_fn = MC_CFG_CAN_UART_ALTFN,
   };
+  uart_init(MC_CFG_CAN_UART_PORT, &uart_settings, &s_uart_storage);
 
-  generic_can_mcp2515_init(&s_can_mcp2515, &mcp2515_settings);
+  generic_can_uart_init(&s_can_uart, MC_CFG_CAN_UART_PORT);
 }
 
 int main(void) {
@@ -61,7 +61,7 @@ int main(void) {
 
   // clang-format off
   MotorControllerSettings mc_settings = {
-    .motor_can = (GenericCan *)&s_can_mcp2515,
+    .motor_can = (GenericCan *)&s_can_uart,
     .ids = {
       [MOTOR_CONTROLLER_LEFT] = {
           .motor_controller = MC_CFG_MOTOR_CAN_ID_MC_LEFT,
