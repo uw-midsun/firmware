cace19059cbf4f53827373e7627ea372ee55e577
myh999 <myh.999@hotmail.com>
2019-02-04 03:49:34 +0000

    ELEC-71: Resolve Travis errors

--- projects/telemetry/src/gps.c
+++ projects/telemetry/src/gps.c
@@ -53,16 +53,11 @@ StatusCode gps_init(GpsSettings *settings, GpsStorage *storage) {
   };
 
   // Initializes UART
-  StatusCode ret =
-      uart_init(s_settings->port, s_settings->uart_settings, &s_settings->uart_storage);
+  uart_init(s_settings->port, s_settings->uart_settings, &s_settings->uart_storage);
   uart_set_rx_handler(s_settings->port, prv_gps_callback, NULL);
 
-  if (!status_ok(ret)) {
-    return status_msg(STATUS_CODE_INTERNAL_ERROR, "Error initializing UART ports");
-  }
-
   // Initializes the pins
-  ret |= gpio_init_pin(s_settings->pin_power, &telemetry_settings_gpio_general);
+  StatusCode ret = gpio_init_pin(s_settings->pin_power, &telemetry_settings_gpio_general);
   ret |= gpio_init_pin(s_settings->pin_on_off, &telemetry_settings_gpio_general);
 
   if (!status_ok(ret)) {
