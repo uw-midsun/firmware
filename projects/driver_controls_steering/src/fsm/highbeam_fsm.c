4b1b70bf9b3d802126f83ed1d7325b4de30cfd2a
Thomas Kidd <tjkidd@uwaterloo.ca>
2019-03-14 00:48:32 +0000

    ELEC-580: Updated console main.c and fixed errors in highbeam_fsm.c

--- projects/driver_controls_steering/src/fsm/highbeam_fsm.c
+++ projects/driver_controls_steering/src/fsm/highbeam_fsm.c
@@ -1,4 +1,6 @@
-#include "hazards_fsm.h"
+#include "highbeam_fsm.h"
+#include "can_transmit.h"
+#include "exported_enums.h"
 #include "sc_input_event.h"
 #include "steering_output.h"
 
@@ -6,11 +8,11 @@
 FSM_DECLARE_STATE(highbeam_on);
 FSM_DECLARE_STATE(highbeam_off);
 
-FSM_STATE_TRANSITION(highbeams_off) {
-  FSM_ADD_TRANSITION(INPUT_EVENT_CONTROL_STALK_DIGITAL_HEADLIGHT_FWD_PRESSED, highbeam_on);
+FSM_STATE_TRANSITION(highbeam_off) {
+  FSM_ADD_TRANSITION(INPUT_EVENT_CONTROL_STALK_DIGITAL_HIGH_BEAM_FWD_PRESSED, highbeam_on);
 }
 
-FSM_STATE_TRANSITION(highbeams_on) {
+FSM_STATE_TRANSITION(highbeam_on) {
   FSM_ADD_TRANSITION(INPUT_EVENT_CONTROL_STALK_DIGITAL_HIGH_BEAM_FWD_RELEASED, highbeam_off);
 }
 
@@ -27,8 +29,8 @@ static void prv_highbeam_on_output(Fsm *fsm, const Event *e, void *context) {
 }
 
 StatusCode highbeam_fsm_init(Fsm *fsm, EventArbiterStorage *storage) {
-  fsm_init_state(highbeam_off, prv_highbeam_off_output);
-  fsm_init_state(highbeam_on, prv_highbeam_on_output);
+  fsm_state_init(highbeam_off, prv_highbeam_off_output);
+  fsm_state_init(highbeam_on, prv_highbeam_on_output);
 
   EventArbiterGuard *guard = event_arbiter_add_fsm(storage, fsm, NULL);
 
