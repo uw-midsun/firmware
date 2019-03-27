b46d6489f035f3f46d605c9cd259fc3d3efd1180
Misha Zharov <mishazharov1@gmail.com>
2019-03-27 02:13:14 +0000

    ELEC-63: Fixing x86 stub

--- libraries/ms-helper/src/x86/sd_binary.c
+++ libraries/ms-helper/src/x86/sd_binary.c
@@ -10,18 +10,11 @@ StatusCode sd_wait_data(SpiPort spi, uint8_t data) {
   return status_code(STATUS_CODE_UNIMPLEMENTED);
 }
 
-StatusCode sd_read_blocks(SpiPort spi, uint32_t *pData, uint32_t ReadAddr,
-                          uint32_t NumberOfBlocks) {
+StatusCode sd_read_blocks(SpiPort spi, uint8_t *dest, uint32_t ReadAddr, uint32_t NumberOfBlocks) {
   return status_code(STATUS_CODE_UNIMPLEMENTED);
 }
 
-StatusCode sd_write_blocks(SpiPort spi, uint32_t *pData, uint32_t WriteAddr,
-                           uint32_t NumberOfBlocks) {
-  return status_code(STATUS_CODE_UNIMPLEMENTED);
-}
-
-StatusCode sd_multi_write_blocks(SpiPort spi, uint32_t *pData, uint32_t WriteAddr,
-                                 uint32_t NumberOfBlocks) {
+StatusCode sd_write_blocks(SpiPort spi, uint8_t *src, uint32_t WriteAddr, uint32_t NumberOfBlocks) {
   return status_code(STATUS_CODE_UNIMPLEMENTED);
 }
 
