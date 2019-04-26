2ef63606e66c4ded1027ed56c3430e3676fc0932
Becca Cho <cho.becca@hotmail.com>
2019-04-26 02:41:12 -0400

    WIP: Disable Chaos flags

--- projects/chaos/inc/chaos_flags.h
+++ projects/chaos/inc/chaos_flags.h
@@ -2,5 +2,7 @@
 
 // Flags for Chaos to disable/enable commonly toggled features.
 
-#define CHAOS_FLAG_ENABLE_SOLAR_RELAYS
-#define CHAOS_FLAG_ENABLE_POWERTRAIN_HB
+// TODO: Enable the solar relays instead of performing a manual connection
+// #define CHAOS_FLAG_ENABLE_SOLAR_RELAYS
+// TODO: Re-enable the powertrain heartbeat if necessary
+// #define CHAOS_FLAG_ENABLE_POWERTRAIN_HB
