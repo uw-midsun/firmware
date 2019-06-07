110f7e877b8421871568de82be7dc6cb33b425f7
Karl <karlding@users.noreply.github.com>
2019-06-07 14:24:21 -0700

    ELEC-580: Add Pedal FSMs and project skeleton
    Add the Pedal FSMs that are not dependant on Drive Output. These FSMs
    are only dependent on input from Driver Controls: Center Console and
    Driver Controls: Steering.
    
    An Event Arbiter is introduced in order to block FSMs from accepting
    input in certain states. Technically, this could be flattend into a
    giant FSM by computing the Transitive Closure, but it probably makes it
    a little easier to reason about and modify this way. This implementation
    is pretty much taken from the old Driver Controls code.

---
+++ projects/driver_controls_pedal/src/fsm/horn_fsm.c
@@ -0,0 +1,48 @@
+#include "horn_fsm.h"
+
+#include "can_transmit.h"
+#include "event_arbiter.h"
+#include "exported_enums.h"
+#include "pedal_events.h"
+
+// Horn FSM state definitions
+
+FSM_DECLARE_STATE(state_horn_off);
+FSM_DECLARE_STATE(state_horn_on);
+
+// Horn FSM transition table definitions
+
+FSM_STATE_TRANSITION(state_horn_off) {
+  FSM_ADD_TRANSITION(PEDAL_EVENT_INPUT_CONTROL_STALK_DIGITAL_HORN_PRESSED, state_horn_on);
+}
+
+FSM_STATE_TRANSITION(state_horn_on) {
+  FSM_ADD_TRANSITION(PEDAL_EVENT_INPUT_MECH_BRAKE_RELEASED, state_horn_off);
+
+  FSM_ADD_TRANSITION(PEDAL_EVENT_INPUT_POWER_STATE_OFF, state_horn_off);
+  FSM_ADD_TRANSITION(PEDAL_EVENT_INPUT_POWER_STATE_FAULT, state_horn_off);
+}
+
+// Horn FSM output function
+static void prv_horn_off_output(Fsm *fsm, const Event *e, void *context) {
+  CAN_TRANSMIT_HORN(EE_HORN_STATE_OFF);
+}
+
+static void prv_horn_on_output(Fsm *fsm, const Event *e, void *context) {
+  CAN_TRANSMIT_HORN(EE_HORN_STATE_ON);
+}
+
+StatusCode horn_fsm_init(Fsm *fsm, EventArbiterStorage *storage) {
+  fsm_state_init(state_horn_off, prv_horn_off_output);
+  fsm_state_init(state_horn_on, prv_horn_on_output);
+
+  EventArbiterGuard *guard = event_arbiter_add_fsm(storage, fsm, NULL);
+
+  if (guard == NULL) {
+    return status_code(STATUS_CODE_RESOURCE_EXHAUSTED);
+  }
+
+  fsm_init(fsm, "Horn FSM", &state_horn_off, guard);
+
+  return STATUS_CODE_OK;
+}
