67f1f0327b34e37ed0e9006dd20e6e1474bea701
Taiping Li <litaiping97@gmail.com>
2019-06-27 17:31:30 +0000

    Disabled solar relays

--- projects/chaos/inc/chaos_flags.h
+++ projects/chaos/inc/chaos_flags.h
@@ -2,5 +2,5 @@
 
 // Flags for Chaos to disable/enable commonly toggled features.
 
-#define CHAOS_FLAG_ENABLE_SOLAR_RELAYS
+//#define CHAOS_FLAG_ENABLE_SOLAR_RELAYS
 //#define CHAOS_FLAG_ENABLE_POWERTRAIN_HB
