a2286d582ff8e981aec1b4447450f988293d1562
Taiping Li <litaiping97@gmail.com>
2019-07-02 17:00:33 +0000

    WIP: Merging from elec_639

--- projects/plutus/src/ltc_afe.c
+++ projects/plutus/src/ltc_afe.c
@@ -4,9 +4,44 @@
 #include "ltc_afe_impl.h"
 #include "plutus_event.h"
 
+#include <string.h>
+#include "can_transmit.h"
+#include "can_unpack.h"
+#include "can.h"
+#include "soft_timer.h"
+#include "log.h"
+
+static void prv_periodic_discharge_bitset(SoftTimerId timer_id, void *context) {
+  LtcAfeStorage *afe = context;
+  uint64_t data = { 0 };
+  memcpy(&data, afe->discharge_bitset, sizeof(data));
+  CAN_TRANSMIT_DISCHARGE_STATE(data);
+
+  soft_timer_start_millis(PLUTUS_CFG_LTC_AFE_DISCHARGE_DUMP_PERIOD_MS, prv_periodic_discharge_bitset, afe,
+                   NULL);
+}
+
+ static StatusCode prv_handle_can_bitset(const CanMessage *msg, void *context,
+                                        CanAckStatus *ack_reply) {
+  LtcAfeStorage *afe = context; 
+  uint8_t module = 0;
+  uint8_t discharge = 0; 
+
+  CAN_UNPACK_SET_MODULE_DISCHARGE(msg, &module, &discharge);
+  ltc_afe_impl_toggle_cell_discharge(afe, module, discharge); 
+  return STATUS_CODE_OK;
+}
+
 StatusCode ltc_afe_init(LtcAfeStorage *afe, const LtcAfeSettings *settings) {
   status_ok_or_return(ltc_afe_impl_init(afe, settings));
-  return ltc_afe_fsm_init(&afe->fsm, afe);
+  status_ok_or_return(ltc_afe_fsm_init(&afe->fsm, afe));
+
+  status_ok_or_return(
+    can_register_rx_handler(SYSTEM_CAN_MESSAGE_SET_MODULE_DISCHARGE, prv_handle_can_bitset, afe)
+  );
+
+  return soft_timer_start_millis(PLUTUS_CFG_LTC_AFE_DISCHARGE_DUMP_PERIOD_MS,
+                          prv_periodic_discharge_bitset, afe, NULL);
 }
 
 StatusCode ltc_afe_set_result_cbs(LtcAfeStorage *afe, LtcAfeResultCallback cell_result_cb,
---
+++ projects/plutus/test/test_ltc_afe_discharge.c
@@ -0,0 +1,112 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+#include "event_queue.h"
+#include "interrupt.h"
+#include "log.h"
+#include "ltc_afe.h"
+#include "ms_test_helpers.h"
+#include "plutus_cfg.h"
+#include "plutus_event.h"
+#include "soft_timer.h"
+#include "test_helpers.h"
+#include "unity.h"
+#include "delay.h"
+
+#define TEST_LTC_AFE_NUM_SAMPLES 100
+// Maximum total measurement error in filtered mode - +/-2.8mV
+// Maximum peak-to-peak at 7kHz: +/-250uV
+#define TEST_LTC_AFE_VOLTAGE_VARIATION 5
+
+static LtcAfeStorage s_afe;
+static uint16_t s_result_arr[PLUTUS_CFG_AFE_TOTAL_CELLS];
+
+static void prv_conv_cb(uint16_t *result_arr, size_t len, void *context) {
+  memcpy(s_result_arr, result_arr, sizeof(s_result_arr));
+}
+
+static void prv_wait_conv(void) {
+  Event e = { 0 };
+  do {
+    MS_TEST_HELPER_AWAIT_EVENT(e);
+    TEST_ASSERT_NOT_EQUAL(PLUTUS_EVENT_AFE_FAULT, e.id);
+    TEST_ASSERT_TRUE(ltc_afe_process_event(&s_afe, &e));
+  } while (e.id != PLUTUS_EVENT_AFE_CALLBACK_RUN);
+}
+
+void setup_test(void) {
+  gpio_init();
+  interrupt_init();
+  soft_timer_init();
+  event_queue_init();
+
+  const LtcAfeSettings afe_settings = {
+    .mosi = PLUTUS_CFG_AFE_SPI_MOSI,
+    .miso = PLUTUS_CFG_AFE_SPI_MISO,
+    .sclk = PLUTUS_CFG_AFE_SPI_SCLK,
+    .cs = PLUTUS_CFG_AFE_SPI_CS,
+
+    .spi_port = PLUTUS_CFG_AFE_SPI_PORT,
+    .spi_baudrate = PLUTUS_CFG_AFE_SPI_BAUDRATE,
+    .adc_mode = PLUTUS_CFG_AFE_MODE,
+
+    .cell_bitset = PLUTUS_CFG_CELL_BITSET_ARR,
+    .aux_bitset = PLUTUS_CFG_AUX_BITSET_ARR,
+
+    .cell_result_cb = prv_conv_cb,
+    .aux_result_cb = prv_conv_cb,
+    .result_context = NULL,
+  };
+
+  ltc_afe_init(&s_afe, &afe_settings);
+
+}
+
+void teardown_test(void) {}
+
+void test_ltc_afe_adc_conversion_initiated(void) {
+  TEST_ASSERT_OK(ltc_afe_request_cell_conversion(&s_afe));
+  prv_wait_conv();
+
+  // if the ADC conversion packet is valid, then these should be voltage values
+  // otherwise, the reading will correspond to the values the registers are
+  // initialized as by default (0xFF)
+  for (int i = 0; i < PLUTUS_CFG_AFE_TOTAL_CELLS; ++i) {
+    TEST_ASSERT_NOT_EQUAL(0xFFFF, s_result_arr[i]);
+  }
+}
+
+
+void test_ltc_afe_discharge_cell(void) {
+//  StatusCode discharge_status = ltc_afe_toggle_cell_discharge(&s_afe, 0, true);
+  delay_s(2);
+  LOG_DEBUG("Discharge test...\n");
+  StatusCode discharge_status; 
+
+  for(int i = 0; i < 12; i++){
+    discharge_status = ltc_afe_toggle_cell_discharge(&s_afe, i, true);
+    LOG_DEBUG("Status: %i for %i\n", discharge_status, i);
+    //TEST_ASSERT_OK(discharge_status);
+
+    ltc_afe_request_aux_conversion(&s_afe); 
+
+  }
+  prv_wait_conv();
+  delay_s(180);
+
+
+  //discharge_status = ltc_afe_toggle_cell_discharge(&s_afe, 11, true); 
+  //LOG_DEBUG("Status: %i\n", discharge_status); 
+
+  LOG_DEBUG("Discharge ON\n") ; 
+
+
+  for(int i = 0; i < 12; i++){
+    ltc_afe_toggle_cell_discharge(&s_afe, i, false);
+  }
+
+  //ltc_afe_toggle_cell_discharge(&s_afe, 11, false);
+  TEST_ASSERT_OK(ltc_afe_request_cell_conversion(&s_afe));
+  prv_wait_conv();
+
+}
