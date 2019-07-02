d239f9b6e90d4cc0de9a9fb7a1fc813693642db9
Taiping Li <litaiping97@gmail.com>
2019-07-02 16:55:15 +0000

    WIP Adding discharge over CAN

--- projects/plutus/src/main.c
+++ projects/plutus/src/main.c
@@ -17,7 +17,6 @@ static size_t s_telemetry_counter = 0;
 
 static void prv_periodic_tx_debug(SoftTimerId timer_id, void *context) {
   FaultMonitorResult *result = &s_fault_monitor.result;
-
   if (s_telemetry_counter < PLUTUS_CFG_AFE_TOTAL_CELLS) {
     CAN_TRANSMIT_BATTERY_VT(s_telemetry_counter, result->cell_voltages[s_telemetry_counter],
                             result->temp_voltages[s_telemetry_counter]);
