a4e8093b3ce453e88f8eb2bceb5318e02afbc0ba
karl ding <karlding@users.noreply.github.com>
2019-07-09 12:52:18 -0700

    ELEC-580: Remove old Driver Controls code (#349)
    This project is no longer used in MS XII.
    
    The firmware was originally used in MS XII prior to FSGP/ASC 2018, but
    due to various issues around EMC, the hardware was heavily modified for
    competition. It was later superseded by the distributed Driver Controls
    hardware, consisting of the Pedal, Steering, and Center Console boards,
    which were used at FSGP 2019.

--- projects/driver_controls/inc/fsm/hazards_fsm.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-// Handles hazards - this FSM will output hazard state over CAN.
-#include "event_arbiter.h"
-#include "fsm.h"
-
-StatusCode hazards_fsm_init(Fsm *fsm, EventArbiterStorage *storage);
