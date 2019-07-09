97974065eaa06934669d5558746fa312c4b9d8a7
Karl <karlding@users.noreply.github.com>
2019-07-09 12:31:38 -0700

    ELEC-580: Remove old Driver Controls code
    This project is no longer used in MS XII.
    
    The firmware was originally used in MS XII prior to FSGP/ASC 2018, but
    due to various issues around EMC, the hardware was heavily modified for
    competition. It was later superseded by the distributed Driver Controls
    hardware, consisting of the Pedal, Steering, and Center Console boards,
    which were used at FSGP 2019.

--- projects/driver_controls/inc/fsm/cruise_fsm.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-// Cruise control FSM
-//
-// Note that the cruise FSM sets drive output cruise states. If set, this value should override the
-// pedal state.
-#include "event_arbiter.h"
-#include "fsm.h"
-
-StatusCode cruise_fsm_init(Fsm *fsm, EventArbiterStorage *storage);
